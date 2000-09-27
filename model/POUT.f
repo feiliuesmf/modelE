4fda33d5bf88a93e7a2e05c0cc7ef34137a1b630
cdjal <cdjal>
2000-09-27 01:36:17 +0000

    Addition of POUT.f

---
+++ model/POUT.f
@@ -0,0 +1,13 @@
+C**** Dummy Routines for saving data for post-processing.
+
+      Subroutine POUT_GISS
+      return
+      end
+
+      Subroutine POUT_HDF
+      return
+      end
+
+      Subroutine POUT_NETCDF
+      return
+      end
