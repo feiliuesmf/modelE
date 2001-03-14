47620b206e4d8fbee6df52f4ba41e0997f056650
gavin <gavin>
2001-03-14 18:28:29 +0000

    Added a file that converts GDATA.xxx files to new modelE format for the ground initial conditions. Note that all the GDATA files I found only had GDATA(1:14) included (i.e. they were not current with the four layer sea ice code). If anyone has a more up-to-date GDATA.xxx file, (with GDATA(15:16)) we should convert 'conv_gic' to work with that one instead.
    Added conv_gic to Makefile

---
+++ aux/conv_gic.f
@@ -0,0 +1,84 @@
+      program convert_gic_files
+C**** convert GIC files from model II' (B399) format to modelE format 
+C**** compile with: gmake conv_gic.o
+C**** f90 -o conv_gic conv_gic.o ../model/*.o -O2 -64 -mips4 \
+C****                      -OPT:reorg_comm=off -w2 -listing
+C**** Note that since it uses modules and routines from the model, it
+C**** must be compiled after the model
+      USE CONSTANT, only : lhm,shi
+      USE MODEL_COM, only : im,jm,lm,iowrite 
+      USE GHYCOM, only : ghdata,snowe,tearth,wearth,aiearth,snoage
+      USE OCEAN, only : tocean,z1o
+      USE SEAICE_COM, only : rsi,msi,hsi,snowi
+      USE SEAICE, only : ace1i,xsi,ac2oim
+      USE LANDICE_COM, only : tlandi,snowli
+      IMPLICIT NONE
+      CHARACTER infile*60, outfile*60
+      INTEGER IARGC,iu_GIC,I,J,L,N,ioerr,kunit,iaction
+      REAL*8 MSI1,X
+      INTEGER*4 len
+      
+      IF (IARGC().lt.2) THEN
+        PRINT*,"Convert GIC files from old format to new"
+        PRINT*,"conv_gic filename output_file"
+        STOP
+      END IF
+
+      CALL GETARG(1,infile)
+      CALL GETARG(2,outfile)
+
+      iu_GIC=9
+
+      print*,trim(infile)
+
+      OPEN(iu_GIC,FILE=trim(infile),FORM="UNFORMATTED",STATUS="OLD")
+
+C**** Note that old GDATA file only has GDATA(1:14)!!
+
+      READ(iu_GIC,END=810) SNOWI,SNOWE,
+     *     ((HSI(1,I,J),I=1,IM),J=1,JM),TEARTH,WEARTH,AIEARTH,
+     *     ((HSI(2,I,J),I=1,IM),J=1,JM),((X,I=1,IM),J=1,JM),
+     *     (((SNOAGE(L,I,J),I=1,IM),J=1,JM),L=1,3),SNOWLI,
+     *     (((TLANDI(L,I,J),I=1,IM),J=1,JM),L=1,2),
+c     *     (((HSI(L,I,J),I=1,IM),J=1,JM),L=3,4),
+     *     GHDATA,((TOCEAN(1,I,J),I=1,IM),J=1,JM),RSI
+      CLOSE (iu_GIC)
+
+
+C**** define sea ice defaults: Use AC2OIM instead of MSI.
+      DO J=1,JM
+        DO I=1,IM
+          MSI1=SNOWI(I,J)+ACE1I
+          HSI(1,I,J) = (SHI*HSI(1,I,J)-LHM)*XSI(1)*MSI1
+          HSI(2,I,J) = (SHI*HSI(2,I,J)-LHM)*XSI(2)*MSI1
+          HSI(3,I,J) = (SHI*HSI(3,I,J)-LHM)*XSI(3)*AC2OIM
+          HSI(4,I,J) = (SHI*HSI(4,I,J)-LHM)*XSI(4)*AC2OIM
+          MSI(I,J) = AC2OIM
+          TOCEAN(2:3,I,J) = TOCEAN(1,I,J)
+        END DO
+      END DO
+
+      print*,TOCEAN(1,45,2),RSI(45,2),SNOWI(45,2)
+
+      OPEN(iu_GIC,FILE=trim(outfile),
+     *     FORM="UNFORMATTED",STATUS="UNKNOWN")
+
+      kunit=iu_GIC
+      iaction=iowrite
+      ioerr=-1
+      call io_ocean  (kunit,iaction,ioerr)
+      call io_seaice (kunit,iaction,ioerr)
+      call io_earth  (kunit,iaction,ioerr)
+      call io_soils  (kunit,iaction,ioerr)
+      call io_landice(kunit,iaction,ioerr)
+      if (ioerr.eq.1) WRITE(6,*) "I/O ERROR IN GIC FILE: KUNIT="
+     *     ,kunit
+      close (kunit)
+
+      print*,"New GIC file written out to ",trim(outfile)
+      stop
+ 800  print*,"Error reading in file"
+      stop
+ 810  print*,"Premature End of file"
+      stop
+      end 
