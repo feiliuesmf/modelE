9e20a6619eb0c6658da61a6fc60197e4c28a25ab
gavin <gavin>
2001-05-16 01:29:11 +0000

    Fix of conv_strat_rsf.f due to an odd rc(169) declaration instead of rc(161).

--- aux/conv_strat_rsf.f
+++ aux/conv_strat_rsf.f
@@ -27,7 +27,7 @@ C**** must be compiled after the model
       IMPLICIT NONE
       CHARACTER infile*60, outfile*60               ,clabel*156
       INTEGER IARGC,iu_AIC,I,J,L,N,ioerr,iu_TOPO    ,jc(100)
-      REAL*8 TAUX,X                                 ,rc(169)
+      REAL*8 TAUX,X                                 ,rc(161)    !(169)????
       REAL*8 MSI1
       INTEGER ItimeX
 !@ egcm_init_max maximum initial vaule of egcm
