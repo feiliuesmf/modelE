472bf746c40af16de195dc3a4e337219178872b0
gavin <gavin>
2001-05-31 15:37:32 +0000

    Since im,jm,lm,ls1,plbot are now in the database, no special mention is required inside 'qc' (which is good because it didn't work anyway!). I have added an option to 'qc' so that you can toggle between percentage timings (which I prefer) or actual timings (which apparently someone else prefers).
    The default is percentages, but using the option '-t' gives actual timing.
    Example:
    
    qc E001Sgas/fort.2
             1  1/ 1/1950 hr 1  E001Sgas (new modelE based on B402A - strat. versi
     TIME  25.24 (MINUTES) ATMOS. DYNAM 63.5 CONDENSATION  2.6    RADIATION 15.8
                                SURFACE  0.0  DIAGNOSTICS 10.0        OTHER  8.0
    
    qc -t E001Sgas/fort.2
             1  1/ 1/1950 hr 1  E001Sgas (new modelE based on B402A - strat. versi
     TIME  25.24 (MINUTES) ATMOS. DYNAM 16.0 CONDENSATION  0.7    RADIATION  4.0
                                SURFACE  0.0  DIAGNOSTICS  2.5        OTHER  2.0

--- aux/qc.f
+++ aux/qc.f
@@ -1,33 +1,33 @@
       program qc
 !@sum  qc query c-array and other parameters
-!@auth Original Development Team
+!@auth G. Schmidt/R. Ruedy
 !@ver  1.0
       USE MODEL_COM
-c     USE CLD01_COM_E001
-      USE RADNCB
-      USE DAGCOM
       USE TIMINGS
       USE PARAM
       IMPLICIT NONE
-      CHARACTER*80 FILEIN,DIR*60
-      INTEGER N,NARGS,K,iargc,KSTART,KFILE,I,days_togo
-      INTEGER :: ioerr=0
-      REAL*8 TOT,yrs_togo
-      LOGICAL QCALL
-
-      QCALL=.FALSE.
-      KSTART=1
+      CHARACTER*80 FILEIN
+      INTEGER N,NARGS,K,iargc,KFILE,I,days_togo
+      INTEGER :: ioerr=0, KSTART=1
+      REAL*8 TOT,yrs_togo,FAC,FACT
+      LOGICAL :: QCALL = .FALSE., QCMIN=.FALSE.
 
       NARGS = IARGC()
       IF(NARGS.LE.0)  GO TO 800
-C**** check for argument
-      CALL GETARG(1,FILEIN)
+C**** check for arguments
+ 10   CALL GETARG(KSTART,FILEIN)
       IF (FILEIN(1:1).eq."-") THEN
-        QCALL=.TRUE.
-        KSTART=2
+        SELECT CASE (FILEIN(2:2))
+        CASE ("a","A")
+          QCALL=.TRUE.
+        CASE ("t","T")
+          QCMIN=.TRUE.
+        END SELECT
+        KSTART=KSTART+1
+        GOTO 10
       END IF
 
- 150  DO K=KSTART,NARGS
+      DO K=KSTART,NARGS
       if (qcall .and. k.gt.kstart) write (6,*)
       CALL GETARG (K,FILEIN)
       OPEN (10,FILE=FILEIN,FORM='UNFORMATTED',STATUS='OLD',err=850)
@@ -43,16 +43,19 @@ C**** check for argument
       DO N=1,NTIMEACC
         TOT = TOT + TIMING(N)
       END DO
-      TOT = TOT/100.
-      IF (TOT.gt.0) THEN
-      WRITE (6,906) 24.*TOT/(60.*(Itime-Itime0)),(TIMESTR(N),
-     *     24.*TIMING(N)/(6000.*(Itime-Itime0)),N=1,3)
-      WRITE (6,907) (TIMESTR(N),
-     *     24.*TIMING(N)/(6000.*(Itime-Itime0)),N=4,NTIMEACC)
+      IF (Itime-Itime0.gt.0) THEN
+        FACT = 24./(6000*(Itime-Itime0))  
+      ELSE
+        FACT = 0.
       END IF
+      IF (QCMIN) THEN ! output in minutes
+        FAC = FACT
+      ELSE            ! output in percentages 
+        FAC = 100./TOT 
+      END IF
+      WRITE (6,906) FACT*TOT,(TIMESTR(N),FAC*TIMING(N),N=1,3)
+      WRITE (6,907) (TIMESTR(N),FAC*TIMING(N),N=4,NTIMEACC)
       IF (QCALL) THEN
-        write (6,*) "IM,JM,LM = ",IM,JM,LM," LS1 = ",LS1," P_cp = ",PTOP
-        write (6,*) "PLbot(mean values) = ",PTOP+PSFMPT*SIGE
         call print_param(6)
 c       write (6,*) "IDACC = ",(IDACC(I),I=1,12)
         call getdte(ItimeI,Nday,Iyear1,Jyear,Jmon,Jday,Jdate,Jhour,amon)
@@ -72,13 +75,13 @@ c       write (6,*) "IDACC = ",(IDACC(I),I=1,12)
       END DO
       Stop
   800 continue
-      write(6,*) " Usage: qc [-a] FILE1 [FILE2...]"
+      write(6,*) " Usage: qc [-a] [-t] FILE1 [FILE2...]"
       write(6,*) " Where FILE1,2.. are modelE files ",
      *     "(rsf/acc/fort.[12])"
       write(6,*) "  -a  output all header information, otherwise"
       write(6,*) "      only timing info is printed"
-c     write(6,*) " If first FILE argument is a directory, then ",
-c    *     "DIR/fort.[12] are opened"
+      write(6,*) "  -t  output absolute minutes for each sub-section,"
+      write(6,*) "      otherwise percentage time is printed"
       STOP
   850 continue
       write(6,*) "Cannot open file ",FILEIN
@@ -87,7 +90,7 @@ c    *     "DIR/fort.[12] are opened"
       write(6,*) "Error reading file ", FILEIN
       STOP
  900  FORMAT (I10,1X,I2,'/',I2,'/',I4,' hr',I2,2X,A)
- 906  FORMAT (' TIME',F7.2,' (MINUTES)',3(X,A12,F4.2))
- 907  FORMAT (10(22X,3(X,A12,F4.2) / ))
+ 906  FORMAT (' TIME',F7.2,' (MINUTES)',3(X,A12,F5.1))
+ 907  FORMAT (10(22X,3(X,A12,F5.1) / ))
       end
 
