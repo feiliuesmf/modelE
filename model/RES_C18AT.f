6f36d5704545e8e414ea4d9fda2ad2b97e2ac9b8
cdrar <cdrar>
2003-05-08 15:47:03 +0000

    Added Coarse grid (8x10) 18-layer resolution file (tropospheric model).

---
+++ model/RES_C18AT.f
@@ -0,0 +1,72 @@
+!@sum  RES_C18AT Resolution info for 18 layer, 8x10 non-strat model
+!@+    (Same as M12 for troposphere, 2 more layers between 150-10mb
+!@+     plus 4 more layers above 10mb, top at .1mb, NO GWdrag)
+!@auth Original Development Team
+!@ver  1.0
+
+      MODULE RESOLUTION
+!@sum  RESOLUTION contains horiz/vert resolution variables
+!@auth Original Development Team
+!@ver  1.0
+      IMPLICIT NONE
+      SAVE
+!@var IM,JM longitudinal and latitudinal number of grid boxes
+!@var LM number of vertical levels
+!@var LS1 Layers LS1->LM: constant pressure levels, L<LS1: sigma levels
+      INTEGER, PARAMETER :: IM=36,JM=24,LM=18, LS1=9
+
+!@var PSF,PMTOP global mean surface, model top pressure  (mb)
+!@var PTOP pressure at interface level sigma/const press coord syst (mb)
+      REAL*8, PARAMETER :: PSF=984.d0, PTOP = 150.d0, PMTOP = .1d0
+!@var PSFMPT,PSTRAT pressure due to troposhere,stratosphere
+      REAL*8, PARAMETER :: PSFMPT=PSF-PTOP, PSTRAT=PTOP-PMTOP
+
+!@var SIGE sigma levels at layer interfaces (1)
+      REAL*8, PARAMETER, DIMENSION(LM+1) :: SIGE = ((/
+     t     PSF,   934.d0,  854.d0,  720.d0,  550.d0,    ! Pbot L=1,5
+     t  390.d0,   285.d0,  210.d0,                      !      L=...
+     1    PTOP,                                         !      L=LS1
+     s  110.d0,  80.d0,   55.d0,   35.d0,    20.d0,     !      L=...
+     s  10.d0,    3.d0,    1.d0,     .3d0,   PMTOP /)   !      L=..,LM+1
+     *  - PTOP)/(PSF-PTOP)
+!!!!  Note:   sige(1)=1,  sige(ls1)=0,  sige(lm+1)=-pstrat/psfmpt
+
+!@var SIG,DSIG,byDSIG mid point, depth, 1/depth of sigma levels (1)
+      REAL*8, dimension(lm), parameter ::
+     &     SIG    = (sige(1:lm)+sige(2:lm+1))*0.5d0,
+     &     DSIG   =  sige(1:lm)-sige(2:lm+1),
+     &     byDSIG =  1./DSIG
+
+C**** KEP depends on whether stratos. EP flux diagnostics are calculated
+C**** If dummy EPFLUX is used set KEP=0, otherwise KEP=21
+!@param KEP number of lat/height E-P flux diagnostics
+      INTEGER, PARAMETER :: KEP=0
+
+C**** Based on model top, determine how much of stratosphere is resolved
+C****         PMTOP >= 10 mb,    ISTRAT = 0
+C**** 1 mb <= PMTOP <  10 mb,    ISTRAT = 1
+C****         PMTOP <   1 mb,    ISTRAT = 2
+      INTEGER, PARAMETER :: ISTRAT = 2
+
+      END MODULE RESOLUTION
+
+C**** The vertical resolution also determines whether
+C**** stratospheric wave drag will be applied or not.
+C**** Hence also included here are some dummy routines for non-strat
+C**** models.
+
+      SUBROUTINE DUMMY_STRAT
+!@sum DUMMY dummy routines for non-stratospheric models
+C**** Dummy routines in place of STRATDYN
+      ENTRY init_GWDRAG
+      ENTRY GWDRAG
+      ENTRY VDIFF
+      ENTRY io_strat
+C**** Dummy routines in place of STRAT_DIAG (EP flux calculations)
+C**** Note that KEP=0 is set to zero above for the dummy versions.
+      ENTRY EPFLUX
+      ENTRY EPFLXI
+      ENTRY EPFLXP
+C****
+      RETURN
+      END SUBROUTINE DUMMY_STRAT
