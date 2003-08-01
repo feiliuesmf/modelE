980bc7b8daa62eae024f2a453cf7bb789a2e7680
ialeinov <ialeinov>
2003-08-01 00:11:16 +0000

    There is no need to introduce special preprocessing case for TRACERS_SPECIAL_Shindell . Since the increase in the number of integer parameters is not that big, the change can be done for the entire model. The increase in the memory consumption is negligible. The PARAM.f was designed as a system module which basically should not depend on anytging else in the model.

--- model/shared/PARAM.f
+++ model/shared/PARAM.f
@@ -1,5 +1,3 @@
-#include "rundeck_opts.h"
-
       module param
 !@sum  param does all the bookkeeping for input parameters
 !@auth I. Aleinov
@@ -74,11 +72,7 @@
 
       integer, parameter :: MAX_PARAMS = 256
       integer, parameter :: MAX_RPARAMS = 128
-#ifdef TRACERS_SPECIAL_Shindell
       integer, parameter :: MAX_IPARAMS = 134
-#else
-      integer, parameter :: MAX_IPARAMS = 128
-#endif
       integer, parameter :: MAX_CPARAMS = 64
       integer, parameter :: MAX_NAME_LEN = 32
       integer, parameter :: MAX_CHAR_LEN = 64
