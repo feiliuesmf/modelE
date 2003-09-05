50b6ff1a1f3f1bdf71c0e2ee39488b2431ce5326
holoso <holoso>
2003-09-05 20:47:45 +0000

    1. Modified array and loop bounds 2. Switched to dynamic allocation for static arrays

--- model/CLOUDS_COM.f
+++ model/CLOUDS_COM.f
@@ -4,50 +4,105 @@
 !@auth M.S.Yao/T. Del Genio (modularisation by Gavin Schmidt)
 !@ver  1.0 (taken from CB265)
       USE MODEL_COM, only : IM,JM,LM
-
+      USE DOMAIN_DECOMP, only : grid
       IMPLICIT NONE
       SAVE
 !@var TTOLD,QTOLD previous potential temperature, humidity
-      REAL*8, DIMENSION(LM,IM,JM) :: TTOLD,QTOLD
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: TTOLD,QTOLD
 !@var SVLHX,SVLAT previous latent heat of evaporation
-      REAL*8, DIMENSION(LM,IM,JM) :: SVLHX,SVLAT
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: SVLHX,SVLAT
 !@var RHSAV previous relative humidity
-      REAL*8, DIMENSION(LM,IM,JM) :: RHSAV
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: RHSAV
 C**** some arrays here for compatility with new clouds
 !@var CLDSAV, CLDSAV1 previous cloud cover area (percent)
-      REAL*8, DIMENSION(LM,IM,JM) :: CLDSAV,CLDSAV1
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: CLDSAV,CLDSAV1
 !@var ULS,VLS,UMC,VMC velocity work arrays
-      REAL*8, DIMENSION(IM,JM,LM) :: ULS,VLS,UMC,VMC
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: ULS,VLS,UMC,VMC
 !@var TLS,QLS,TMC,QMC temperature and humidity work arrays
-      REAL*8, DIMENSION(IM,JM,LM) :: TLS,QLS,TMC,QMC
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: TLS,QLS,TMC,QMC
 !@var FSS grid fraction for large-scale clouds
 !@+   initialised as 1. for compatibility with previous clouds
-      REAL*8, DIMENSION(LM,IM,JM) :: FSS = 1. 
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: FSS
 
 C**** variables saved for radiation calculations
 !@var TAUSS optical depth from super-saturated clouds
-      REAL*8, DIMENSION(LM,IM,JM) :: TAUSS
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: TAUSS
 !@var TAUMC optical depth from moist-convective clouds
-      REAL*8, DIMENSION(LM,IM,JM) :: TAUMC
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: TAUMC
 !@var CLDSS super-saturated cloud cover area (percent)
-      REAL*8, DIMENSION(LM,IM,JM) :: CLDSS
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: CLDSS
 !@var CLDMC moist convective cloud cover area (percent)
-      REAL*8, DIMENSION(LM,IM,JM) :: CLDMC
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: CLDMC
 !@var CSIZMC,CSIZSS mc,ss effective cloud droplet radius (microns)
-      REAL*8, DIMENSION(LM,IM,JM) :: CSIZMC,CSIZSS
+      REAL*8, ALLOCATABLE, DIMENSION(:,:,:) :: CSIZMC,CSIZSS
 
 C**** variables saved for surface wind spectrum calculations
 !@var DDM1 downdraft mass flux / rho at lowest level (m/s)
-      REAL*8, DIMENSION(IM,JM) :: DDM1
+      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: DDM1
 
 C**** variables used (and saved) for gravity wave drag calculations
 !@var AIRX, AIRMX*DXYP(J) convective mass flux (kg/s)
-      REAL*8, DIMENSION(IM,JM) :: AIRX
+      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: AIRX
 !@var LMC max layer of mc convective mass flux. 
-      INTEGER, DIMENSION(2,IM,JM) :: LMC
+      INTEGER, ALLOCATABLE, DIMENSION(:,:,:) :: LMC
 
 !@var LLOW,LMID,LHI max levels for low, mid and high clouds
       INTEGER LLOW,LMID,LHI
+
+      CONTAINS
+
+      SUBROUTINE INIT_CLOUDS_COM(grid)
+!@sum  To allocate arrays whose sizes now need to be determined at
+!@+    run time
+!@auth NCCS (Goddard) Development Team
+!@ver  1.0
+      USE DOMAIN_DECOMP, ONLY : DYN_GRID
+      IMPLICIT NONE
+      TYPE (DYN_GRID), INTENT(IN) :: grid
+
+      INTEGER :: J_1H, J_0H
+      INTEGER :: IER
+
+      J_0H = grid%J_STRT_HALO
+      J_1H = grid%J_STOP_HALO
+
+      ALLOCATE(     TTOLD(LM,IM,J_0H:J_1H),
+     *              QTOLD(LM,IM,J_0H:J_1H),
+     *              SVLHX(LM,IM,J_0H:J_1H),
+     *              SVLAT(LM,IM,J_0H:J_1H),
+     *              RHSAV(LM,IM,J_0H:J_1H),
+     *             CLDSAV(LM,IM,J_0H:J_1H),
+     *            CLDSAV1(LM,IM,J_0H:J_1H),
+     *                FSS(LM,IM,J_0H:J_1H),
+     *              TAUSS(LM,IM,J_0H:J_1H),
+     *              TAUMC(LM,IM,J_0H:J_1H),
+     *              CLDSS(LM,IM,J_0H:J_1H),
+     *              CLDMC(LM,IM,J_0H:J_1H),
+     *             CSIZMC(LM,IM,J_0H:J_1H),
+     *             CSIZSS(LM,IM,J_0H:J_1H),
+     *         STAT=IER)
+
+      ALLOCATE(     ULS(IM,J_0H:J_1H,LM),
+     *              VLS(IM,J_0H:J_1H,LM),
+     *              UMC(IM,J_0H:J_1H,LM),
+     *              VMC(IM,J_0H:J_1H,LM),
+     *              TLS(IM,J_0H:J_1H,LM),
+     *              QLS(IM,J_0H:J_1H,LM),
+     *              TMC(IM,J_0H:J_1H,LM),
+     *              QMC(IM,J_0H:J_1H,LM),
+     *         STAT=IER)
+
+!@var FSS initialized to 1.
+      FSS = 1. 
+
+      ALLOCATE(     DDM1(IM,J_0H:J_1H),
+     *              AIRX(IM,J_0H:J_1H),
+     *         STAT=IER)
+
+      ALLOCATE(     LMC(2,IM,J_0H:J_1H),
+     *         STAT=IER)
+
+      END SUBROUTINE INIT_CLOUDS_COM
 
       END MODULE CLOUDS_COM
 
