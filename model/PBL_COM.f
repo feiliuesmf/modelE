ee096cc61c43cef5e3520c26f4eae5954bfe1e77
tclune <tclune>
2003-10-08 13:45:41 +0000

    Typical modifications for ESMF conversion.   - Modified loops over J   - Modified array bounds involving JM   - Added routine to dynamically allocate previously static module arrays     which have a JM dimension.

--- model/PBL_COM.f
+++ model/PBL_COM.f
@@ -15,24 +15,24 @@
 !@var vabl boundary layer profile for meridional wind
 !@var tabl boundary layer profile for temperature
 !@var qabl boundary layer profile for humidity
-      real*8, dimension(npbl,im,jm,4) :: uabl,vabl,tabl,qabl
+      real*8, allocatable, dimension(:,:,:,:) :: uabl,vabl,tabl,qabl
 !@var eabl boundary layer profile for turbulent KE (calc. on sec. grid)
-      real*8, dimension(npbl,im,jm,4) :: eabl
+      real*8, allocatable, dimension(:,:,:,:) :: eabl
 #ifdef TRACERS_ON
 !@var trabl boundary layer profile for tracers
-      real*8, dimension(npbl,ntm,im,jm,4) :: trabl
+      real*8, allocatable, dimension(:,:,:,:,:) :: trabl
 #endif
 
 !@var cmgs drag coefficient (dimensionless surface momentum flux)
 !@var chgs Stanton number   (dimensionless surface heat flux)
 !@var cqgs Dalton number    (dimensionless surface moisture flux)
-      real*8, dimension(im,jm,4) :: cmgs,chgs,cqgs
+      real*8, allocatable, dimension(:,:,:) :: cmgs,chgs,cqgs
 
 !@var ipbl flag for whether pbl properties were found at last timestep
-      integer, dimension(im,jm,4) :: ipbl
+      integer, allocatable, dimension(:,:,:) :: ipbl
 
 !@var ROUGHL log10(zgs/roughness length), prescribed with zgs=30 m.
-      REAL*8, dimension(im,jm) :: roughl
+      REAL*8, allocatable, dimension(:,:) :: roughl
 !@var WSAVG     COMPOSITE SURFACE WIND MAGNITUDE (M/S)
 !@var TSAVG     COMPOSITE SURFACE AIR TEMPERATURE (K)
 !@var QSAVG     COMPOSITE SURFACE AIR SPECIFIC HUMIDITY (1)
@@ -42,21 +42,21 @@
 !@var TAUAVG    COMPOSITE SURFACE MOMENTUM TRANSFER (TAU)
 !@var w2_l1     COMPOSITE vertical component of t.k.e. at gcm layer 1
 !@var USTAR_pbl friction velocity (sqrt of srfc mom flux) (m/s)
-      REAL*8, dimension(im,jm) ::
+      REAL*8, allocatable, dimension(:,:) ::
      &     wsavg,tsavg,qsavg,dclev,usavg,vsavg,tauavg,tgvavg,qgavg
      &    ,w2_l1
-      REAL*8, dimension(im,jm,4) :: ustar_pbl
+      REAL*8, allocatable, dimension(:,:,:) :: ustar_pbl
 
 !@var egcm  3-d turbulent kinetic energy in the whole atmosphere
 !@var w2gcm vertical component of egcm
 !@var t2gcm 3-d turbulent temperature variance in the whole atmosphere
-      real*8, dimension(lm,im,jm) :: egcm,w2gcm,t2gcm
+      real*8, allocatable, dimension(:,:,:) :: egcm,w2gcm,t2gcm
 
 !@var uflux surface turbulent u-flux (=-<uw>)
 !@var vflux surface turbulent v-flux (=-<vw>)
 !@var tflux surface turbulent t-flux (=-<tw>)
 !@var qflux surface turbulent q-flux (=-<qw>)
-      real*8, dimension(im,jm) :: uflux,vflux,tflux,qflux
+      real*8, allocatable, dimension(:,:) :: uflux,vflux,tflux,qflux
 
       END MODULE PBLCOM
 
@@ -149,4 +149,69 @@
  10   IOERR=1
       RETURN
       END SUBROUTINE io_bldat
+
+      SUBROUTINE ALLOC_PBL_COM(grid)
+!@sum  To allocate arrays whose sizes now need to be determined at
+!@+    run time
+!@auth NCCS (Goddard) Development Team
+!@ver  1.0
+      USE PBLCOM
+      USE DOMAIN_DECOMP, ONLY : DYN_GRID, GET
+      IMPLICIT NONE
+      TYPE (DYN_GRID), INTENT(IN) :: grid
+
+      INTEGER :: J_1H, J_0H
+      INTEGER :: IER
+
+C****
+C**** Extract useful local domain parameters from "grid"
+C****
+      CALL GET(grid, J_STRT_HALO=J_0H, J_STOP_HALO=J_1H)
+
+      ALLOCATE(    uabl(npbl,    im,J_0H:J_1H,4),
+     *             vabl(npbl,    im,J_0H:J_1H,4),
+     *             tabl(npbl,    im,J_0H:J_1H,4),
+     *             qabl(npbl,    im,J_0H:J_1H,4),
+     *             eabl(npbl,    im,J_0H:J_1H,4),
+     *         STAT=IER)
+
+#ifdef TRACERS_ON
+      ALLOCATE(    trabl(npbl,ntm,im,J_0H:J_1H,4),
+     *         STAT=IER)
+#endif
+
+      ALLOCATE(    cmgs(im,J_0H:J_1H,4),
+     *             chgs(im,J_0H:J_1H,4),
+     *             cqgs(im,J_0H:J_1H,4),
+     *             ipbl(im,J_0H:J_1H,4),
+     *         STAT=IER)
+
+      ALLOCATE(    roughl(im,J_0H:J_1H),
+     *              wsavg(im,J_0H:J_1H),
+     *              tsavg(im,J_0H:J_1H),
+     *              qsavg(im,J_0H:J_1H),
+     *              dclev(im,J_0H:J_1H),
+     *              usavg(im,J_0H:J_1H),
+     *              vsavg(im,J_0H:J_1H),
+     *             tauavg(im,J_0H:J_1H),
+     *             tgvavg(im,J_0H:J_1H),
+     *              qgavg(im,J_0H:J_1H),
+     *              w2_l1(im,J_0H:J_1H),
+     *         STAT=IER)
+
+      ALLOCATE(    ustar_pbl(im,J_0H:J_1H,4),
+     *         STAT=IER)
+
+      ALLOCATE(    egcm(lm,im,J_0H:J_1H),
+     *            w2gcm(lm,im,J_0H:J_1H),
+     *            t2gcm(lm,im,J_0H:J_1H),
+     *         STAT=IER)
+
+      ALLOCATE(   uflux(im,J_0H:J_1H),
+     *            vflux(im,J_0H:J_1H),
+     *            tflux(im,J_0H:J_1H),
+     *            qflux(im,J_0H:J_1H),
+     *         STAT=IER)
+
+      END SUBROUTINE ALLOC_PBL_COM
 
