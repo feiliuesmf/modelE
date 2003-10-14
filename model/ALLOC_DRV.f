99c0e2cdfb6aadf10c02d14d27e17ab9fb4b7c7d
tclune <tclune>
2003-10-14 19:35:55 +0000

    Added a call to Alloc_DIAG_LOC() for ESMF conversion

--- model/ALLOC_DRV.f
+++ model/ALLOC_DRV.f
@@ -1,3 +1,4 @@
+#include "rundeck_opts.h"
       subroutine alloc_drv()
 c Driver to allocate arrays that become dynamic as a result of
 c set-up for MPI implementation
@@ -12,6 +13,11 @@ c$$$      call alloc_fluxes(grid)
       call alloc_pbl_com(grid)
       call alloc_icedyn(grid)
       call alloc_icedyn_com(grid)
+c      call alloc_dagcom(grid)
+      call alloc_diag_loc(grid)
       call alloc_smomtq(grid)
+#if (defined TRACERS_ON) || (defined TRACERS_OCEAN)
+      call alloc_tracer_com(grid)
+#endif
 
       end subroutine alloc_drv
