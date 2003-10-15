11a12ab31f0ddf1d741b3ecebb8d4536959f8922
cdmsy <cdmsy>
2003-10-15 14:48:16 +0000

    Computed MC condensate detrainment after downdraft. Small differences in results are expected.

--- model/CLOUDS.f
+++ model/CLOUDS.f
@@ -959,24 +959,6 @@ C**** Add plume tracers at LMAX
       IF(LMCMIN.EQ.0) LMCMIN=LMIN
       IF(LMCMAX.LT.MAXLVL) LMCMAX=MAXLVL
 C****
-C**** Partition condensate into precipitation and cloud water
-C****
-      IF(PLE(LMIN)-PLE(LMAX+1).GE.450.) THEN
-        DO L=LMAX,LMIN,-1
-          IF(COND(L).LT.CONDP(L)) CONDP(L)=COND(L)
-          FCLW=0.
-          IF (COND(L).GT.0) FCLW=(COND(L)-CONDP(L))/COND(L)
-          SVWMXL(L)=SVWMXL(L)+FCLW*COND(L)*BYAM(L)
-          COND(L)=CONDP(L)
-#ifdef TRACERS_WATER
-C**** Apportion cloud tracers and condensation
-C**** Note that TRSVWML is in mass units unlike SVWMX
-          TRSVWML(1:NTX,L) = TRSVWML(1:NTX,L) + FCLW*TRCOND(1:NTX,L)
-          TRCOND(1:NTX,L) = (1.-FCLW)*TRCOND(1:NTX,L)
-#endif
-        END DO
-      END IF
-C****
 C**** PROCESS OF DOWNDRAFTING
 C****
       LDMIN=LMIN
@@ -1189,6 +1171,24 @@ C**** save new 'environment' profile for static stability calc.
 #ifdef TRACERS_ON
       TM1(:,1:NTX) = TM(:,1:NTX)
 #endif
+C****
+C**** Partition condensate into precipitation and cloud water
+C****
+      IF(PLE(LMIN)-PLE(LMAX+1).GE.450.) THEN
+        DO L=LMAX,LMIN,-1
+          IF(COND(L).LT.CONDP(L)) CONDP(L)=COND(L)
+          FCLW=0.
+          IF (COND(L).GT.0) FCLW=(COND(L)-CONDP(L))/COND(L)
+          SVWMXL(L)=SVWMXL(L)+FCLW*COND(L)*BYAM(L)
+          COND(L)=CONDP(L)
+#ifdef TRACERS_WATER
+C**** Apportion cloud tracers and condensation
+C**** Note that TRSVWML is in mass units unlike SVWMX
+          TRSVWML(1:NTX,L) = TRSVWML(1:NTX,L) + FCLW*TRCOND(1:NTX,L)
+          TRCOND(1:NTX,L) = (1.-FCLW)*TRCOND(1:NTX,L)
+#endif
+        END DO
+      END IF
 C****
 C**** REEVAPORATION AND PRECIPITATION
 C****
