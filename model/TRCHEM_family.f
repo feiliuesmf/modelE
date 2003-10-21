b5e04b91d85607b850b118389450bcfe86380e73
faluvegi <faluvegi>
2003-10-21 16:13:08 +0000

    Includes two new subroutines. Numerous changes that should only effect models that have SHINDELL_STRAT_CHEM defined.  The status of the strat chem is that it is now run-able, but still needs testing and tweaking of the chemistry. (except I still need to update rundecks with new input files.)

--- model/TRCHEM_family.f
+++ model/TRCHEM_family.f
@@ -1,3 +1,4 @@
+#include "rundeck_opts.h"
 c     Family chemistry calculations:
 c     equil values are production/loss from rxns within family only
 c     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -159,6 +160,10 @@ C
 c
 C**** GLOBAL parameters and variables:
 c
+      USE MODEL_COM, only : LM
+#ifdef SHINDELL_STRAT_CHEM
+     &      ,ptop,psf,sig
+#endif
       USE DYNAMICS, only: LTROPO
       USE TRACER_COM, only : n_CH4,n_HNO3,n_CH3OOH,n_H2O2,n_HCHO,n_CO,
      &                       n_Paraffin,n_Alkenes,n_Isoprene,n_AlkylNit
@@ -170,7 +175,7 @@ c
      &                           nO2,nM,nHO2,nOH,nH2,nAldehyde,nXO2,
      &                           nXO2N,ta,ss,nC2O3,nROR
 #ifdef SHINDELL_STRAT_CHEM
-     &                           ,nBrO,nClO,nOClO,nBr,nCl,SF3
+     &                           ,nBrO,nClO,nOClO,nBr,nCl,SF3,nO
 #endif
 C
       IMPLICIT NONE
@@ -193,6 +198,16 @@ c
       integer, parameter :: iH2O2form=96,iHNO3form=97,iHONOform=100
 #else
       integer, parameter :: iH2O2form=49,iHNO3form=50,iHONOform=53
+#endif
+#ifdef SHINDELL_STRAT_CHEM
+!@var PRES local nominal pressure for regional Ox tracers
+      REAL*8, DIMENSION(LM) :: PRES
+#endif
+c
+#ifdef SHINDELL_STRAT_CHEM
+      do L=1,LM
+       PRES(L)=SIG(L)*(PSF-PTOP)+PTOP
+      end do
 #endif
 C
 cc    Troposphere
@@ -283,7 +298,7 @@ C
      & *y(nNO2,L)+rr(iHONOform,L)*y(nNO,L))+rr(22,L)*yCH3O2(I,J,L)
      &  +rr(52,L)*y(n_HCl,L)*pHOx(I,J,L)+rr(53,L)*y(n_HOCl,L)
      &  *pHOx(I,J,L)+rr(56,L)*y(nOClO,L)*pHOx(I,J,L)+
-     &  rr(59,L)*y(nCl,L)+
+     &  rr(59,L)*y(nCl,L)
      &  +rr(62,L)*y(nClO,L)*pHOx(I,J,L)+rr(63,L)*y(nClO,L)
      &  +rr(68,L)*y(n_HBr,L)*pHOx(I,J,L)+rr(72,L)*y(nBr,L)
      &  +rr(73,L)*y(nBrO,L)+rr(81,L)*y(nBrO,L)*pHOx(I,J,L)
@@ -303,7 +318,7 @@ c     &  rr(10,L)*y(nH2O,L)+rr(11,L)*y(n_CH4,L))
      &  +SF3(I,J,L)*pHOx(I,J,L) !water vapor photolysis in SRBs
 
 c      production from O1D limited to O1D amount
-       rcqqz=rr(8,L)*y(n_O2,L)+rr(9,L)*y(nM,L)+
+       rcqqz=rr(8,L)*y(nO2,L)+rr(9,L)*y(nM,L)+
      & rr(10,L)*y(nH2O,L)+rr(11,L)*y(n_CH4,L)
        if(rcqqz.gt.1)then
         ratio=1./rcqqz
@@ -353,10 +368,10 @@ c
        y(nHO2,L)=(temp_yHOx-y(nOH,L))
 
 c      At low pressures, include loss of OH into H
-       if(L.gt.LM-4)then
+       if(PRES(L).lt.2.d0)then
         rHprod=rr(89,L)*y(nOH,L)*y(nO,L)
         rHspecloss=y(nO3,L)*1.4E-10*exp(-470./ta(L))
-        rkzero=y(nM,L)*5.7d-32*((ta(L)/300)**-1.6)
+        rkzero=y(nM,L)*5.7d-32*((ta(L)/300)**(-1.6))
         rktot=(rkzero/(1+(rkzero/7.5d-11)))
         rHspecloss=rHspecloss+y(nO2,L)*rktot
         y(nOH,L)=y(nOH,L)-rHprod/rHspecloss
@@ -375,3 +390,276 @@ c
 c
       return
       END SUBROUTINE HOxfam
+c
+C
+      SUBROUTINE ClOxfam(lmax,I,J,ClOx_old)
+c
+#ifdef SHINDELL_STRAT_CHEM
+c
+!@sum ClOxfam Find ClOx family (Cl,ClO,OClO,Cl2,Cl2O2) partitioning 
+!@+   assuming equilibrium within ClOx.
+!@auth Drew Shindell
+!@ver  1.0 (based on ds4p_famchem_M23)
+c
+C**** GLOBAL parameters and variables:
+c
+      USE MODEL_COM, only :  LM,ls1
+      USE TRACER_COM, only : n_ClOx,n_HOCl,n_ClONO2,n_HCl,n_H2O2,n_CH4
+      USE TRCHEM_Shindell_COM, only:pClOx,rr,y,nClO,nOClO,nCl,nCl2O2,
+     &    ta,ss,nO3,nHO2,nNO3,nO,nNO,nBr,nOH,nBrO,nCH3O2,nM,nCl2,nH2,
+     &    SZA,dt2,pClx,pOClOx,nNO2
+C
+      IMPLICIT NONE
+c
+C**** Local parameters and variables and arguments:
+!@var A,B,C,D,F,G,Q,V,X,YY,dClOx,ww,p1,p2,p3,dOClO,ratioc dummy vars
+!@var ClOx_old total ClOx at start of chemical timestep
+!@var rnormnum is temporary var for conservation of Cl
+!@var destCl, prodCl temporary vars for simple steady state Cl calc
+      REAL*8, DIMENSION(LM)      :: ClOx_old 
+      real*8 A,B,C,D,F,G,Q,V,X,YY,dClOx,ww,p1,p2,p3,dOClO,ratioc,
+     &rnormnum,destCl,prodCl
+      integer L
+      integer, intent(IN) :: lmax,I,J
+
+      do L=LS1,lmax
+
+c      full ClOxfam code from offline photochemistry
+c       goto 10
+c       if(y(n_ClOx,L).gt.0.6E-9*y(nM,L))y(n_ClOx,L)=0.6d-9*y(nM,L)
+       y(nClO,L)=y(n_ClOx,L)*pClOx(I,J,L)
+       y(nOClO,L)=y(n_ClOx,L)*pOClOx(I,J,L)
+       y(nCl,L)=y(n_ClOx,L)*pClx(I,J,L)
+       if(y(n_ClOx,L).eq.0)goto50
+
+c      low temperature stabilizes ClO dimer, use [Cl2O2] only for
+c      calculating Cl amount, otherwise ignore
+       if(ta(L).le.210)then
+        y(nCl2O2,L)=(rr(102,L)*y(nClO,L)*y(nClO,L))/
+     * (rr(93,L)+ss(20,L,i,j))
+        if(y(nCl2O2,L).gt.y(nClO,L))y(nCl2O2,L)=y(nClO,L)
+        y(nClO,L)=y(nClO,L)-y(nCl2O2,L)
+       endif
+
+       A=y(nO3,L)*rr(45,L)+y(nOClO,L)*rr(47,L)+y(nHO2,L)*
+     *   rr(60,L)+y(nNO3,L)*rr(66,L)
+       B=y(nCl,L)*rr(47,L)+y(nO,L)*rr(50,L)+y(nNO,L)*
+     *   rr(67,L)+y(nBr,L)*rr(74,L)+ss(19,L,i,j)
+       C=y(nO,L)*rr(46,L)+y(nO3,L)*(rr(48,L)+rr(49,L))+
+     *   y(nOH,L)*rr(61,L)+y(nNO,L)*rr(64,L)+y(nBrO,L)*
+     *   (rr(75,L)+rr(76,L))+ss(17,L,i,j)+rr(85,L)*
+     *   y(nCH3O2,L)
+       D=y(nO3,L)*rr(49,L)+y(nBrO,L)*rr(75,L)
+       F=(rr(53,L)*y(nOH,L)*y(n_HOCl,L)+rr(55,L)*y(nO,L)*
+     *   y(n_HOCl,L)+rr(65,L)*y(n_ClONO2,L)*y(nO,L)+rr(93,L)*
+     *   y(nCl2O2,L)*y(nM,L)*2)/y(n_ClOx,L)
+       G=rr(62,L)*y(nOH,L)+rr(63,L)*y(nHO2,L)+rr(77,L)*
+     *   y(nBrO,L)+2*rr(102,L)*y(nClO,L)+rr(103,L)*y(nNO2,L)
+       Q=rr(56,L)*y(nOH,L)
+       V=C-D
+       X=rr(51,L)*y(nOH,L)*y(nCl2,L)+rr(54,L)*y(nO,L)*
+     *   y(n_HCl,L)+
+     *   2*ss(18,L,i,j)*y(nCl2,L)+2*ss(20,L,i,j)*y(nCl2O2,L)+
+     *   ss(21,L,i,j)*y(n_HOCl,L)+ss(22,L,i,j)*y(n_ClONO2,L)
+       X=X/y(n_ClOx,L)
+       YY=rr(57,L)*y(n_HOCl,L)+rr(58,L)*y(n_H2O2,L)+rr(59,L)*
+     *   y(nHO2,L)+rr(82,L)*y(n_CH4,L)+rr(83,L)*y(nH2,L)
+       if((dt2*y(n_ClOx,L)).ne.0)then
+        dClOx=(y(n_ClOx,L)-ClOx_old(L))/(dt2*y(n_ClOx,L))
+       else
+        dClOx=0.
+       endif
+       ww=A+yy+dClOx
+
+         if(v.ne.0)then 
+          p1=(-(ww/v)*(B+C+G+dClOx))+A-B
+          if(p1.ne.0)then
+           p1=abs(-((x/v)*(B+C+G+dClOx)+F+B)/p1)
+           if((B+D-Q-dClOx).ne.0)then
+            p3=abs((-p1*D+D)/(B+D-Q-dClOx))
+           else 
+            p3=0.  
+           endif   
+           p2=1-p1-p3
+          else 
+           p2=1. 
+           p1=0.
+           p3=0.  
+          endif  
+         else 
+          p2=1. 
+          p1=0.
+          p3=0.  
+         endif          
+       
+       if(SZA.gt.90)then
+        dOClO=(y(nClO,L)*D-y(nOClO,L)*(B+Q))*dt2
+        y(nOClO,L)=y(nOClO,L)+dOClO
+        if(y(nOClO,L).lt.0.)y(nOClO,L)=0.0
+       endif 
+       
+c       if(SZA.lt.93)then       
+          y(nCl,L)=p1*y(n_ClOx,L)
+c         faction of Cl recycled within ClOx vs other paths
+c          ratioC=(y(nO3,L)*rr(45,L)+y(nOClO,L)*rr(47,L)+
+c     *    y(nHO2,L)*rr(60,L))
+c          ratioC=ratioC/(ratioC+y(n_HOCl,L)*rr(57,L)+
+c     *    y(n_H2O2,L)*rr(58,L)+y(nHO2,L)*rr(59,L)
+c     *    +y(n_CH4,L)*rr(82,L)+y(nH2,L)*rr(83,L))
+c          y(nCl,L)=y(nCl,L)*ratioC
+          y(nClO,L)=p2*y(n_ClOx,L)
+c         else 
+cc          y(nCl,L)=0.
+c        dClO=(
+cc     *   y(nCl,L)*A+
+c     *   y(nOClO,L)*B+y(nClOx,L)*F-y(nClO,L)*(C+G))*dt2
+c          y(nClO,L)=y(nClO,L)+dClO   
+c          y(nCl,L)=y(n_ClOx,L)-(y(nClO,L)+y(nOClO,L))
+c         endif   
+       if(SZA.lt.90)y(nOClO,L)=p3*y(n_ClOx,L)  
+         if(y(nCl,L).lt.0)y(nCl,L)=0.0
+         if(y(nClO,L).lt.1)y(nClO,L)=0.0
+         if(y(nOClO,L).lt.1)y(nOClO,L)=0.0
+         if(y(n_ClOx,L).lt.1)y(n_ClOx,L)=1.0
+           
+c      write(*,*) ' L, Cl, ClO, OClO, ClOx =',L,y(nCl,L),
+c     * y(nClO,L),y(nOClO,L),y(nClOx,L)
+
+c      Normalize so that amount of ClOx doesn't change
+       if((y(nCl,L)+y(nClO,L)+y(nOClO,L)).gt.0)then
+          rnormnum=y(n_ClOx,L)/(y(nCl,L)+y(nClO,L)+y(nOClO,L))
+          y(nCl,L)=y(nCl,L)*rnormnum
+          y(nClO,L)=y(nClO,L)*rnormnum
+          y(nOClO,L)=y(nOClO,L)*rnormnum
+       endif
+
+c      if(J.eq.jprn.and.I.eq.iprn.and.L.eq.lprn)write(*,*)
+c     & 'ClOx fam: Cl, ClO, NO, NOx, rxn coeff ClO+NO',
+c     & y(nCl,L),y(nClO,L),y(nNO,L),y(nNOx,L),
+c     & rr(64,L),ww,v,B,C,G,dClOx,A,x,F,p1
+c      if(J.eq.jprn.and.I.eq.iprn.and.L.eq.lprn)write(*,*)
+c     & 'A: ',y(nO3,L),rr(45,L),y(nOClO,L),rr(47,L),y(nHO2,L),
+c     *   rr(60,L),y(nNO3,L),rr(66,L)
+
+       pClOx(I,J,L)=y(nClO,L)/y(n_ClOx,L)
+       pClx(I,J,L)=y(nCl,L)/y(n_ClOx,L)
+       pOClOx(I,J,L)=y(nOClO,L)/y(n_ClOx,L)
+
+c      Temporarily, no het chem, Cl2 and Cl2O2 both zero.
+c       y(nCl2O2,L)=0.
+       y(nCl2,L)=0. !het rxns set to produce Cl instead of Cl2
+
+       goto 50 !skip alternative method
+
+ 10    continue
+cc     simple Cl/ClO calculation
+       if(y(n_ClOx,L).eq.0)goto50
+       destCl=y(nO3,L)*rr(45,L)+y(nHO2,L)*rr(60,L)
+     *    +y(n_HOCl,L)*rr(57,L)+
+     *    y(n_H2O2,L)*rr(58,L)+y(nHO2,L)*rr(59,L)
+     *    +y(n_CH4,L)*rr(82,L)+y(nH2,L)*rr(83,L)
+       prodCl=(y(nO,L)*rr(46,L)+y(nO3,L)*rr(48,L)+
+     *   y(nOH,L)*rr(61,L)+y(nNO,L)*rr(64,L)+ss(17,L,i,j))*
+     *   y(n_ClOx,L)
+
+c      if(J.eq.jprn.and.I.eq.iprn.and.L.eq.lprn)write(*,*)
+c     * 'p diags: ',y(nO,L)*rr(46,L),y(nO3,L)*rr(48,L),
+c     *   y(nOH,L)*rr(61,L),y(nNO,L)*rr(64,L),ss(17,L,i,j)
+c      if(J.eq.jprn.and.I.eq.iprn.and.L.eq.lprn)write(*,*)
+c     * 'l diags: ',y(nO3,L)*rr(45,L),y(nHO2,L)*rr(60,L)
+c     *    ,y(n_HOCl,L)*rr(57,L),
+c     *    y(n_H2O2,L)*rr(58,L),y(nHO2,L)*rr(59,L)
+c     *    ,y(n_CH4,L)*rr(82,L),y(nH2,L)*rr(83,L)
+
+       y(nCl,L)=prodCl/destCl
+       y(nClO,L)=y(n_ClOx,L)-y(nCl,L)
+
+c       p2=1./((C/A)+1.)
+c       p1=1.-p2
+c       y(nCl,L)=p1*y(n_ClOx,L)
+c       y(nClO,L)=p2*y(n_ClOx,L)
+
+c      Normalize so that amount of ClOx doesn't change
+       if((y(nCl,L)+y(nClO,L)).gt.0)then
+          rnormnum=y(n_ClOx,L)/(y(nCl,L)+y(nClO,L))
+          y(nCl,L)=y(nCl,L)*rnormnum
+          y(nClO,L)=y(nClO,L)*rnormnum
+       endif
+
+       pClOx(I,J,L)=y(nClO,L)/y(n_ClOx,L)
+       pClx(I,J,L)=y(nCl,L)/y(n_ClOx,L)
+       pOClOx(I,J,L)=0.
+
+c      Temporarily, no het chem, Cl2 and Cl2O2 both zero.
+       y(nCl2O2,L)=0.
+       y(nCl2,L)=0.
+       y(nOClO,L)=0.
+
+ 50   enddo  ! end of altitude loop
+#endif
+      return
+      END SUBROUTINE ClOxfam
+C
+c     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+C
+      SUBROUTINE BrOxfam(lmax,I,J)
+c
+#ifdef SHINDELL_STRAT_CHEM
+c
+!@sum BrOxfam Find BrOx family (Br,BrO) partitioning 
+!@+   assuming equilibrium within BrOx.
+!@auth Drew Shindell
+!@ver  1.0 (based on ds4p_famchem_M23)
+c
+C**** GLOBAL parameters and variables:
+c
+      USE MODEL_COM, only :  ls1
+      USE TRACER_COM, only : n_BrOx,n_H2O2,n_HBr,n_HOBr,n_BrONO2
+      USE TRCHEM_Shindell_COM, only:rr,y,nO3,nClO,nOClO,nNO,nO,nBr,nOH,
+     &    nBrO,ss,nHO2,nNO2,pBrOx
+C
+      IMPLICIT NONE
+c
+C**** Local parameters and variables and arguments:
+!@var a,b,c,d,eq,f,p2,p1 dummy vars
+      real*8 a,b,c,d,eq,f,p2,p1
+      integer L
+      integer, intent(IN) :: lmax,I,J
+
+      do L=LS1,lmax
+         a=y(nO3,L)*rr(70,L)+y(nOClO,L)*rr(74,L)
+         b=y(nO,L)*rr(69,L)+y(nNO,L)*rr(71,L)+y(nClO,L)*
+     *   (rr(75,L)+rr(76,L))+2*y(nBrO,L)*rr(78,L)+y(nOH,L)*
+     *   rr(80,L)+ss(25,L,i,j)
+       c=rr(73,L)*y(nHO2,L)+rr(77,L)*y(nClO,L)+rr(81,L)*
+     *   y(nOH,L)+rr(104,L)*y(nNO2,L)    
+       d=rr(72,L)*y(nHO2,L)+rr(79,L)*y(n_H2O2,L)
+       eq=rr(68,L)*y(n_HBr,L)*y(nOH,L)+rr(84,L)*y(n_HBr,L)*
+     *  y(nO,L)+ss(24,L,i,j)*y(n_HOBr,L)
+       f=ss(23,L,i,j)*y(n_BrONO2,L)
+       if(a+b.ne.0)then
+        p2=a/(a+b)
+       else
+        p2=0.
+       endif
+       if(p2.lt.0)p2=0.
+       if(p2.gt.1)p2=1.
+         p1=1-p2    
+         y(nBr,L)=p1*y(n_BrOx,L)
+         y(nBrO,L)=p2*y(n_BrOx,L)
+         if(y(nBr,L).lt.1)y(nBr,L)=0.0
+         if(y(nBrO,L).lt.1)y(nBrO,L)=0.0
+         if(y(nBr,L).gt.1d9)y(nBr,L)=0.0
+         if(y(nBrO,L).gt.1d9)y(nBrO,L)=0.0
+         if(y(n_BrOx,L).lt.1)y(n_BrOx,L)=1.0
+c       write(*,*) ' L, Br, BrO, BrOx =',L,y(nBr,L),y(nBrO,L),
+c     * y(n_BrOx,L)
+
+       pBrOx(I,J,L)=y(nBrO,L)/y(n_BrOx,L)
+      enddo ! end of altitude loop
+#endif
+c
+      return
+      END SUBROUTINE BrOxfam
+
+
