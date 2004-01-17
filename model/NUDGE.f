135479f3e04ad9c92fe1b4272185c7fb45377f7d
sbauer <sbauer>
2004-01-17 00:23:50 +0000

    cleaned up version - finally successfully working

--- model/NUDGE.f
+++ model/NUDGE.f
@@ -7,18 +7,18 @@ c *****************************************************************
 c      USE DOMAIN_DECOMP, only : grid
       IMPLICIT NONE
       SAVE
-!@var  U1, V1 NCEP U wind at prior ncep timestep (m/s)
-      REAL*4, DIMENSION(IM,JM,LM) :: u1,v1
-!@var  U2, V2 NCEP U wind at the following ncep timestep (m/s)
-c      REAL*4, DIMENSION(IM,grid%J_STRT_HALO:grid%J_STOP_HALO,LM) :: U2,V2
-      REAL*4, DIMENSION(IM,JM,LM) :: u2,v2
+!@param  nlevnc vertical levels of NCEP data  
+      INTEGER, PARAMETER :: nlevnc =17
+!@var  U1, V1 NCEP wind at prior ncep timestep (m/s)
+!@var  U2, V2 NCEP wind at the following ncep timestep (m/s)
+      REAL*4, DIMENSION(IM,JM,LM) :: u1,v1,u2,v2
+      REAL*4, DIMENSION(IM,JM-1,nlevnc) :: UN1,VN1,UN2,VN2
+      REAL*4, DIMENSION(nlevnc) :: pl
 !@var netcdf integer
       INTEGER :: ncidu,ncidv,uid,vid,plid
       INTEGER :: step_rea=1,zirk=0
 !@var tau nuding time interpoltation
       REAL*8 :: tau
-!@param  nlevnc vertical levels of NCEP data  
-      INTEGER, PARAMETER :: nlevnc =17
 !@param  anudgeu anudgev relaxation constant
       REAL*8 :: anudgeu = 0., anudgev = 0.
 
@@ -28,18 +28,16 @@ c------------------------------------------------------------------
       SUBROUTINE NUDGE(UGCM,VGCM,DTSTEP)
 c******************************************************************
 !@sum  Nudging of the horizontal wind comonents to reanalysis data sets
-!@auth Susanne
+!@auth Susanne Bauer
 !@ver  
-      USE MODEL_COM, only : im,jm,lm
+      USE MODEL_COM, only : im,jm,lm,plbot
       USE DOMAIN_DECOMP, only : grid
-      USE NUDGE_COM, only : u1,v1,u2,v2,tau,anudgeu,anudgev
+      USE NUDGE_COM, only : u1,v1,u2,v2,tau,anudgeu,anudgev,pl,nlevnc
       IMPLICIT NONE
-
       REAL*8,DIMENSION(IM,JM,LM) ::  ugcm,vgcm
-
 c     LOCAL
       INTEGER i,j,l
-      REAL   alphau,alphav,a,dtstep
+      REAL*8   alphau,alphav,a,dtstep
 
 c - - - - - - - - - - - - - - - - - - - - - - - - - |
 c   x_gcm = a * x_gcm + ( 1 - a ) * x_reanalys      |
@@ -48,26 +46,16 @@ C----------------------------------------------------
              alphav=dtstep * anudgev  !alphav !nudgev
 
          do l=1,lm
-            if(l.gt.17) then
-               alphau=0.
-               alphav=0.
-             endif
-             if(l.gt.12) then
-               alphau=alphau * EXP(11.-l)
-               alphav=alphav * EXP(11.-l)
-             endif   
+           if (plbot(l+1).lt.pl(nlevnc)) cycle
          do j=2,jm  ! Please pay attention j starts at 2
          do i=1,im
             a=(1.-tau)*u1(i,j,l)+tau*u2(i,j,l)         !time interpolation
-c            ugcm(i,j,l)=alphau*ugcm(i,j,l)+(1-alphau)*a 
             ugcm(i,j,l) = (ugcm(i,j,l)+ (a * alphau))/ (1+alphau) !nudging
             a=(1.-tau)*v1(i,j,l)+tau*v2(i,j,l)         !time interpolation
-c            vgcm(i,j,l)=alphav*vgcm(i,j,l)+(1-alphav)*a 
             vgcm(i,j,l) = (vgcm(i,j,l)+ (a * alphav))/ (1+alphav) !nudging
          enddo
          enddo
          enddo
-
 
       RETURN
       END SUBROUTINE NUDGE
@@ -82,126 +70,79 @@ c******************************************************************
       USE NUDGE_COM
       IMPLICIT NONE
       include 'netcdf.inc'
-      INTEGER i,B
+      INTEGER i
       integer start(4),count(4),status
+      character(len=3) :: nstr1,nstr2
 C-----------------------------------------------------------------------
 
       if (jday.eq.1.and.mod(itime,nday).eq.0) then
             zirk = jyear - iyear1
       endif 
-      if (step_rea.eq.1464) then
-                  zirk = jyear - iyear1 + 1
-      endif
-      B=0
-      if (jyear.eq.1996.or.jyear.eq.2000) B=1
-      if (step_rea.eq.1460.and.B.ne.1) then
-                  zirk = jyear - iyear1 + 1
-      endif
+      if (step_rea.eq.1460.) then
+            zirk = jyear - iyear1 + 1
+c      endif
 c -----------------------------------------------------------------
 c   Opening of the files to be read
 c -----------------------------------------------------------------
 
-      if(zirk.eq.1) then
-            status=NF_CLOSE('u.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v.nc',NCNOWRIT,ncidv)
+        if (zirk.lt.10) then
+           write(nstr1,'(I1)') zirk
+        elseif (zirk.lt.100) then
+           write(nstr1,'(I2)') zirk
+        endif   
 
-            status=NF_OPEN('u1.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v1.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.2) then
-            status=NF_CLOSE('u1.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v1.nc',NCNOWRIT,ncidv)
+        if (zirk+1.lt.10) then
+           write(nstr2,'(I1)') zirk +1
+        elseif (zirk+1.lt.100) then
+           write(nstr2,'(I2)') zirk + 1
+        endif   
 
-            status=NF_OPEN('u2.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v2.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.3) then
-            status=NF_CLOSE('u2.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v2.nc',NCNOWRIT,ncidv)
+      print*, '  I N    N U D G E : OPEN NF FILES','  u',nstr2,'.nc'
 
-            status=NF_OPEN('u3.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v3.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.4) then
-            status=NF_CLOSE('u3.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v3.nc',NCNOWRIT,ncidv)
+            status=NF_CLOSE('u'//trim(nstr1)//'.nc',NCNOWRIT,ncidu)
+            status=NF_CLOSE('v'//trim(nstr1)//'.nc',NCNOWRIT,ncidv)
 
-            status=NF_OPEN('u4.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v4.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.5) then
-            status=NF_CLOSE('u4.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v4.nc',NCNOWRIT,ncidv)
+            status=NF_OPEN('u'//trim(nstr2)//'.nc',NCNOWRIT,ncidu)
+            status=NF_OPEN('v'//trim(nstr2)//'.nc',NCNOWRIT,ncidv)
 
-            status=NF_OPEN('u5.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v5.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.6) then
-            status=NF_CLOSE('u5.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v5.nc',NCNOWRIT,ncidv)
-
-            status=NF_OPEN('u6.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v6.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.7) then
-            status=NF_CLOSE('u6.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v6.nc',NCNOWRIT,ncidv)
-
-            status=NF_OPEN('u7.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v7.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.8) then
-            status=NF_CLOSE('u7.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v7.nc',NCNOWRIT,ncidv)
-
-            status=NF_OPEN('u8.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v8.nc',NCNOWRIT,ncidv)
-      endif     
-      if(zirk.eq.9) then
-            status=NF_CLOSE('u8.nc',NCNOWRIT,ncidu)
-            status=NF_CLOSE('v8.nc',NCNOWRIT,ncidv)
-
-            status=NF_OPEN('u9.nc',NCNOWRIT,ncidu)
-            status=NF_OPEN('v9.nc',NCNOWRIT,ncidv)
-      endif      
-            status=NF_INQ_VARID(ncidu,'uwnd',uid)
             status=NF_INQ_VARID(ncidu,'level',plid)
+            status=NF_INQ_VARID(ncidu,'uwnd',uid)
             status=NF_INQ_VARID(ncidv,'vwnd',vid)
 
+        endif  ! step = 1460
 C-----------------------------------------------------------------------
       if (mod(itime,nday/4).eq.0.) then
-           v1(:,:,:) = v2(:,:,:)
-           u1(:,:,:) = u2(:,:,:)
+           vn1(:,:,:) = vn2(:,:,:)
+           un1(:,:,:) = un2(:,:,:)
            step_rea = INT( (((jday - 1) * 24) + jhour)/6) + 2
-           if (step_rea.eq.1461.and.B.ne.1) step_rea = 1
-           if (step_rea.gt.1464)            step_rea = 1
-c         print*,'READING REANALYSIS AT ',jday, jhour, step_rea
-           call read_ana(u2,v2,step_rea)
+            if (step_rea.eq.1461.) step_rea = 1
+           call read_ana(step_rea)
       endif
 C-----------------------------------------------------------------------
       tau  = mod(itime,nday/4)/float(nday/4)
+
+      call vinterana2mod(un1,nlevnc,pl,u1)
+      call vinterana2mod(vn1,nlevnc,pl,v1)
+      call vinterana2mod(un2,nlevnc,pl,u2)
+      call vinterana2mod(vn2,nlevnc,pl,v2)
 
       RETURN
       END SUBROUTINE NUDGE_PREP
 
    
 c -----------------------------------------------------------------
-      SUBROUTINE READ_ANA(UN,VN,timestep)
+      SUBROUTINE READ_ANA(timestep)
 c******************************************************************
 !@sum  read in analysis data sets
 !@auth Susanne Bauer
 !@ver  
       USE MODEL_COM, only : im,jm,lm
-      USE NUDGE_COM, only : nlevnc,ncidu,ncidv,uid,vid,plid
+      USE NUDGE_COM
       USE DOMAIN_DECOMP, only : grid
       IMPLICIT NONE
       include 'netcdf.inc'
 
       integer timestep,l
-
-      REAL*4 U(IM,JM-1,nlevnc),V(IM,JM-1,nlevnc),PL(nlevnc)
-      REAL*4, DIMENSION(IM,JM,LM) :: UN,VN
-
       integer start(4),count(4),status
 
 c -----------------------------------------------------------------
@@ -219,11 +160,9 @@ c -----------------------------------------------------------------
       count(4)=1
 
 c  u
-      status=NF_GET_VARA_REAL(ncidu,uid,start,count,u)
+      status=NF_GET_VARA_REAL(ncidu,uid,start,count,un2)
 c  v
-      status=NF_GET_VARA_REAL(ncidv,vid,start,count,v)
-      call vinterana2mod(u,nlevnc,pl,un)
-      call vinterana2mod(v,nlevnc,pl,vn)
+      status=NF_GET_VARA_REAL(ncidv,vid,start,count,vn2)
 
       return
       end subroutine read_ana
@@ -247,28 +186,36 @@ c ==============
         real po(lmo)! pressure levels in millibars of the input
         REAL varo(im,jm-1,lmo)!  Variable on the old grid (input)
         REAL varn(im,jm,lm) !  Variable on the new grid (output)
-        real coef
+        real coef,dp1,dp2
+ 
+        do j= 2,jm              ! Please pay attention j starts at 2
+          do i=1,im
+          do ln=1,lm
 
-        do i=1,im
-        do j= 2,jm     ! Please pay attention j starts at 2
-           do ln=1,lm
-              if (pmid(ln,i,j).ge.po(1))then
-                 varn(i,j,ln) =  varo(i,j-1,1)
-              else if (pmid(ln,i,j).le.po(lmo)) then
-                 varn(i,j,ln) =  varo(i,j-1,lmo)
+            lo=1
+            if (pmid(ln,i,j).ge.po(1))then
+              varn(i,j,ln) =  varo(i,j-1,1)
+            else if (pmid(ln,i,j).le.po(lmo)) then
+              varn(i,j,ln) =  varo(i,j-1,lmo)
+            else
+ 10           if ( (pmid(ln,i,j).le.po(lo)).and.
+     &             (pmid(ln,i,j).gt.po(lo+1)) )then
+c                coef=(pmid(ln,i,j)-po(lo))/(po(lo+1)-po(lo))
+c                varn(i,j,ln)=varo(i,j-1,lo)
+c     &               +coef*(varo(i,j-1,lo+1)-varo(i,j-1,lo))
+
+                dp1 = (-pmid(ln,i,j)+po(lo))
+                dp2 = (pmid(ln,i,j)-po(lo+1))
+
+                varn(i,j,ln)=((varo(i,j-1,lo) * dp1)
+     &               + (varo(i,j-1,lo+1) *dp2)) / (dp1 + dp2)
               else
-                 do lo=1,lmo-1 
-                    if ( (pmid(ln,i,j).le.po(lo)).and.
-     &                 (pmid(ln,i,j).gt.po(lo+1)) )then
-                       coef=(pmid(ln,i,j)-po(lo))
-     &                 /(po(lo+1)-po(lo))
-                       varn(i,j,ln)=varo(i,j-1,lo)
-     &                 +coef*(varo(i,j-1,lo+1)-varo(i,j-1,lo))
-                    end if
-                 enddo           
-              endif
-           enddo
+                lo=lo+1
+                if (lo.le.lmo) goto 10
+              end if
+            endif
 
+          enddo
         enddo
         enddo
 
@@ -280,15 +227,14 @@ c******************************************************************
 !@sum  Initialization for Nudging
 !@auth Susanne
 !@ver  
-      USE MODEL_COM, only : im,jm,lm,jhour,jday
+      USE MODEL_COM, only : im,jm,lm,jhour,jday,itime,nday,jyear,iyear1
       USE NUDGE_COM
       USE PARAM
       IMPLICIT NONE
       include 'netcdf.inc'
 
-      REAL*4 U(IM,JM-1,nlevnc),V(IM,JM-1,nlevnc),PL(nlevnc)
-
       integer start(4),count(4),status
+      character(len=3) :: nstr1
 C**** Rundeck parameters:
       call sync_param( "ANUDGEU", ANUDGEU )
       call sync_param( "ANUDGEV", ANUDGEV )
@@ -297,15 +243,32 @@ c   Initialisation of the files to be read
 c -----------------------------------------------------------------
            step_rea = INT( (((jday - 1) * 24) + jhour)/6) + 1
            print*,'READING REANALYSIS INIT ',jhour, jday, step_rea
+C-----------------------------------------------------------------------
 
-      print*, '  I N    N U D G E : OPEN NF FILES'
-            status=NF_OPEN('u.nc',NCNOWRIT,ncidu)
-            status=NF_INQ_VARID(ncidu,'uwnd',uid)
+      if (jday.eq.1.and.mod(itime,nday).eq.0) then
+            zirk = jyear - iyear1
+      endif 
+      if (step_rea.eq.1460.) then
+            zirk = jyear - iyear1 + 1
+      endif
+c -----------------------------------------------------------------
+c   Opening of the files to be read
+c -----------------------------------------------------------------
+
+        if (zirk.lt.10) then
+           write(nstr1,'(I1)') zirk
+        elseif (zirk.lt.100) then
+           write(nstr1,'(I2)') zirk
+        endif   
+
+            status=NF_OPEN('u'//trim(nstr1)//'.nc',NCNOWRIT,ncidu)
+            status=NF_OPEN('v'//trim(nstr1)//'.nc',NCNOWRIT,ncidv)
+
             status=NF_INQ_VARID(ncidu,'level',plid)
+            status=NF_INQ_VARID(ncidu,'uwnd',uid)
+            status=NF_INQ_VARID(ncidv,'vwnd',vid) 
 
-            status=NF_OPEN('v.nc',NCNOWRIT,ncidv)
-            status=NF_INQ_VARID(ncidv,'vwnd',vid)
-
+C------------------------------------------------------------------
 c -----------------------------------------------------------------
 c   read u, v, and pl
 c -----------------------------------------------------------------
@@ -320,19 +283,12 @@ c -----------------------------------------------------------------
       count(3)=nlevnc
       count(4)=1
 
-      status=NF_GET_VARA_REAL(ncidu,uid,start,count,u)
-      status=NF_GET_VARA_REAL(ncidv,vid,start,count,v)
-
-      call vinterana2mod(u,nlevnc,pl,u1)
-      call vinterana2mod(v,nlevnc,pl,v1)
+      status=NF_GET_VARA_REAL(ncidu,uid,start,count,un1)
+      status=NF_GET_VARA_REAL(ncidv,vid,start,count,vn1)
 
       start(4)=step_rea+1
-      status=NF_GET_VARA_REAL(ncidu,uid,start,count,u)
-      status=NF_GET_VARA_REAL(ncidv,vid,start,count,v)
-
-      call vinterana2mod(u,nlevnc,pl,u2)
-      call vinterana2mod(v,nlevnc,pl,v2)
-
+      status=NF_GET_VARA_REAL(ncidu,uid,start,count,un2)
+      status=NF_GET_VARA_REAL(ncidv,vid,start,count,vn2)
 
       return
       end subroutine nudge_init
