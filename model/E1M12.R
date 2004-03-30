40043b7e1991a11635810f0a081c4432b3aff17c
cdrar <cdrar>
2004-03-30 15:32:19 +0000

    Added a balanced 12-layer modelE version with 1979 ocean/atmosphere. Minor comment changes to default rundeck.

--- model/E1M12.R
+++ model/E1M12.R
@@ -1,13 +1,13 @@
-E1M20.R GISS Model E  2004 modelE                 rar 12/01/03
+E1M12.R GISS Model E  2004 modelE                 rar 12/01/03
 
-E1M20: replace this section by a description of what distinguishes this run   ?
+E1M12: replace this section by a description of what distinguishes this run   ?
        Use as many lines as you need. Look carefully at all the possible      ?
        choices, particularly the lines containing '?'.
        The final rundeck should contain no '?'
        Check and modify the rest of the description below:                    ?
-modelE1 (3.0) 4x5 hor. grid with 20 lyrs, top at .1 mb (+ 3 rad.lyrs)         ?
-atmospheric composition from year 1880 (or 1979)                   (see _yr)  ?
-ocean data: prescribed, 1876-1885 (or 1975-1984) climatology  (see OSST/SICE) ?
+modelE1 (3.0) 4x5 hor. grid with 12 lyrs, top at 10 mb (+ 3 rad.lyrs)         ?
+atmospheric composition from year 1979                      (see _yr)         ?
+ocean data: prescribed, 1975-1984 climatology               (see OSST/SICE)   ?
 uses turbulence scheme (not dry conv), simple strat.drag (not grav.wave drag) ?
 time steps: dynamics 7.5 min leap frog; physics 30 min.; radiation 2.5 hrs    ?
 filters: U,V in E-W direction (after every dynamics time step)                ?
@@ -18,7 +18,7 @@ Preprocessor Options
 End Preprocessor Options
 
 Object modules: (in order of decreasing priority)
-RES_M20AT                           ! horiz/vert resolution, 4x5deg, 20 layers -> .1mb
+RES_M12                             ! horiz/vert resolution, 4x5deg, 12 layers -> 10mb
 MODEL_COM GEOM_B IORSF              ! model variables and geometry
 MODELE                              ! Main and model overhead
 PARAM PARSER                        ! parameter database
@@ -46,13 +46,11 @@ POUT                                ! post-processing output
 Data input files:
     ! start up (from restart file of earlier run or) from observed conditions
 ! AIC=1DEC????.rsfE???   ! or:    ! initial conditions (atm./ground), no GIC, ISTART=8
-AIC=AIC.RES_M20A.D771201          ! initial conditions (atm.)      needs GIC, ISTART=2
+AIC=AIC.RES_M12.D771201           ! initial conditions (atm.)      needs GIC, ISTART=2
 GIC=GIC.E046D3M20A.1DEC1955       ! initial conditions (ground)
     ! ocean data for "prescribed ocean" runs : climatological ocean
-OSST=OST4X5.B.1876-85avg.Hadl1.1  ! prescr. climatological ocean (1 yr of data)
-SICE=SICE4X5.B.1876-85avg.Hadl1.1 ! prescr. climatological sea ice
-!1979 OSST=OST4X5.B.1975-84avg.Hadl1.1
-!1979 SICE=SICE4X5.B.1975-84avg.Hadl1.1
+OSST=OST4X5.B.1975-84avg.Hadl1.1  ! prescr. climatological ocean (1 yr of data)
+SICE=SICE4X5.B.1975-84avg.Hadl1.1 ! prescr. climatological sea ice
     ! or:            annually varying ocean  (pick IYEAR1 appropriately, here 1871)
 ! OSST=OST4X5.B.1871.M02.Hadl1.1  ! ocean data   Feb 1871 - 2002
 ! SICE=SICE4X5.B.1871.M02.Hadl1.1 ! ocean data   Feb 1871 - 2002
@@ -97,7 +95,7 @@ TOP_INDEX=top_index_72x46.ij
 MSU_wts=MSU.RSS.weights.data
 
 Label and Namelist:
-E1M20 (ModelE1 4x5, 20 lyrs, 1880 atm/ocn; use up to 72 (or 80) columns and ??
+E1M12 (ModelE1 4x5, 12 lyrs, 1979 atm/ocn; use up to 72 (or 80) columns and ??
 up to 60 (or 52) columns here to describe your run)?<- col 53  to  72 ->   80 ->
 DTFIX=300
 
@@ -107,7 +105,7 @@ KOCEAN=0 ! 0 or 1 , use =0 if ocn is prescribed, use =1 if ocn is predicted
 Kvflxo=0 ! use 1 ONLY to save VFLXO daily to prepare for q-flux run ?
 ocn_cycl=1  ! use =0 if ocean varies from year to year; irrelevant for pred. ocn
 
-! drag params if grav.wave drag is not used and top is at .01mb
+! drag params if grav.wave drag is not used
 X_SDRAG=.002,.0002  ! used above P(P)_sdrag mb (and in top layer)
 C_SDRAG=.0002       ! constant SDRAG above PTOP=150mb
 P_sdrag=1.          ! linear SDRAG only above 1mb (except near poles)
@@ -122,9 +120,8 @@ xCDpbl=1.
 cond_scheme=2    ! more elaborate conduction scheme (GHY, Nancy Kiang)
 
 U00ice=.59      ! increase U00ice to decrease albedo    goals: NetHtz0=0,plan.alb=30%
-U00wtrX=1.40    ! U00wtrX+.01=>nethtz0+.7                      for global annual mean
-!use  U00wtrX=1.39    for 1979 atmosphere/ocean
-! HRMAX=500.    ! not needed unless do_blU00=1, HRMAX up => nethtz0 down (alb up)
+U00wtrX=1.28    ! U00wtrX+.01=>nethtz0+.5                      for global annual mean
+! HRMAX=500.    ! not needed unless do_blU00=1, HRMAX incr. => nethtz0 decr (alb incr)
 
 CO2X=1.
 H2OstratX=1.
@@ -135,15 +132,15 @@ KSOLAR=2
 
 ! parameters that control the atmospheric/boundary conditions
 ! if set to 0, the current (day/) year is used: transient run
-crops_yr=1880 ! if -1, crops in VEG-file is used   ! =1979 , also change OSST,SICE,U00wtrX
-s0_yr=1880                                         ! =1979 , also change OSST,SICE,U00wtrX
+crops_yr=1979 ! if -1, crops in VEG-file is used
+s0_yr=1979
 s0_day=182
-ghg_yr=1880                                        ! =1979 , also change OSST,SICE,U00wtrX
+ghg_yr=1979
 ghg_day=182
-volc_yr=1880                                       ! =1979 , also change OSST,SICE,U00wtrX
+volc_yr=1979
 volc_day=182
-aero_yr=1880                                       ! =1979 , also change OSST,SICE,U00wtrX
-o3_yr=1880                                         ! =1979 , also change OSST,SICE,U00wtrX
+aero_yr=1979
+o3_yr=1979
 
 ! parameters that control the Shapiro filter
 DT_XUfilter=450. ! Shapiro filter on U in E-W direction; usually same as DT (below)
