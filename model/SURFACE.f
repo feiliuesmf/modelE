718a2450fa3b97adba0ea1a21c034ead5f3bd08e
ntausnev <ntausnev>
2007-02-09 15:59:03 +0000

    Added a statement trgasex(:,:,:,:) = 0. for an option TRACERS_GASEXCH_Natassa. It does not change the results butgood for debug

--- model/SURFACE.f
+++ model/SURFACE.f
@@ -376,6 +376,11 @@ C****
 !$OMP*  )
 !$OMP*  SCHEDULE(DYNAMIC,2)
 C
+#ifdef TRACERS_ON
+#  ifdef TRACERS_GASEXCH_Natassa
+      trgasex(:,:,:,:) = 0. 
+#  endif
+#endif
       DO J=J_0,J_1
       HEMI=1.
       IF(J.LE.JM/2) HEMI=-1.
