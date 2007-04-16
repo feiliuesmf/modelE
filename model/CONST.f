5ddae47cd92aedd23ec0736a1bb3ca2ac0e39f5a
ialeinov <ialeinov>
2007-04-16 23:01:25 +0000

    Some compilers are picky about use of Z'..'  constants (SGI). Made the code more straightorward so that it compiles on those compilers.

--- model/CONST.f
+++ model/CONST.f
@@ -30,8 +30,9 @@ C**** Numerical constants
       real*8,parameter :: undef=-1.d30
 !@param teeny  small positive value used in num/(den+teeny) to avoid 0/0
       real*8,parameter :: teeny=1.d-30
+      integer*8,parameter :: intNaN=Z'FFFFFFFFFFFFFFFF'
 !@param NaN NaN
-      real*8,parameter :: NaN=transfer(Z'FFFFFFFFFFFFFFFF',1.d0)
+      real*8,parameter :: NaN=transfer(intNaN,1.d0)
 
 C**** Physical constants
 
