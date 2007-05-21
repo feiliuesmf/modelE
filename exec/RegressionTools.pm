e3e9161479a996eeb261735dc8cfcd0ccfda5fa8
tclune <tclune>
2007-05-21 12:58:53 +0000

    Update of /giss/cvsroot/modelE/exec In directory simplex.giss.nasa.gov:/tmp/cvs-serv13201
    Added Files:
    regressionTests.pl CommandPool.pm CommandEntry.pm
    Log Message:
    Some perl scripts to support automated regression testing - initially on
    "discover".
    
    - CommandEntry.pm is a perl class that manages running a command either
      locally or via batch.  Writes a semaphore to indicate when command
      completes.
    
    - CommandPool.m is a perl class that manages a suite of commands with
      dependencies.  As commands are added to the pool, they can be set to depend
      upon the completion of other tasks.  In this way, say, running of compiled
      executables can wait for completion of the compilation.
    
    - regressionTests.pl - ad hoc driver for the above two packages.  Currently
      executes MPI, OPENMP, and SERIAL configurations of E1M20, E1oM20, E001tr,
      and E1F20.  MPI tests push these up to JM/2 processors.
    
    - RegressionTools.pm - This package is a suite of perl routines that wrap
      shell commands for the various bits of building and running modelE.   This
      file was tempororarily lost, but has now been recovered.

---
+++ exec/RegressionTools.pm
@@ -0,0 +1,182 @@
+#package RegressionTools;
+use CommandEntry;
+
+my $standardFlags = "SETUP_FLAGS=-wait";
+
+my $extraFlags;
+$extraFlags{SERIAL}   = "";
+$extraFlags{MPI}      = "ESMF=YES NPES=\$npes";
+$extraFlags{OPENMP}   = "EXTRA_FFLAGS=-mp MP=YES NPROCS=\$npes";
+$extraFlags{SERIALMP} = "EXTRA_FFLAGS=-mp"; # Intel kludge for matching serial and OpenMP.
+
+my $numLinesCMP;
+$numLinesCMP{E1M20}  = 120;
+$numLinesCMP{E1F20}  = 120;
+$numLinesCMP{E1oM20} = 110;
+$numLinesCMP{E001tr} = 123;
+
+sub setModuleEnvironment {
+#    require perlModule;
+    require "$ENV{MODULESHOME}/init/perl";
+    module (purge);
+#    module (load, "comp/intel-9.1.042", "mpi/scali-5.3", "lib/mkl-9.0.017");
+    module (load, "comp/intel-9.1.049", "mpi/scali-5.3", "lib/mkl-9.0.017");
+}
+
+sub createTemporaryCopy {
+  my $env = shift;
+  my $tempDir = shift;
+
+  my $referenceDir = $env -> {REFERENCE_DIRECTORY};
+  my $commandString = "mkdir -p $tempDir;  cp -r -u $referenceDir/* $tempDir;";
+  return (CommandEntry -> new({COMMAND => $commandString}));
+
+  }
+
+# Build rundeck and compile
+# Do a vclean just in case receiving a dirty copy.
+# Creat CMPE002P if SERIAL configuration - used for verification
+sub compileRundeck {
+  my $env = shift;
+  my $installDir = shift;
+
+  my $rundeck    = $env -> {RUNDECK};
+  my $configuration = $env -> {CONFIGURATION};
+  my $resultsDir = $env -> {RESULTS_DIRECTORY};
+
+  my $flags = "$standardFlags $extraFlags{$configuration}";
+  $flags =~ s/(\$npes)/1/eeg;
+
+  my $expName;
+  if (@_) {$expName = shift}
+  else {$expName = "$rundeck.$configuration"};
+
+  my $commandString = <<EOF;
+  export MODELERC;
+  cd $installDir/decks;
+  make rundeck RUN=$expName RUNSRC=$rundeck;
+  make vclean RUN=$expName;
+  make gcm RUN=$expName $flags;
+EOF
+
+  my $binDir = $expName . "_bin";
+  if ($configuration eq SERIAL) {
+    $commandString .= "make aux RUN=$expName $flags;\n";
+    $commandString .= "cp $binDir/CMPE002P $resultsDir/CMPE002P.$rundeck;\n";
+    $commandString .= "cp $binDir/CMPE002 $resultsDir/CMPE002.$rundeck;\n";
+  }
+
+  return (CommandEntry -> new({COMMAND => $commandString, QUEUE => "datamove", STDOUT_LOG_FILE => "$resultsDir/$expName.buildlog"}));
+}
+
+sub runConfiguration {
+    my $env = shift;
+    my $installDir = shift;
+    my $npes = shift;
+
+    my $rundeck    = $env -> {RUNDECK};
+    my $configuration = $env -> {CONFIGURATION};
+    my $resultsDir = $env -> {RESULTS_DIRECTORY};
+
+    my $flags = "$standardFlags $extraFlags{$configuration}";
+    $flags =~ s/(\$npes)/$npes/eeg;
+    my $expName = "$rundeck.$configuration";
+
+    my $suffix;
+    if ($configuration eq "SERIAL" or $configuration eq "SERIALMP") {
+	$suffix = "";
+    }
+    else {
+	$suffix = ".np=$npes";
+    }
+    
+    my $continue1Day = "./$expName -r ";
+    if ($configuration eq "MPI" or $configuration eq "OPENMP") {$continue1Day = "./$expName -np $npes -r ";}
+
+    my $commandString = <<EOF;
+    echo "Using flags: $flags "
+    export MODELERC;
+    cd $installDir/decks;
+    make setup_nocomp RUN=$expName $flags;
+    cd $expName;
+    cp fort.2 $resultsDir/$expName.1hr$suffix;
+    $continue1Day;
+    cp fort.2 $resultsDir/$expName.1dy$suffix;
+EOF
+  return (CommandEntry -> new({COMMAND => $commandString, QUEUE => "", STDOUT_LOG_FILE => "$resultsDir/$expName.runlog", NUM_PROCS => $npes}));
+}
+
+sub runSetup {
+  my $env = shift;
+
+  my $installDir    = $env -> {INSTALL_DIR};
+  my $rundeck       = $env -> {RUNDECK};
+  my $configuration = $env -> {CONFIGURATION};
+  my $npes          = $env -> {NUM_PROCS};
+  my $resultsDir    = $env -> {RESULTS_DIR};
+
+  my $expName;
+  if (@_) {$expName = shift}
+  else {$expName = "$rundeck.$configuration"};
+
+  my $commandString = <<EOF;
+cd $dir/decks;
+make setup_nocompile RUN=$expName $standardFlags;
+cp $expName/fort.2 $resultsDir/$expName.1hr;
+pushd $expName;
+./$expName -r;
+cp $expName/fort.2 $resultsDir/$expName.1dy;
+EOF
+
+  return (CommandEntry -> new({COMMAND => $commandString}))
+}
+
+sub checkResults {
+  my $env = shift;
+  my $tempDir = shift;
+  my $npes = shift;
+
+  my $installDir    = $env -> {INSTALL_DIR};
+  my $rundeck       = $env -> {RUNDECK};
+  my $configuration = $env -> {CONFIGURATION};
+  my $resultsDir    = $env -> {RESULTS_DIR};
+  my $cmp           = "$resultsDir/CMPE002";
+  my $expName;
+
+  if (@_) {$expName = shift}
+  else {$expName = "$rundeck.$configuration"};
+
+  my $suffix;
+  if ($configuration eq "SERIAL" or $configuration eq "SERIALMP") {
+      $suffix = "";
+  }
+  else {
+      $suffix = ".np=$npes";
+  }
+
+  my $commandString = <<EOF;
+cd $resultsDir;
+$cmp $rundeck.SERIAL.1hr $expName.1hr$suffix;
+$cmp $rundeck.SERIAL.1dy $expName.1dy$suffix;
+EOF
+
+  return (CommandEntry -> new({COMMAND => $commandString, STDOUT_LOG_FILE => "$expName.log"}))
+}
+
+sub cvsCheckout {
+#    my $what = shift;
+  my $env = shift;
+
+  my $scratchDirectory = $env -> {SCRATCH_DIRECTORY};
+  my $cvsroot          = $env -> {CVSROOT};
+
+  my $commandString = <<EOF;
+(pushd $scratchDirectory
+cvs -d $cvsroot co modelE
+popd)
+EOF
+
+  return (CommandEntry -> new({COMMAND => $commandString}))
+}
+
+1;
