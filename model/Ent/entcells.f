e8e0f3b1451338b67ebc7c2dd325d7851ad8ca8f
pushker <pushker>
2007-08-02 21:49:13 +0000

    Corrected location of R_root value in call to insert_cohort in init_simple_entcell.

--- model/Ent/entcells.f
+++ model/Ent/entcells.f
@@ -446,8 +446,8 @@
      &           cpooldata(ncov,FOL),0.d0,cpooldata(ncov,SW),0.d0,
      &           cpooldata(ncov,HW),0.d0,
      &           cpooldata(ncov,LABILE),0.d0,
-     &           cpooldata(ncov,FR),0.d0,0.d0,0.d0,0.d0, !added last 0 for R_root -PK 5/15/07
-     &           Ci_ini, CNC_ini,0.d0,0.d0,0.d0,
+     &           cpooldata(ncov,FR),0.d0,0.d0,0.d0,
+     &           Ci_ini, CNC_ini,0.d0,0.d0,0.d0,0.d0, !added last 0 for R_root -PK 5/15/07
      &           0.d0,0.d0,0.d0,0.d0,0.d0)
           endif
           call summarize_patch(pp)
