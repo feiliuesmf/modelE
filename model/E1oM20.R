1879139cde21f8fe4ea96677aff2b47bcd155dac
mbhat <mbhat>
2007-10-16 21:50:44 +0000

    Rundeck E1oM20 is modified to include source for sparse gather/scatter.

--- model/E1oM20.R
+++ model/E1oM20.R
@@ -39,6 +39,7 @@ LAKES_COM LAKES                     ! lake modules
 SEAICE SEAICE_DRV                   ! seaice modules
 LANDICE LANDICE_DRV                 ! land ice modules
 ICEDYN_DRV ICEDYN                   ! ice dynamics modules
+SparseCommunicator_mod              ! sparse gather/scatter module
 ODIAG_COM OCEAN_COM OSTRAITS_COM OGEOM ! dynamic ocean modules
 OCNDYN OSTRAITS OCNGM OCNKPP           ! dynamic ocean routines
 ODIAG_PRT                              ! ocean diagnostic print out
