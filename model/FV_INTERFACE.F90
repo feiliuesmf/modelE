0a961457a8b4795b9345d97a7585c4ba4dbe1a21
tclune <tclune>
2007-12-05 14:02:46 +0000

    Oops - added a second call to saveTendencies(); only the first is needed.

--- model/FV_INTERFACE.F90
+++ model/FV_INTERFACE.F90
@@ -382,10 +382,8 @@ contains
 
     call saveTendencies(fv)
     call ESMF_GridCompFinalize ( fv % gc, fv % import, fv % export, clock, rc )
-    call saveTendencies(fv)
 
     Deallocate(fv % U_old, fv % V_old, fv % dPT_old, fv % dT_old, fv % PE_old)
-
 
   end subroutine finalize
 
