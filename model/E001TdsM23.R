6945ccc6b9398940be4fe56b19c53d2135e17a5f
faluvegi <faluvegi>
2008-07-08 22:45:56 +0000

    Comments only: removing one preprocessor directive, adding two. All are commented out though.

--- model/E001TdsM23.R
+++ model/E001TdsM23.R
@@ -23,12 +23,12 @@ Preprocessor Options
 !  OFF #define INITIAL_GHG_SETUP        ! only for setup hour to get ghg IC file
 !  OFF #define TRACERS_AEROSOLS_Koch    ! Dorothy Koch's tracers (aerosols, etc)
 !  OFF #define EDGAR_HYDE_SOURCES       ! use EDGAR-HYDE tracers sources instead
-!  OFF #define regional_Ox_tracers      ! turns on regional Ox tracers
 !  OFF #define INTERACTIVE_WETLANDS_CH4 ! turns on interactive CH4 wetland source
 !  OFF #define NUDGE_ON                 ! nudge the meteorology
 !  OFF #define GFED_3D_BIOMASS          ! turns on IIASA AR4 GFED biomass burning
 !  OFF #define BIOGENIC_EMISSIONS       ! turns on interactive isoprene emissions
-
+!  OFF #ifdef SULF_ONLY_AEROSOLS        ! when using Koch aerosols,omit BC,OC,SS
+!  OFF #ifdef HTAP_LIKE_DIAGS    ! adds many diags, changes OH diag, adds Air tracer
 End Preprocessor Options
 
 Object modules: (in order of decreasing priority)
