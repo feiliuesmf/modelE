62f683bcb422525bb4637c71294ec0d1c0fa64eb
kostas <kostas>
2008-10-23 16:10:44 +0000

    Typo correction

--- model/RAD_DRV.f
+++ model/RAD_DRV.f
@@ -1084,7 +1084,7 @@ C**** REPLICATE VALUES AT POLE
       USE CONSTANT, only : sday,lhe,lhs,twopi,tf,stbo,rhow,mair,grav
      *     ,bysha,pi,radian
       USE MODEL_COM
-      USE GEOM, only : dlat_dg, imaxj, dxyp, axyp, areag, BYDXYP
+      USE GEOM, only : dlat_dg, imaxj, dxyp, axyp, areag, BYAXYP
      &     ,lat2d,lon2d
 c      USE ATMDYN, only : CALC_AMPK
       USE RADPAR
@@ -1761,7 +1761,8 @@ C**** more than one tracer is lumped together for radiation purposes
      *          trm(i,j,l,n_OCI3)+trm(i,j,l,n_OCA3)+
      *          trm(i,j,l,n_OCA4))*BYAXYP(I,J)
           case ("BCIA")
-            TRACER(L,n)=(trm(i,j,l,n_BCII)+trm(i,j,l,n_BCIA))*BYAXYP(I,J)
+            TRACER(L,n)=(trm(i,j,l,n_BCII)+trm(i,j,l,n_BCIA))*
+     *                  BYAXYP(I,J)
           case default
             TRACER(L,n)=wttr(n)*trm(i,j,l,NTRIX(n))*BYAXYP(I,J)
           end select
