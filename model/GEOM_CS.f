566890566f2b9b135ad5450eca409de2e4e0ccca
tclune <tclune>
2008-12-15 22:37:25 +0000

    1) Converted names to agree with 2D qty's in GEOM_B 2) Added a few derived quantities (coslat, sinlat, ...)

--- model/GEOM_CS.f
+++ model/GEOM_CS.f
@@ -5,8 +5,6 @@
 !@ver  1.0 (CS grid version)
 !@cont GEOM_CS
 
-      use fv_grid_tools_mod, only: 
-
 
       IMPLICIT NONE
       SAVE
@@ -14,24 +12,23 @@
 !@var AREAG global integral of area (m^2)
       REAL*8 :: AREAG
 !@var  LAT latitude of mid point of primary grid box (radians)
-      REAL*8, ALLOCATABLE :: LAT(:,:)
+      REAL*8, ALLOCATABLE :: lat2d(:,:)
+      REAL*8, ALLOCATABLE :: lat2d_dg(:,:)
+      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: sinlat2d, coslat2d
 !@var  LON longitude of mid points of primary grid box (radians)
-      REAL*8, ALLOCATABLE :: LON(:,:)
+      REAL*8, ALLOCATABLE :: lon2d(:,:)
 !@var  DXYP,BYDXYP area of grid box (+inverse) (m^2)
 C**** Note that this is not the exact area, but is what is required for
 C**** some B-grid conservation quantities
       REAL*8, ALLOCATABLE, DIMENSION(:,:) :: DXYP,BYDXYP
 
-!@var  DXP,DYP,BYDXP,BYDYP distance between points on primary grid
-!@+     (+inverse)
-      REAL*8, DIMENSION(JM) :: DXP,DYP,BYDXP,BYDYP
 
       SUBROUTINE GEOM_CS
 !@sum  GEOM_CS Calculate spherical geometry for CS grid
 !@auth T. Clune
 !@ver  1.0 (CS grid version)
-
-      use fv_grid_tools_mod, only: CSgrid => grid
+      USE CONSTANT, only : RADIUS,TWOPI,radian
+      use fv_grid_tools_mod, only: CSgrid => grid, area
       use DOMAIN_DECOMP, only: grid, get
       implicit none
 
@@ -40,16 +37,24 @@ C**** some B-grid conservation quantities
       
       call get(grid, J_STRT_HALO=j0h, J_STOP_HALO=j1h, I_STRT_HALO=i0h, I_STOP_HALO=i1h)
 
-      allocate(LAT(i0h:i1h, j0h:j1h)
-      allocate(LON(i0h:i1h, j0h:j1h)
+      allocate(lat2d(i0h:i1h, j0h:j1h)
+      allocate(lat2d_deg(i0h:i1h, j0h:j1h)
+      allocate(lon2d(i0h:i1h, j0h:j1h)
+      allocate(sinlat2d(i0h:i1h, j0h:j1h)
+      allocate(coslon2d(i0h:i1h, j0h:j1h)
+
       allocate(DXYP(i0h:i1h, j0h:j1h)
       allocate(BYDXYP(i0h:i1h, j0h:j1h)
 
       AREAG = 2 * TWOPI * RADIUS*RADIUS
 
       ! From FV CS grid
-      LON = CSgrid(:,:,1)
-      LAT = CSgrid(:,:,2)
+      lon2d = CSgrid(:,:,1)
+      lat2d = CSgrid(:,:,2)
+      lat2d_dg = lat2d/radian
+      sinlat2d = sin(lat2d)
+      coslat2d = cos(lat2d)
+
       DXYP = area(:,:)
       BYDXYP = 1/DXYP
 
