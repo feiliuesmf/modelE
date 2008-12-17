94a167dcf037564988a6e8b9ea95e84c2e04883f
tclune <tclune>
2008-12-17 19:40:21 +0000

    1) Fixed some more typos from earlier sketch.  (Too liberal use of cut/paste.) 2) Corrected source of cell centers from FV. 3) Added lat2d_corner and lon2d_corner for the corner points on the cubed sphere cells.

--- model/GEOM_CS.f
+++ model/GEOM_CS.f
@@ -11,16 +11,26 @@
 
 !@var AREAG global integral of area (m^2)
       REAL*8 :: AREAG
-!@var  LAT latitude of mid point of primary grid box (radians)
+!@var  lat2d latitude of mid point of primary grid box (radians)
       REAL*8, ALLOCATABLE :: lat2d(:,:)
+!@var  lat2d_dg latitude of mid point of primary grid box (degrees)
       REAL*8, ALLOCATABLE :: lat2d_dg(:,:)
       REAL*8, ALLOCATABLE, DIMENSION(:,:) :: sinlat2d, coslat2d
-!@var  LON longitude of mid points of primary grid box (radians)
+!@var  lon2d longitude of mid point of primary grid box (radians)
       REAL*8, ALLOCATABLE :: lon2d(:,:)
-!@var  DXYP,BYDXYP area of grid box (+inverse) (m^2)
+!@var  lon2d_dg longitude of mid point of primary grid box (degrees)
+      REAL*8, ALLOCATABLE :: lon2d_dg(:,:)
+      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: sinlon2d, coslon2d
+
+!@var  lat2d_corner latitude of corner point (radians)
+      REAL*8, ALLOCATABLE :: lat2d_corner(:,:)
+!@var  lon2d_corner longitude of mid point of primary grid box (radians)
+      REAL*8, ALLOCATABLE :: lon2d_corner(:,:)
+
+!@var  axyp,byaxyp area of grid box (+inverse) (m^2)
 C**** Note that this is not the exact area, but is what is required for
 C**** some B-grid conservation quantities
-      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: DXYP, BYDXYP
+      REAL*8, ALLOCATABLE, DIMENSION(:,:) :: axyp, byaxyp
 
 
       SUBROUTINE GEOM_CS
@@ -28,7 +38,7 @@ C**** some B-grid conservation quantities
 !@auth T. Clune
 !@ver  1.0 (CS grid version)
       USE CONSTANT, only : RADIUS,TWOPI,radian
-      use fv_grid_tools_mod, only: CSgrid => grid, area
+      use fv_grid_tools_mod, only: agrid, corner_grid => gridarea
       use DOMAIN_DECOMP, only: grid, get
       implicit none
 
@@ -38,20 +48,31 @@ C**** some B-grid conservation quantities
       call get(grid, J_STRT_HALO=j0h, J_STOP_HALO=j1h, I_STRT_HALO=i0h, I_STOP_HALO=i1h)
 
       allocate(lat2d(i0h:i1h, j0h:j1h)
-      allocate(lat2d_deg(i0h:i1h, j0h:j1h)
       allocate(lon2d(i0h:i1h, j0h:j1h)
+
+      allocate(lat2d_deg(i0h:i1h, j0h:j1h)
+      allocate(lon2d_deg(i0h:i1h, j0h:j1h)
+
       allocate(sinlat2d(i0h:i1h, j0h:j1h)
       allocate(coslat2d(i0h:i1h, j0h:j1h)
 
-      allocate(DXYP(i0h:i1h, j0h:j1h)
-      allocate(BYDXYP(i0h:i1h, j0h:j1h)
+      allocate(lat2d_corner(i0h:i1h+1, j0h:j1h+1)
+      allocate(lon2d_corner(i0h:i1h+1, j0h:j1h+1)
+
+      allocate(axyp(i0h:i1h, j0h:j1h)
+      allocate(byaxyp(i0h:i1h, j0h:j1h)
 
       AREAG = 2 * TWOPI * RADIUS*RADIUS
 
       ! From FV CS grid
-      lon2d = CSgrid(:,:,1)
-      lat2d = CSgrid(:,:,2)
+      lon2d = agrid(:,:,1)
+      lat2d = agrid(:,:,2)
+
+      lon2d_corner = corner_grid(:,:,1)
+      lat2d_corner = corner_grid(:,:,2)
+
       lat2d_dg = lat2d/radian
+      lon2d_dg = lon2d/radian
       sinlat2d = sin(lat2d)
       coslat2d = cos(lat2d)
 
