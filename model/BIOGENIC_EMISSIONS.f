08ccde9fa82a310e5c94ff7d6b2289ed61f8dd8c
gavin <gavin>
2008-12-24 21:06:34 +0000

    adjustment of area array and general tidy-up of code (use parameter statements to hard code arrays).

--- model/BIOGENIC_EMISSIONS.f
+++ model/BIOGENIC_EMISSIONS.f
@@ -6,7 +6,12 @@
 !@+ globally when #defined BIOGENIC_EMISSIONS
       real*8 :: base_isopreneX=1.d0
       real*8, allocatable, dimension(:,:,:) :: baseisop
-      real*8 isopcoeff(npolynb)
+      real*8, parameter :: isopcoeff(npolynb) = (/
+     *     -1.86E-01, 2.19E+00,  2.12E+00, -2.43E-01, -4.72E+00,
+     *      1.05E+01, 3.77E-01, -3.05E+00,  3.05E-01,  5.16E-01,
+     *      2.72E+00,-4.82E+00, -9.59E-04, -1.29E+00,  9.37E-01,
+     *     -3.31E-01, 1.07E+00, -7.59E-02, -3.01E-01, -4.07E-01 /) 
+
       end module biogenic_emis
 
 
@@ -37,7 +42,7 @@
                !canopy_temp_ij = array(im,jm) of canopy temperatures (C)
                !Same temperature for all veg types over grid cell.
       use rad_com, only : cosz1,cfrac
-      use geom, only : bydxyp
+      use geom, only : byaxyp
       use pbl_drv, only : t_pbl_args
       use constant, only : rgas
       use tracers_drydep, only : xylai,ijreg,ijuse
@@ -101,7 +106,7 @@
 ! emisop = kg C emitted from grid cell per second
 ! 2D interative isoprene source. Convert to units kg C/m2/s:
 
-        emisop=emisop*bydxyp(j)
+        emisop=emisop*byaxyp(i,j)
 
       end select
 
@@ -115,6 +120,7 @@
 !@+  in from the file 'isoprene.coef'. I am hardcoding them
 !@+  here, as the quickest way to make sure this is ESMF-
 !@+  compliant, as I don't suspect we will commit this code.
+!@ THIS IS NOW OBSOLETE
 
       use biogenic_emis
 
@@ -125,16 +131,6 @@
 
       if(npolynb /= 20)call stop_model('npolynb problem',255)
 
-      isopcoeff(1) = -1.86E-01; isopcoeff(2) =  2.19E+00
-      isopcoeff(3) =  2.12E+00; isopcoeff(4) = -2.43E-01
-      isopcoeff(5) = -4.72E+00; isopcoeff(6) =  1.05E+01
-      isopcoeff(7) =  3.77E-01; isopcoeff(8) = -3.05E+00
-      isopcoeff(9) =  3.05E-01; isopcoeff(10)=  5.16E-01
-      isopcoeff(11)=  2.72E+00; isopcoeff(12)= -4.82E+00
-      isopcoeff(13)= -9.59E-04; isopcoeff(14)= -1.29E+00
-      isopcoeff(15)=  9.37E-01; isopcoeff(16)= -3.31E-01
-      isopcoeff(17)=  1.07E+00; isopcoeff(18)= -7.59E-02
-      isopcoeff(19)= -3.01E-01; isopcoeff(20)= -4.07E-01
 
       return                                                            
       end                       
@@ -154,56 +150,33 @@
       use biogenic_emis
       use tracers_drydep, only : ijreg,ijland
       use constant, only   : avog
-      use geom, only : dxyp,imaxj
+      use geom, only : axyp,imaxj
       use domain_decomp, only : get, grid
 
       implicit none
 
-      integer:: i,j,k,j_0,j_1
-      real*8, dimension(nvegtype) ::  converT
+      integer:: i,j,k,j_0,j_1,i_0,i_1
+      real*8, dimension(0:nvegtype-1), parameter ::  converT = (/
+     *     0.00E+00, 2.79E+12, 8.71E+11, 2.79E+12, 2.79E+12, 2.79E+12, 
+     *     2.79E+12, 2.79E+12, 3.34E+12, 2.79E+12, 2.79E+12, 2.79E+12, 
+     *     2.79E+12, 2.79E+12, 2.79E+12, 2.79E+12, 2.79E+12, 2.79E+12, 
+     *     2.79E+12, 2.79E+12, 1.67E+12, 1.67E+12, 1.67E+12, 1.39E+12, 
+     *     3.34E+12, 6.27E+12, 6.27E+12, 3.34E+12, 0.93E+12, 0.93E+12,
+     *     8.71E+11, 8.71E+11, 2.49E+12, 1.39E+12, 2.79E+12, 2.79E+12,
+     *     8.71E+11, 8.71E+11, 8.71E+11, 8.71E+11, 0.93E+12, 1.39E+12,
+     *     8.71E+11, 2.79E+12, 1.67E+12, 1.67E+12, 3.34E+12, 2.79E+12,
+     *     9.41E+12, 2.79E+12, 3.34E+12, 3.34E+12, 2.79E+12, 2.79E+12,
+     *     3.34E+12, 2.79E+12, 4.18E+12, 4.18E+12, 1.39E+12, 3.34E+12,
+     *     3.14E+12, 3.34E+12, 1.39E+12, 3.34E+12, 8.71E+11, 2.79E+12,
+     *     2.79E+12, 2.79E+12, 2.79E+12, 3.34E+12, 2.79E+12, 3.34E+12,
+     *     8.71E+11, 2.79E+12 /)
+
       real*8 :: factor                       
 
-      CALL GET(grid, J_STRT=J_0, J_STOP=J_1)
+      CALL GET(grid, J_STRT=J_0, J_STOP=J_1,
+     &               I_STRT=I_0, I_STOP=I_1)
 
       if(nvegtype /= 74) call stop_model('nvegtype problem',255)
-
-      convert( 0+1)=0.00E+00; convert( 1+1)=2.79E+12
-      convert( 2+1)=8.71E+11; convert( 3+1)=2.79E+12
-      convert( 4+1)=2.79E+12; convert( 5+1)=2.79E+12
-      convert( 6+1)=2.79E+12; convert( 7+1)=2.79E+12
-      convert( 8+1)=3.34E+12; convert( 9+1)=2.79E+12
-      convert(10+1)=2.79E+12; convert(11+1)=2.79E+12
-      convert(12+1)=2.79E+12; convert(13+1)=2.79E+12
-      convert(14+1)=2.79E+12; convert(15+1)=2.79E+12
-      convert(16+1)=2.79E+12; convert(17+1)=2.79E+12
-      convert(18+1)=2.79E+12; convert(19+1)=2.79E+12
-      convert(20+1)=1.67E+12; convert(21+1)=1.67E+12
-      convert(22+1)=1.67E+12; convert(23+1)=1.39E+12
-      convert(24+1)=3.34E+12; convert(25+1)=6.27E+12
-      convert(26+1)=6.27E+12; convert(27+1)=3.34E+12
-      convert(28+1)=0.93E+12; convert(29+1)=0.93E+12
-      convert(30+1)=8.71E+11; convert(31+1)=8.71E+11
-      convert(32+1)=2.49E+12; convert(33+1)=1.39E+12
-      convert(34+1)=2.79E+12; convert(35+1)=2.79E+12
-      convert(36+1)=8.71E+11; convert(37+1)=8.71E+11
-      convert(38+1)=8.71E+11; convert(39+1)=8.71E+11
-      convert(40+1)=0.93E+12; convert(41+1)=1.39E+12
-      convert(42+1)=8.71E+11; convert(43+1)=2.79E+12
-      convert(44+1)=1.67E+12; convert(45+1)=1.67E+12
-      convert(46+1)=3.34E+12; convert(47+1)=2.79E+12
-      convert(48+1)=9.41E+12; convert(49+1)=2.79E+12
-      convert(50+1)=3.34E+12; convert(51+1)=3.34E+12
-      convert(52+1)=2.79E+12; convert(53+1)=2.79E+12
-      convert(54+1)=3.34E+12; convert(55+1)=2.79E+12
-      convert(56+1)=4.18E+12; convert(57+1)=4.18E+12
-      convert(58+1)=1.39E+12; convert(59+1)=3.34E+12
-      convert(60+1)=3.14E+12; convert(61+1)=3.34E+12
-      convert(62+1)=1.39E+12; convert(63+1)=3.34E+12
-      convert(64+1)=8.71E+11; convert(65+1)=2.79E+12
-      convert(66+1)=2.79E+12; convert(67+1)=2.79E+12
-      convert(68+1)=2.79E+12; convert(69+1)=3.34E+12
-      convert(70+1)=2.79E+12; convert(71+1)=3.34E+12
-      convert(72+1)=8.71E+11; convert(73+1)=2.79E+12
 
 ! Isoprene is traced in terms of equivalent C atoms.
 ! Compute the baseline ISOPRENE emissions, which depend on veg type   
@@ -213,17 +186,16 @@
       factor = 12.d-3*1.d4/avog      
 
       do J=J_0,J_1
-        do I=1,imaxj(J)
+        do I=I_0,imaxj(J)
           do k=1,ijreg(i,j)
             baseisop(i,j,K) = 
-     &      convert(ijland(i,j,k)+1)*factor*dxyp(j)*base_isopreneX
+     &      convert(ijland(i,j,k)+1)*factor*axyp(i,j)*base_isopreneX
           enddo                                                       
         enddo                                                          
       enddo                                                             
                                                                         
       return                                                            
       end                                                               
-                      
 
 ! Local Air temperature correction for isoprene emissions:
 ! From Guenther et al. 1992
@@ -232,13 +204,9 @@
 
       implicit none
 
-      real*8 :: r,ct1,ct2,t1,t3,temp
-
-      r=8.314d0
-      ct1=9.5d4
-      ct2=2.3d5
-      t1=3.03e2
-      t3=3.14e2
+      real*8, intent(in) :: temp
+      real*8, parameter :: r=8.314d0, ct1=9.5d4, ct2=2.3d5, t1=3.03d2,
+     *     t3=3.14d2 
 
       tcorr =    exp( ct1/(r*t1*temp)*(temp-t1) ) /
      &   (1.D0 + exp( ct2/(r*t1*temp)*(temp-t3) ))
