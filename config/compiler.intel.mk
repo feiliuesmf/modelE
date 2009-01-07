b726c7d988b45866b01e815b093b9b420ec4d326
ialeinov <ialeinov>
2009-01-07 04:42:56 +0000

    Added special flags "-assume protect_parens -fp-model strict" which are enabled only for ifort 10.1 and 11.0 . This (hopefully) should fix reproducibility problems with these compilers, but I'm still testing it...
    Still don't understand why without "-fp-model strict" the results
    are different for different number of MPI threads...

--- config/compiler.intel.mk
+++ config/compiler.intel.mk
@@ -1,5 +1,7 @@
 
 F90 = ifort
+IFORT_RELEASE := $(shell ifort --version | perl -e \
+  'while(<>){ if(/ifort.* (\d+\.\d+)/) { print "$$1"; } }')
 FMAKEDEP = $(SCRIPTS_DIR)/sfmakedepend
 CMP_MOD = $(SCRIPTS_DIR)/compare_module_file.pl -compiler INTEL-ifort-9-0-on-LINUX
 FFLAGS = -fpp -O2 -ftz         -convert big_endian 
@@ -12,6 +14,18 @@ FFLAGS += -openmp
 F90FLAGS += -openmp
 LFLAGS += -openmp
 endif
+
+# flags needed for particular releases
+ifeq ($(IFORT_RELEASE),11.0)
+FFLAGS += -assume protect_parens -fp-model strict
+F90FLAGS += -assume protect_parens -fp-model strict
+endif
+
+ifeq ($(IFORT_RELEASE),10.1)
+FFLAGS += -assume protect_parens -fp-model strict
+F90FLAGS += -assume protect_parens -fp-model strict
+endif
+
 ifeq ($(COMPILE_WITH_TRAPS),YES)
 FFLAGS += -CB -fpe0
 LFLAGS += -CB -fpe0
