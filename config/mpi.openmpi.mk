d25ac8c3a4c8fa101e6046fe1a36352d596e8ffa
ialeinov <ialeinov>
2009-01-09 05:30:17 +0000

    Removed unneeded libraries. This file is now compatible with MAC (ifort 10.1 + gcc).

--- config/mpi.openmpi.mk
+++ config/mpi.openmpi.mk
@@ -9,8 +9,10 @@ endif
 #LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
 #-lfmpi -lmpi -lstdc++ -threads
 
-LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
--lmpi_f90 -lmpi_f77 -lmpi -lstdc++ -threads
+##LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
+##-lmpi_f90 -lmpi_f77 -lmpi -lstdc++ -threads
+
+LIBS += -lmpi_f77 -lmpi -lmpi_cxx -lstdc++
 
 
 #LIBS +=  -lmpi -lmpigc3 -lmpigc4 -lmpigf -lmpigi -lmpiic4 -lmpiic -lmpiif -threads
