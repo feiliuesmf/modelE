8bd690b8e1050664c5676c56c17dd671c39db7f2
ialeinov <ialeinov>
2009-01-17 04:39:44 +0000

    This is a hack for those who really need to remove certain external modules from the list of dependencies. Just edit this file and add the names of modules you want to skip to "$skip_modules" , for example
     $skip_modules="domain_decomp.mod|model_com.mod|constant.mod";
    
    This is a temporary solution. The problem with external modules will
    be fixed in a new version of makefile which supports subdirectories.

--- exec/pproc_dep.pl
+++ exec/pproc_dep.pl
@@ -1,6 +1,14 @@
 #!/usr/bin/perl
 
+# to skip dependencies on certan modules list them here
+# for example:
+# $skip_modules="domain_decomp.mod|model_com.mod|constant.mod";
+$skip_modules="";
+
 while(<>) {
+
+    # hack to skip unneeded dependencies
+    if ( $skip_modules ) { s/ ($skip_modules)//g; }
 
     $MOD = "mod"; # could be different for other compilers
 
