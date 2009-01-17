55384130c98ac75a176f3101d915cf74d739c6b1
ialeinov <ialeinov>
2009-01-17 05:12:09 +0000

    Included MPITYPE_LOOKUP_HACK by default for openmpi builds. This seems to be working ok to prevent memory leak in openmpi . If you notice any problems with this (model works with scali but crashes with openmpi), comment out corresponding line in this file and let me know.
    Also added "rt" library since Linux seems to need it.

--- config/mpi.openmpi.mk
+++ config/mpi.openmpi.mk
@@ -5,6 +5,8 @@ F90FLAGS += -I${MPIDIR}/include
 LIBS += -L${MPIDIR}/lib
 endif
 
+# try to work around memory leak
+CPPFLAGS += -DMPITYPE_LOOKUP_HACK
 
 #LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
 #-lfmpi -lmpi -lstdc++ -threads
@@ -12,7 +14,7 @@ endif
 ##LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
 ##-lmpi_f90 -lmpi_f77 -lmpi -lstdc++ -threads
 
-LIBS += -lmpi_f77 -lmpi -lmpi_cxx -lstdc++
+LIBS += -lmpi_f77 -lmpi -lmpi_cxx -lstdc++ -lrt
 
 
 #LIBS +=  -lmpi -lmpigc3 -lmpigc4 -lmpigf -lmpigi -lmpiic4 -lmpiic -lmpiif -threads
