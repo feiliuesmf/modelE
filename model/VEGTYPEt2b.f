a962cb36f9501014b06be074055a5de7e8b47b85
denis <denis>
2009-02-04 04:51:08 +0000

    *converting LAI files to binary. Binary LAI files follow same conventions as binary vegtype fileii.e. store file along increasing vegetation types. i *Both must be converted at the same time as LAI needs ILAND/IREG information from vegetation types file.

--- model/VEGTYPEt2b.f
+++ model/VEGTYPEt2b.f
@@ -7,7 +7,7 @@
 !@! enddo 
 !@  note that the upper bound is now NVEGTYPE. 
 !@+ The main change is that now the file contains many entries where IUSE_glob=0.
-!@+ Explanation about ordering of veg. types
+!@+ Add explanation about ordering of veg. types
 !@+
 !@+ The arrays IREG_glob and ILAND_GLOB are deduced from
 !@+ IUSE_glob by checking which values of IREG_glob are nonzero
@@ -26,11 +26,10 @@
       integer, dimension(IM,JM) :: IREG_glob
       integer, dimension(IM,JM,NTYPE) :: ILAND_glob,IUSE_glob
       real*4, dimension(IM,JM,NVEGTYPE) :: FUSE_glob,order   !fraction btw 0. and 1., index for veg. type ordering
-      CHARACTER*1, dimension(70) :: COM
-      character(len=300) :: out_line
-      INTEGER, DIMENSION(NVEGTYPE) :: IZO
-      INTEGER :: I,J,K,L,NWAT2,IDUMMY,iu_data,iols,iveg
-      INTEGER, DIMENSION(NWAT) :: IWATER
+      real*8, dimension(im,jm,ntype) :: XLAI_glob
+      real*4, dimension(im,jm,nvegtype) :: XLAI_out
+
+      INTEGER :: I,J,K,L,iu_data,iveg
       character*80 :: fname,oname,TITLE_VEG_TYPE,TITLE
       character(len=2) :: vegtype 
 
@@ -57,7 +56,6 @@ c***  for fortran arrays
      &              IUSE_glob(I,J,K)/1000.
                order(I,J,ILAND_GLOB(I,J,K)+1)=K
             enddo
-            write(*,*) "ireg=",IREG_glob(I,J),maxval(order(I,J,:),3)
          enddo
       enddo
       
@@ -91,5 +89,36 @@ c***  Check that fractions at a particular cell add up to 1
 c      write(6,*) sum(FUSE_glob,3)
  
       close(unit=3200)
+
+C      Read lai:
+      fname='lai01.global'
+      open(20,FILE=fname,FORM="FORMATTED",status="UNKNOWN")
+10      READ(20,"(3I3,20F5.1)",END=20) I,J,INDEX,
+     &  (XLAI_glob(I,J,K),K=1,INDEX)
+        GOTO 10
+20      close(20)
+
+      XLAI_out(:,:,:)=0.
+
+      do I=1,IM
+         do J=1,JM
+            do K=1,IREG_glob(I,J)
+               XLAI_out(I,J,ILAND_GLOB(I,J,K)+1)=
+     &              XLAI_glob(I,J,K)
+            enddo
+         enddo
+      enddo
+
+      oname=trim(fname)//".bin"
+      write(*,*) oname
+      open(unit=3200, FILE=oname,FORM='unformatted',STATUS='unknown')
+
+      do iveg=0,NVEGTYPE-1
+         write(vegtype,'(i2)') iveg
+         TITLE_VEG_TYPE="LAI corresponding to veg type="//vegtype
+         write(3200) TITLE_VEG_TYPE,XLAI_out(:,:,iveg+1)
+      enddo
+
+      close(3200)
 
       end program vegtypet2b
