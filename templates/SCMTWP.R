c7a09dc73dfb0401670f3ad134e4ce4b18eda539
awolf <awolf>
2009-04-30 14:56:44 +0000

    c      added source code files

--- templates/SCMTWP.R
+++ templates/SCMTWP.R
@@ -24,6 +24,7 @@ MODELE                              ! Main and model overhead
 PARAM PARSER                        ! parameter database
 DOMAIN_DECOMP ALLOC_DRV             ! domain decomposition, allocate global distributed arrays
 ATMDYN_SCM_COM ATMDYN_SCM MOMEN2ND  ! replace atmospheric dynamics with SCM routines
+ATMDYN_SCM_EXT ATM_UTILS
 SCM_COM SCMDATA_TWPICE              ! routines for reading and processing SCM forcings and IC's
 QUS_COM QUSDEF QUS_DRV              ! advection of tracers
 TQUS_DRV                            ! advection of Q
