33fb451ea2872fe65d1c1a81d1b221535453528c
kelley <kelley>
2009-05-23 02:38:46 +0000

    Introduced a 3D version of TRIDIAG_new for 3D arrays. This allows larger messages to be sent during the costly transpose steps.

--- model/solvers/TRIDIAG.f
+++ model/solvers/TRIDIAG.f
@@ -9,6 +9,7 @@
       Module procedure tridiag_2d_glob
 #ifndef OFFLINE_RUN
       Module procedure tridiag_2d_dist
+      Module procedure tridiag_3d_dist
 #endif
       End Interface
 
@@ -168,6 +169,88 @@ c     &     call stop_model("TRIDIAG: N > NMAX, increase NMAX",255)
 
       RETURN
       END SUBROUTINE TRIDIAG_2D_DIST
+
+      SUBROUTINE TRIDIAG_3D_DIST(A_dist, B_dist, C_dist, R_dist,
+     &                           U_dist,grid, j_lower, j_upper )
+!@sum  TRIDIAG  solves an array of tridiagonal matrix equations (A,B,C)U=R
+!@auth Numerical Recipes
+!@ver  1.0
+      USE DOMAIN_DECOMP_1D, ONLY : DIST_GRID
+      USE DOMAIN_DECOMP_1D, ONLY : TRANSP
+      IMPLICIT NONE
+
+      Type (DIST_GRID), Intent(IN) :: grid
+      REAL*8, INTENT(INOUT) :: A_dist(:,grid%j_strt_halo:,:)
+      REAL*8, INTENT(INOUT) :: B_dist(:,grid%j_strt_halo:,:)
+      REAL*8, INTENT(INOUT) :: C_dist(:,grid%j_strt_halo:,:)
+      REAL*8, INTENT(INOUT) :: R_dist(:,grid%j_strt_halo:,:)
+      REAL*8, INTENT(OUT)   :: U_dist(:,grid%j_strt_halo:,:)
+      INTEGER, INTENT(IN)   :: J_LOWER, J_UPPER
+
+      REAL*8, ALLOCATABLE :: A_tr(:,:,:), B_tr(:,:,:),
+     &                       C_tr(:,:,:), R_tr(:,:,:),
+     &                       U_tr(:,:,:)
+
+      REAL*8 :: BET                   !@var BET  work variable
+      REAL*8 :: GAM(grid%jm_world)        !@var GAM  work array
+
+      Integer :: i, j, l
+      Integer :: N, N_i, N_l
+
+
+! Determine the size of the global arrays
+      N = grid%jm_world
+      n_i = grid%ni_loc
+      n_l = size(A_dist,3)
+
+! Matrix size consistent with array size?
+      if ( J_upper > N ) then
+        print*, 'TRIDIAG: upper bound of matrix arrays is too large'
+        print*, 'j_upper = ', j_upper, 'jm =', n, ' ( need j_upper<=jm)'
+        call stop_model('TRIDIAG: j_upper argument too large', 255)
+      end if
+
+! Allocate the transposed arrays
+      allocate( a_tr(n_i,n,n_l), b_tr(n_i,n,n_l), c_tr(n_i,n,n_l) )
+      allocate( r_tr(n_i,n,n_l), u_tr(n_i,n,n_l) )
+
+! First get the transpose of A,B,C,R
+      call transp(grid, a_dist, a_tr)
+      call transp(grid, b_dist, b_tr)
+      call transp(grid, c_dist, c_tr)
+      call transp(grid, r_dist, r_tr)
+
+! Solve
+      do l=1,n_l
+        do i=1,n_i
+          BET=B_tr(i,j_lower,l)
+          IF (BET.eq.0) then
+           print*, "TRIDIAG_3D_DIST: DENOM. = ZERO  i,j= ", i,' 1'
+           stop
+          end if
+          U_tr(i,j_lower,l)=R_tr(i,j_lower,l)/BET
+          DO J=j_lower+1, j_upper
+            GAM(J)=C_tr(i,J-1,l)/BET
+            BET=B_tr(i,J,l)-A_tr(i,J,l)*GAM(J)
+            IF (BET.eq.0) then
+             print*, "TRIDIAG_3D_DIST: DENOM. = ZERO i,j= ", i, j
+             stop
+            end if
+          U_tr(i,J,l)=( R_tr(i,J,l) - A_tr(i,J,l)*U_tr(i,J-1,l) )/BET
+          END DO
+          DO J=j_upper-1,j_lower,-1
+            U_tr(i,J,l)=U_tr(i,J,l)-GAM(J+1)*U_tr(i,J+1,l)
+          END DO
+        end do
+      end do
+
+! Transfer the solution to the j-distributed array
+        call transp( grid, u_dist, u_tr, reverse=.true.)
+
+      deallocate( a_tr, b_tr, c_tr, r_tr, u_tr )
+
+      RETURN
+      END SUBROUTINE TRIDIAG_3D_DIST
 #endif
 
 
