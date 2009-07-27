376a8d9c7beb19d9a34dea867b6242d5c53b56f3
koch <koch>
2009-07-27 18:43:34 +0000

    Online  aerosol rundeck, 4x5 slushy, updated to use either  old historic aerosol emissions (imAER=3, aer_int_yr=1890 to 2000)  or AR5 emissions (imAER=5, aer_in_yr = 1850 OR 2000)

--- templates/E1TaerM20.R
+++ templates/E1TaerM20.R
@@ -1,8 +1,10 @@
 E1TaerM20.R GISS Model E  2004 modelE             Dorothy Koch  11/08
 
 E1TaerM20: Sample rundeck with interactive mass aerosols
-  including sulfate, sea salt, black and organic carbon
- Set imAER=3,1 for historic (1890 to 2000) or AeroCom emissions
+  including sulfate, nitrate, sea salt, black and organic carbon
+ Set imAER=3, aer_int_yr = 1890 to 2000 for historic 
+ Set imAER=5, aer_int_yr = 1850 or 2000 for AR5
+ Set imAER=1 for AEROCOM emissions (but this hasn't been tested lately)
  Set emission input files to match (see below)
  Using _E1 'slush' model since it is tested
 modelE 4x5 hor. grid with 20 lyrs, top at .1 mb (+ 3 rad.lyrs)
@@ -17,8 +19,9 @@ Preprocessor Options
 #define TRACERS_ON                  ! include tracers code
 #define TRACERS_WATER               ! tracers can interact with water
 #define TRACERS_DRYDEP              ! include tracer dry deposition
-#define TRACERS_AEROSOLS_Koch    ! Dorothy Koch's tracers (aerosols, etc)
+#define TRACERS_AEROSOLS_Koch       ! Dorothy Koch's tracers (aerosols, etc)
 #define BC_ALB                      !optional tracer BC affects snow albedo
+#define TRACERS_NITRATE
 !#define BIN_TRACERS
 End Preprocessor Options
 
@@ -27,8 +30,7 @@ RES_M20AT DIAG_RES_M FFT72          ! horiz/vert resolution, 4x5deg, 20 layers -
 MODEL_COM GEOM_B IORSF              ! model variables and geometry
 TRIDIAG                             ! tridiagonal matrix solver
 MODELE                              ! Main and model overhead
-                                    ! parameter database
-              ALLOC_DRV             ! domain decomposition, allocate global distributed arrays
+ALLOC_DRV             ! domain decomposition, allocate global distributed arrays
 ATMDYN_COM ATMDYN MOMEN2ND          ! atmospheric dynamics
 ATM_UTILS
 QUS_COM QUSDEF QUS_DRV              ! advection of tracers
@@ -38,14 +40,19 @@ TRDRYDEP                            ! tracer dry deposition from Harvard CTM
 TRACERS                             ! generic tracer code
 TRDIAG_COM TRACER_PRT               ! tracer diagnostic printout
 CLOUDS2_E1 CLOUDS2_DRV CLOUDS_COM   ! clouds modules
+!CLOUDS2 CLOUDS2_DRV CLOUDS_COM   ! clouds modules
 ! ---TRACER SPECIFIC CODES----------
 TRACERS_AEROSOLS_Koch_e4            ! BC/OC/sulfate/seasalt
+TRACER_NITRATE                      ! Nitrate aerosol
+TRAMP_eqsam_v03d                    ! EQSAM module for inorganic aerosol thermodynamic equilibrium
 ! ---------------------------------
 SURFACE FLUXES                      ! surface calculation and fluxes
 GHY_COM GHY_DRV GHY GHY_H           ! land surface and soils
 VEG_DRV VEG_COM VEGETATION          ! vegetation
 PBL_COM PBL_DRV PBL_E1                ! atmospheric pbl
+!PBL_COM PBL_DRV PBL                ! atmospheric pbl
 ATURB_E1                               ! turbulence in whole atmosphere
+!ATURB                               ! turbulence in whole atmosphere
 LAKES_COM LAKES                     ! lake modules
 SEAICE SEAICE_DRV                   ! seaice modules
 LANDICE LANDICE_DRV                 ! land ice modules
@@ -53,6 +60,7 @@ ICEDYN_DRV ICEDYN                   ! ice dynamics modules
 OCEAN OCNML                         ! ocean modules
 SNOW_DRV SNOW                       ! snow model
 RAD_COM RAD_DRV_E1 RADIATION_E1           ! radiation modules
+!RAD_COM RAD_DRV RADIATION           ! radiation modules
 RAD_UTILS ALBEDO                    ! radiation and albedo
 DIAG_COM DIAG DEFACC DIAG_PRT       ! diagnostics
 DIAG_ZONAL GCDIAGb                  ! grid-dependent code for lat-circle diags
@@ -89,7 +97,7 @@ RADN2=radfil33k      ! 8/2003 version
 RADN3=miescatpar.abcdv2
 !RADN2=LWTables33k.1a              ! rad.tables and history files
 !RADN4=LWTables33k.1b              ! rad.tables and history files
-!RADN5=H2Ocont_Ma_2000             ! H2O continuum table
+RADN5=H2Ocont_Ma_2000             ! H2O continuum table
 ! other available H2O continuum tables:
 !    RADN5=H2Ocont_Ma_2004
 !    RADN5=H2Ocont_Roberts
@@ -100,12 +108,12 @@ TAero_SUI=sep2003_SUI_Koch_kg_m2_72x46x9_1875-1990 ! industrial sulfates
 TAero_OCI=sep2003_OCI_Koch_kg_m2_72x46x9_1875-1990 ! industrial organic carbons
 TAero_BCI=sep2003_BCI_Koch_kg_m2_72x46x9_1875-1990 ! industrial black carbons
 ! MADAER=3 needs: (temporary version)
-! TAero_SUL=SUL_Koch2008_kg_m2_72x46x20_1890-2000
-! TAero_SSA=SSA_Koch2008_kg_m2_72x46x20
-! TAero_NIT=NIT_Bauer2008_kg_m2_72x46x20_1890-2000
-! TAero_OCA=OCA_Koch2008_kg_m2_72x46x20_1890-2000
-! TAero_BCA=BCA_Koch2008_kg_m2_72x46x20_1890-2000
-! TAero_BCB=BCB_Koch2008_kg_m2_72x46x20_1890-2000
+!TAero_SUL=SUL_Koch2008_kg_m2_72x46x20_1890-2000
+!TAero_SSA=SSA_Koch2008_kg_m2_72x46x20
+!TAero_NIT=NIT_Bauer2008_kg_m2_72x46x20_1890-2000
+!TAero_OCA=OCA_Koch2008_kg_m2_72x46x20_1890-2000
+!TAero_BCA=BCA_Koch2008_kg_m2_72x46x20_1890-2000
+!TAero_BCB=BCB_Koch2008_kg_m2_72x46x20_1890-2000
 RH_QG_Mie=oct2003.relhum.nr.Q633G633.table
 RADN6=dust_mass_CakmurMillerJGR06_72x46x20x7x12
 RADN7=STRATAER.VOL.1850-1999.Apr02
@@ -148,24 +156,55 @@ LAI10=chem_files/lai10.global
 LAI11=chem_files/lai11.global
 LAI12=chem_files/lai12.global
 Ox_IC=gsin/Ox_init_cond_M23_4x5 !see README in /usr/people/cmrun/gsin
-! ------- Dorothy's inputs needed for imAER= 1 or 3 -----------
+! ------- Dorothy's inputs needed for imAER= 1,3,5 -----------
 SO2_VOLCANO=SO2_volc_conti2000.AEROCOM_FEB12
+! ------- Dorothy's inputs needed for imAER= 1 or 3 -----------
 !SO2_BIOMASS=SO2_bio2000.AEROCOM_DEC03
-BC_SHIPS=Shipping_BC_2000_4x5.bin
-POM_SHIPS=Shipping_POM_2000_4x5.bin
-SO2_SHIPS=Shipping_SO2_2000_4x5.bin
-! --------Dorothy's inputs needed for imAER = 3 --------------
+! ------- Dorothy's inputs needed for imAER= 3 -----------
+!BC_SHIPS=Shipping_BC_2000_4x5.bin
+!POM_SHIPS=Shipping_POM_2000_4x5.bin
+!SO2_SHIPS=Shipping_SO2_2000_4x5.bin
+! --------Dorothy's inputs needed for imAER = 3 or 5 --------------
 DMS_SEA=DMS_Night_4x5
 AER_CHEM=Sulf_chem_drewE_20
 AER_OH_STRAT=Strat_OH_drewE_20
 TERPENE=terp_Guenther_4x5
-BC_BIOMASS=BC_GFED_97-06_4x5
-OC_BIOMASS=OC_GFED_97-06_4x5
-SO2_AIRCRAFT=NOy_sources/aircraft_4x5_1940-2000 ! zero in 1940 and before.
-OC_INDh=OC_Bond_Feb09_4x5_h_1850-2000 !BC/OC Bond  
-BC_INDh=BC_Bond_Feb09_4x5_h_1850-2000
-SO2_INDh=SO2_EDGAR_Feb09_4x5_h_1890-2000
-! -------Dorothy's inputs for imAER=1 AeroCom ---------
+! ------Dorothy's inputs needed for imAER = 3 -old historic emissions------------
+!SO2_EM_1=SO2_EDGAR_Feb09_4x5_h_1890-2000 
+!OC_EM_1=OC_Bond_Feb09_4x5_h_1850-2000 !BC/OC Bond  
+!BC_EM_1=BC_Bond_Feb09_4x5_h_1850-2000
+!BC_BIOMASS=BC_GFED_97-06_4x5
+!OC_BIOMASS=OC_GFED_97-06_4x5
+!SO2_AIRCRAFT=NOy_sources/aircraft_4x5_1940-2000 ! zero in 1940 and before.
+!--------Dorothy's inputs for imAER=5 ---AR5 emissions-----------------
+!   for now all years in this block must be set to either 1850 or 2000
+BC_EM_1=BCII_awb_AR5_2000_4x5_h
+BC_EM_2=BCII_dom_AR5_2000_4x5_h
+BC_EM_3=BCII_ene_AR5_2000_4x5_h
+BC_EM_4=BCII_ind_AR5_2000_4x5_h
+BC_EM_5=BCII_tra_AR5_2000_4x5_h
+BC_EM_6=BCII_wst_AR5_2000_4x5_h
+BC_EM_7=BCII_shp_AR5_2000_4x5_h
+OC_EM_1=OCII_awb_AR5_2000_4x5_h
+OC_EM_2=OCII_dom_AR5_2000_4x5_h
+OC_EM_3=OCII_ene_AR5_2000_4x5_h
+OC_EM_4=OCII_ind_AR5_2000_4x5_h
+OC_EM_5=OCII_tra_AR5_2000_4x5_h
+OC_EM_6=OCII_wst_AR5_2000_4x5_h
+OC_EM_7=OCII_shp_AR5_2000_4x5_h
+SO2_EM_1=SO2_shp_AR5_2000_4x5_h
+SO2_EM_2=SO2_awb_AR5_2000_4x5_h
+SO2_EM_3=SO2_dom_AR5_2000_4x5_h
+SO2_EM_4=SO2_ind_AR5_2000_4x5_h
+SO2_EM_5=SO2_tra_AR5_2000_4x5_h
+SO2_EM_6=SO2_wst_AR5_2000_4x5_h
+BCB_EM_1=BCB_forestfire_AR5_2000_4x5_h
+BCB_EM_2=BCB_grassfire_AR5_2000_4x5_h
+OCB_EM_1=OCB_forestfire_AR5_2000_4x5_h
+OCB_EM_2=OCB_grassfire_AR5_2000_4x5_h
+SO2B_EM_1=SO2_forestfire_AR5_2000_4x5_h
+SO2B_EM_2=SO2_grassfire_AR5_2000_4x5_h
+! -------Dorothy's inputs for imAER=1 AeroCom (not tested lately)---------
 !SO2_VOLCANO_EXP=SO2_volc_expl1750.AEROCOM
 !TERPENE=SOA_2000.AEROCOM_DEC03
 !BC_BIOMASS=BC_fire2000.AEROCOM_DEC03
@@ -177,6 +216,24 @@ SO2_INDh=SO2_EDGAR_Feb09_4x5_h_1890-2000
 !SO2_IND=SO2_ind2000.AEROCOM_DEC03
 !SALT1=SALT_bin1_2000_new.nc
 !SALT2=SALT_bin2_2000_new.nc
+!-----------------------------------------------
+!  AEROSOL INPUT NITRATE oxidants, fields use for imAER=3,5
+O3_FIELD=Ox_3D_field_bell
+OFFLINE_HNO3.nc=HNO3_E70_GISS4x5.nc
+OFFLINE_SEAS.nc=SEASALT_EK1su_GISS4x5.nc
+!------- for imAER=3 ---------
+!NH3_CON_01=NH3hCON_IIASA_CLE_Apr09_4x5_h_1890-2030
+!NH3_CYC_01=NH3hCYC_IIASA_CLE_Apr09_4x5_h_1890-2030
+!------- for imAER=5 -----set date to 1850 or 2000-----
+NH3_CON_01=NH3hCON_OCEAN_Apr09_4x5_h
+NH3_CON_02=NH3_forestfire_AR5_2000_4x5_h
+NH3_CON_03=NH3_grassfire_AR5_2000_4x5_h
+NH3_CON_04=NH3_agr_AR5_2000_4x5_h
+NH3_CON_05=NH3_awb_AR5_2000_4x5_h
+NH3_CON_06=NH3_dom_AR5_2000_4x5_h
+NH3_CON_07=NH3_ind_AR5_2000_4x5_h
+NH3_CON_08=NH3_ene_AR5_2000_4x5_h
+NH3_CON_09=NH3_tra_AR5_2000_4x5_h
 
 
 Label and Namelist:
@@ -263,10 +320,11 @@ ndaa=13
 nda5k=13
 nda4=48         ! to get daily energy history use nda4=24*3600/DTsrc
 nssw=2          ! until diurnal diagn. are fixed, nssw should be even
+!MADAER=3
   
 !--------- Aerosol parameters----------------
-imAER=3         !3 historic; 1 AEROCOM ; 0,2 for standard or sector inputs (not working)
-aer_int_yr=0    !used for imAER=3, select desired year (1890 to 2000) or 0 to use JYEAR
+imAER=5         !3 historic; 1 AEROCOM ; 0,2 for standard or sector inputs (not working)
+aer_int_yr=2000    !used for imAER=3,5 select desired year (1890 to 2000) or 0 to use JYEAR
 rad_interact_tr=1  ! 1=couples aerosols to radiation, 0=use climatology
 rad_forc_lev=1     ! 0 for TOA, 1 for tropopause for rad forcing diags.
 prather_limits=1   ! to avoid some negative tracers in sub-gridscale
@@ -280,7 +338,7 @@ aircrafts_Tyr2=2000 !          set these two equal or omit them.
  &INPUTZ
    QCHECK=.false.
    kdiag = 0,0,0,0,0,0,0,0,0,0,0,0,0,
-   YEARI=1999,MONTHI=12,DATEI=1,HOURI=0, IYEAR1=1999 ! or earlier
-   YEARE=1999,MONTHE=12,DATEE=2,HOURE=0,     
-   ISTART=2,IRANDI=0, YEARE=1999,MONTHE=12,DATEE=1,HOURE=1,
+   YEARI=1998,MONTHI=12,DATEI=1,HOURI=0, IYEAR1=1998 ! or earlier
+   YEARE=2001,MONTHE=1,DATEE=1,HOURE=0,     
+   ISTART=2,IRANDI=0, YEARE=1998,MONTHE=12,DATEE=1,HOURE=1,
  &END
