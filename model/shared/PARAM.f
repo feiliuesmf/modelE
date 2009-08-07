4a4f8435d1eaab5a8c0d1815220f5bc6fba1bac6
kostas <kostas>
2009-08-07 17:51:00 +0000

    Increasing total integers a bit

--- model/shared/PARAM.f
+++ model/shared/PARAM.f
@@ -72,7 +72,7 @@
 
       integer, parameter :: MAX_PARAMS = 310
       integer, parameter :: MAX_RPARAMS = 270
-      integer, parameter :: MAX_IPARAMS = 340
+      integer, parameter :: MAX_IPARAMS = 350
       integer, parameter :: MAX_CPARAMS = 200
       integer, parameter :: MAX_NAME_LEN = 32
       integer, parameter :: MAX_CHAR_LEN = 128
