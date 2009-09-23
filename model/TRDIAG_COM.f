3b41cc56f7af095391bd1467f30bd19b888b1b2a
faluvegi <faluvegi>
2009-09-23 02:53:51 +0000

    In order to get sample deck: E4TcadF40 to run setup, I needed to increase ktajls from 400-->810! and ktaijs from 1223-->1343. Lots of tracers. The comments asked me not to bother with preprocessor directives, and just increase the values as needed, so I did.

--- model/TRDIAG_COM.f
+++ model/TRDIAG_COM.f
@@ -88,7 +88,7 @@ C**** TAIJS  <<<< KTAIJS and IJTS_xx are Tracer-Dependent >>>>
 
 !@param KTAIJS number of special lat/lon tracer diagnostics
 !@+   please just increase this if needed - don't bother with pp options
-      INTEGER,PARAMETER :: ktaijs=1223
+      INTEGER,PARAMETER :: ktaijs=1343
 
 !@param MaxSubCl Maximum number of sub classes of tracers for rad. diagnostics
       INTEGER,PARAMETER :: MaxSubCl=4
@@ -235,7 +235,7 @@ C**** TAJLN
 C**** TAJLS  <<<< KTAJLS and JLS_xx are Tracer-Dependent >>>>
 !@param ktajls number of source/sink TAJLS tracer diagnostics;
 !@+   please just increase this if needed - don't bother with pp options
-      INTEGER,PARAMETER :: ktajls=400
+      INTEGER,PARAMETER :: ktajls=810
 
 !@var jls_XXX index for non-tracer specific or special diags
       INTEGER jls_OHconk,jls_HO2con,jls_NO3
