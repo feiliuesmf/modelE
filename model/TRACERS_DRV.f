be9aab36e684ada72e7802099cbbae5665662f32
tclune <tclune>
2009-12-02 17:00:25 +0000

    Another typo ...

--- model/TRACERS_DRV.f
+++ model/TRACERS_DRV.f
@@ -7635,7 +7635,7 @@ C**** 3D tracer-related arrays but not attached to any one tracer
 #if (defined TRACERS_AEROSOLS_Koch) || (defined TRACERS_OM_SP) ||\
     (defined TRACERS_AMP)
       USE AEROSOL_SOURCES, only: DMSinput,BCI_src,OCI_src,
-     * BCB_src,OCB_src
+     * BCB_src,OCB_src,
 #ifndef TRACERS_AEROSOLS_SOA
      * OCT_src,
 #endif  /* TRACERS_AEROSOLS_SOA */
