f4d7e627ebc8c76fe7006e89de46a3bd2d1ff0c1
tclune <tclune>
2009-12-02 16:19:51 +0000

    Customizing OMP header for varying configuration options (e.g. TRACERS_ON).

--- model/SURFACE.f
+++ model/SURFACE.f
@@ -364,7 +364,10 @@ C****
 #endif
 !$OMP*  ) 
 !$OMP&  SHARED(J_0, J_1, I_0, IMAXJ, FLAND, FLICE, RSI,P, PEDN,
-!$OMP&   PEK,PK,Q,T,JREG, AM,ntx, ntix, itime_tr0, itime,
+!$OMP&   PEK,PK,Q,T,JREG, AM, itime_tr0, itime,
+#ifdef TRACERS_ON
+!$OMP&    ntx, ntix, 
+#endif
 !$OMP&   needtrs,trm,byam,byaxyp,ijdd,idd_pt5,idd_q5,adiurn,
 !$OMP&   idx1,MODDD,IDD_SPR,ih, aij,FOCEAN)
 !$OMP*  SCHEDULE(DYNAMIC,2)
