8503a798140a58a44374d9e771854ab43d8ad273
denis <denis>
2009-12-16 04:33:42 +0000

    * regridding other natural emissions to CS90 * converting netcdf output files to GISS format and adding Greg's header

--- aux/regrid_IPCC_emissions.sh
+++ aux/regrid_IPCC_emissions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Regridding IPCC emissions files to the C90 cubed sphere grid
+# You must run this script on a DALI node 
 #cp /discover/nobackup/projects/giss/prod_input_files/IPCC_emissions*_1850_0.5x0.5_v1_20_04_2009.nc .
 
 #- cleanup
@@ -7,6 +8,17 @@ rm -f paraffin_anthropogenic.nc propane+pentanes_anthropogenic.nc propane+pentan
 rm -f propane+pentanes+butanes+hexanes_ships.nc propane+pentanes_ships.nc propene+oalkenes_anthropogenic.nc propene+oalkenes_Biomass.nc
 rm -f propene+oalkenes_ships.nc alkenes_anthropogenic.nc alkenes_Biomass.nc alkenes_ships.nc 
 rm -f butanes+hexanes_* ethane+ketones_*nc propane+pentanes+butanes+hexanes_Biomass.nc paraffin_Biomass.nc
+rm -f paraffin_ships.nc
+
+#-- Gas tracers - initial conditions
+./remap.pl -par ICgastracers.par -in N2O_IC_M23_4x5_6.17_conc_2x2.5_conc -out N2O_IC_from_2x2.5_C90_Dec_2009 
+./remap.pl -par ICgastracers.par -in CFC_IC_M23_4x5_6.17_conc_2x2.5_conc -out CFC_IC_from_2x2.5_C90_Dec_2009 
+./remap.pl -par ICgastracers.par -in CH4_IC_M23_4x5_6.17_conc_2x2.5_conc -out CH4_IC_from_2x2.5_C90_Dec_2009 
+./remap.pl -par ICgastracers.par -in Ox_init_cond_M23_4x5_conc_2x2.5_conc -out Ox_IC_from_2x2.5_C90_Dec_2009 
+./remap.pl -par ICgastracers.par -in CO_init_cond_M23_conc_2x2.5_conc -out CO_IC_from_2x2.5_C90_Dec_2009 
+./remap.pl -par ICOxref.par -in O3ref_O3JDAY_1850_182.dat -out O3ref_O3JDAY_1850_from_2x2.5_C90_Dec_2009  #check this one
+./remap.pl -par ICsulfate.par -in sulfate_pi_fakeM23_M_SA_2x2.5gf -out sulfate_from_2x2.5_C90_Dec_2009
+
 
 #-- CH4 Natural sources - have been adjusted and rescaled during pre-processing step in /discover/nobackup/gfaluveg/PRE/make_1x1_nat_sources/CH4
 ./remap.pl -par regridCH4natural.par -in CH4WETL+TUNDRA_1X1_temp -out CH4WETL+TUNDRA_C90_Dec_2009
@@ -44,7 +56,7 @@ ncflint -c -v grassfire,forestfire -w 1.0,0.03571428571428571428 propene+oalkene
 
 ./remap.pl -par ncregrid-ijl.par -in alkenes_anthropogenic.nc -out IPCC_emissions_alkenes_anthropogenic_1850_C90_Dec_2009.nc
 
-./remap.pl -par ncregrid-ijl.par -in alkenes_ships.nc -out IPCC_emissions_alkenes_ships.nc
+./remap.pl -par ncregrid-ijl.par -in alkenes_ships.nc -out IPCC_emissions_alkenes_ships_1850_C90_Dec_2009.nc
 
 ./remap.pl -par ncregrid-ijl.par -in alkenes_Biomass.nc -out IPCC_GriddedBiomassBurningEmissions_alkenes_decadalmonthlymean1850_C90_Dec_2009.nc
 
@@ -69,8 +81,18 @@ ncflint -c -v grassfire,forestfire -w 1.0,1.0 propane+pentanes_Biomass.nc butane
 ncflint -c -v grassfire,forestfire -w 1.0,1.0 propane+pentanes+butanes+hexanes_Biomass.nc ethane+ketones_Biomass.nc -o paraffin_Biomass.nc
 
 # then do regridding
-./remap.pl -par ncregrid-ijl.par -in paraffin_ships.nc -out IPCC_emissions_paraffin_shups_1850_C90_Dec_2009.nc
+./remap.pl -par ncregrid-ijl.par -in paraffin_ships.nc -out IPCC_emissions_paraffin_ships_1850_C90_Dec_2009.nc
 
 ./remap.pl -par ncregrid-ijl.par -in paraffin_anthropogenic.nc -out IPCC_emissions_paraffin_anthropogenic_1850_C90_Dec_2009.nc
 
 ./remap.pl -par ncregrid-ijl.par -in paraffin_Biomass.nc -out IPCC_GriddedBiomassBurningEmissions_paraffin_decadalmonthlymean1850_C90_Dec_2009.nc
+
+#-- convert to GISS format and add headers
+    module avail tool/idl
+    module load tool/idl-6.4
+    ulimit -s 6000000
+    ulimit -v unlimited
+    cd /gpfsm/dnb53/gfaluveg/AR5_emissions/v5_anthro
+    ./run_1850_anthro_CS.ksh
+    ./run_1850_ships_CS.ksh
+    ./run_1850_BiomassBurning_CS.ksh
