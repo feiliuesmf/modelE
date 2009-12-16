d5fbc9f60960d5cbaa8fb1a50d8888ba9310566d
denis <denis>
2009-12-16 04:37:02 +0000

    regridding initial conditions for gas tracers to the cubed sphere

--- aux/regrid_IPCC_emissions.sh
+++ aux/regrid_IPCC_emissions.sh
@@ -10,7 +10,7 @@ rm -f propene+oalkenes_ships.nc alkenes_anthropogenic.nc alkenes_Biomass.nc alke
 rm -f butanes+hexanes_* ethane+ketones_*nc propane+pentanes+butanes+hexanes_Biomass.nc paraffin_Biomass.nc
 rm -f paraffin_ships.nc
 
-#-- Gas tracers - initial conditions
+#-- Gas tracers - initial conditions -
 ./remap.pl -par ICgastracers.par -in N2O_IC_M23_4x5_6.17_conc_2x2.5_conc -out N2O_IC_from_2x2.5_C90_Dec_2009 
 ./remap.pl -par ICgastracers.par -in CFC_IC_M23_4x5_6.17_conc_2x2.5_conc -out CFC_IC_from_2x2.5_C90_Dec_2009 
 ./remap.pl -par ICgastracers.par -in CH4_IC_M23_4x5_6.17_conc_2x2.5_conc -out CH4_IC_from_2x2.5_C90_Dec_2009 
