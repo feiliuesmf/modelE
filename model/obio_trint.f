d19505f7997243dfdd18d8b6765b7661e9d83298
tclune <tclune>
2010-02-03 17:28:57 +0000

    Modified to perform distributed global sums rather than gather + serial global sum.  Results should be independent of process count.

--- model/obio_trint.f
+++ model/obio_trint.f
@@ -1,79 +1,115 @@
 #include "rundeck_opts.h"
 
-#ifdef OBIO_ON_GARYocean
       subroutine obio_trint
-#else
-      subroutine obio_trint(nn)
-#endif
 
 #ifdef OBIO_ON_GARYocean
-      Use OCEANRES,  only: idm=>imo,jdm=>jmo,kdm=>lmo,dZO
-      USE OCEAN, only : focean
-      USE OCEANR_DIM, only : ogrid
-      USE obio_com, only: tracer       !use global array
-      USE OCN_TRACER_COM, only : ntrcr=>ntm
-      USE MODEL_COM, only : nstep=>itime
-      USE GEOM, only : dxyp
+      use oceanres,  only: idm=>imo,jdm=>jmo,kdm=>lmo
+      use oceanr_dim, only : ogrid
+      use obio_com, only: tracer       !use global array
+      use ocn_tracer_com, only : ntrcr=>ntm
+      use model_com, only : nstep=>itime
 #else
-      USE hycom_dim_glob, only : jj,isp,ifp,ilp,kk,ntrcr,idm,jdm,kdm
-      USE hycom_arrays_glob, only : tracer,dpinit,scp2
-      USE hycom_scalars, only : nstep,huge
+      use hycom_dim_glob, only : ntrcr,idm,jdm,kdm
+      use hycom_dim, only: ogrid, ifu, ilu, isu
+      use hycom_arrays, only : tracer,dpinit,scp2
+      use hycom_scalars, only : nstep,huge
 #endif
-
-      USE DOMAIN_DECOMP_1D, only: AM_I_ROOT,GLOBALSUM 
-
+      use domain_decomp_1d, only: am_i_root, globalsum, get
 
       implicit none
 
       integer i,j,k,l,kn,nn
       integer ntr
-      real sumo,sum_area,summ
-      real :: arraySum
-      real :: sumtrac(idm,jdm,kdm)
+      real sumo
 
-#ifndef OBIO_ON_GARYocean
-      call gather_dpinit   !ONLY for hycom
-#endif
+      integer :: iTracer
+      integer :: j_0, j_1, j_0h, j_1h
 
-      !!! need to gather tracer etc. before calling this routine
-      if ( AM_I_ROOT() ) then
+      real*8, allocatable :: summ(:)
+      real*8, allocatable :: partialTracerSums(:,:)
 
-      do ntr=1,ntrcr
-      summ=0.
-      sumo=0.
-        do k=1,kdm
-        sum_area=0.
-        do j=1,jdm
-        do i=1,idm
+      call get(ogrid, j_strt = j_0, j_stop = j_1,
+     &     j_strt_halo = j_0h, j_stop_halo = j_1h)
+
+      sumo = getTotalOceanVolume()
+
+      ! Form partial sum for each latitude for each tracer
+      allocate(partialTracerSums(j_0h:j_1h, ntrcr))
+      do iTracer = 1, ntrcr
+         partialTracerSums(:,iTracer) = 
+     &        partialIntegration(tracer(:,:,:,iTracer))
+      end do
+
+      allocate(summ(ntrcr))
+      call globalSum(ogrid, partialTracerSums, summ)
+
+      if (am_i_root()) then
+         do iTracer = 1, ntrcr
+            write(*,*) 'total intgrl tracer:', iTracer, nstep, 
+     &           summ(iTracer), summ(iTracer)/sumo
+         end do
+      end if
+
+      deallocate(partialTracerSums, summ)
+
+      contains
+
 #ifdef OBIO_ON_GARYocean
-         kn=k
-         if (FOCEAN(i,j).gt.0) then
-           sumo=sumo+dzo(kn)*dxyp(j)     !sum volume
-           sum_area=sum_area+dxyp(j)     !sum area
-           summ=summ+dzo(kn)*tracer(i,j,k,ntr)*dxyp(j)   !sum over volume
-         endif !focean
+      function partialIntegration(quantity)
+      use geom, only : dxyp
+      use oceanres, only: dzo
+      real*8, intent(in) :: quantity(:,j_0h:,:)
+      real*8 :: partialIntegration(j_0h:j_1h)
+      
+      real*8 :: gridCellVolume
+
+      partialIntegration = 0
+      do k = 1, kdm
+         do j = j_0, j_1
+            gridCellVolume = dzo(k) * dxyp(j)
+            do i= 1, size(quantity, 1)
+               partialIntegration(j) = partialIntegration(j) + 
+     &              quantity(i,j,k) * gridCellVolume
+            end do
+         end do
+      end do
+
+      end function partialIntegration
 #else
-         kn=k+nn
-         if (dpinit(i,j,k)<huge) then
-           sumo=sumo+dpinit(i,j,k)*scp2(i,j)
-           sum_area=sum_area+scp2(i,j)
-           summ=summ+dpinit(i,j,k)*tracer(i,j,k,ntr)*scp2(i,j)
-          if (nstep.eq.97.and.ntr.eq.1)
-     .     write(*,*)'obio_trint: ',
-     .         nstep,i,j,k,ntr,
-     .         dpinit(i,j,k),scp2(i,j),tracer(i,j,k,ntr)
-         endif
+      function partialIntegration(quantity)
+      real*8, intent(in) :: quantity(:,j_0h:,:)
+      real*8 :: partialIntegration(j_0h:j_1h)
+      
+      integer :: i, j, l, k
+
+      partialIntegration = 0
+      do k = 1, kdm
+         do j = j_0, j_1
+            do l = 1, isu(j)
+               do i= ifu(j,l), ilu(j,l)
+                  partialIntegration(j) = partialIntegration(j) + 
+     &                 quantity(i,j,k)) * dpinit(i,j,j)*scp2(i,j)
+               end do
+            end do
+         end do
+      end do
+
+      end function partialIntegration
 #endif
-          
-        enddo
-        enddo
-        enddo
-      write(*,*)
-     .  'total intgrl tracer:',ntr,nstep,summ,summ/sumo
-      enddo
-      print*,'   '
 
-      endif   !if am_i_root
+      real*8 function getTotalOceanVolume() result(oceanVolume)
+      real*8, allocatable :: partialOceanVolumes(:)
+      real*8, allocatable :: ones(:,:,:)
 
-      return
-      end
+      allocate(partialOceanVolumes(j_0h:j_1h))
+      allocate(ones(idm, J_0H:J_1H, kdm))
+      
+      ones = 1
+      partialOceanVolumes(:) = partialIntegration(ones)
+      call globalSum(ogrid, partialOceanVolumes, oceanVolume)
+
+      deallocate(ones)
+
+      end function getTotalOceanVolume
+
+      end subroutine obio_trint
