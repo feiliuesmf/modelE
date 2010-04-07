b0916fa974c6250d599cb27b203ed230995a3812
denis <denis>
2010-04-07 03:09:21 +0000

    high res SOIL file: extension to the ocean and various other fixes

--- aux/convSOIL.f
+++ aux/convSOIL.f
@@ -13,10 +13,11 @@ c the slope sl and layer thickness originate from S360X180_0098M.rep
 c
 c     
       integer, parameter :: imf=4320, jmf=2160,nx=1440,ny=600
+      real*4, parameter :: skip =0.0
       real*4 :: dz(360,180,6),
      &     ftext(360,180,5,6),      ! coarse grid, 5 textures, 6 layers
      &     ftextk(360,180,5,6),     ! coarse grid, 5 textures, 6 layers
-     &     sl(360,180),sum(360,180,6),sum_rock_peat(360,180,6),
+     &     sl(360,180),summ(360,180,6),sum_rock_peat(360,180,6),
      &     ftext_fine(imf,jmf,3,2), ! fine grid, 3 textures, 2 layers
      &     sumfine(imf,jmf),landclss(imf,jmf),
      &     sand_top(imf,jmf),silt_top(imf,jmf),clay_top(imf,jmf),
@@ -31,15 +32,28 @@ c
      &     peat_lay(720,360,6),
      &     rock_lay(720,360,6),
      &     sl_targ(720,360),dz_targ(720,360,6),
-     &     ftext_targ(720,360,5,6)
-      real*4 :: ftext_gldas(nx,ny,3),fext_coarse(360,180)
+     &     ftext_targ(720,360,5,6),ftext_targ_n(720,360,5,6),
+     &     sumtext(720,360,6),
+     &     f2X2H(144,90,5,6),
+     &     dz2X2H(144,90,6),
+     &     sl2X2H(144,90)
+
+      real*4 :: ftext_gldas(nx,ny,3),fext_coarse(360,180),
+     &     focean(720,360),fland(720,360)
       real*4 :: ftext_gldas_extend(nx,720,3),alpha
 
-      character*80 title,title2,title3,title4,title5,
-     &     name,nameout,nameout2,nameout3,nameout4,nameout5
-      integer iu_SOIL,iuout,iuout2,iuout3,iuout4,iuout5,iuout6,i,k,
-     &    iby2,jby2,iby3,jby3,iby4,jby4,icoarse,jcoarse
+      character*80 title,title2,title3,title4,title5,title7,
+     &     name,nameout,nameout2,nameout3,nameout4,nameout5,nameout7
+      integer iu_SOIL,iuout,iuout2,iuout3,iuout4,iuout5,iuout6,iuout7,
+     &    iby2,jby2,iby3,jby3,iby4,jby4,icoarse,jcoarse,i,k
       character*1 :: kch,ich
+
+
+
+      open(iu_TOPO,FILE='ZHXH00',FORM='unformatted',STATUS='old')
+
+      read(iu_TOPO) title,focean
+      read(iu_TOPO) title,fland
 
       write(*,*) "ims,jms,nts",ims,jms,nts
 
@@ -90,13 +104,13 @@ c
      &     FORM='unformatted', STATUS="UNKNOWN")
       
       do k=1,6
-         sum(:,:,k)=0.
+         summ(:,:,k)=0.
          write(kch,'(i1)') k
          do i=1,5
-            sum(:,:,k)=sum(:,:,k)+ftext(:,:,i,k)
+            summ(:,:,k)=summ(:,:,k)+ftext(:,:,i,k)
          enddo
          title2="sum textures for layer " // kch
-         write(unit=iuout2) title2,sum(:,:,k)-1.0
+         write(unit=iuout2) title2,summ(:,:,k)-1.0
       enddo
       close(iuout2)
 
@@ -108,8 +122,8 @@ c
       
       do k=1,6
          write(kch,'(i1)') k
-         sum_rock_peat(:,:,k)= !ftext(:,:,4,k)+
-     & ftext(:,:,5,k)
+         sum_rock_peat(:,:,k)= ftext(:,:,4,k)+
+     &        ftext(:,:,5,k)
          title3="peat + bedrock for layer " // kch
          write(unit=iuout3) title3,sum_rock_peat(:,:,k)
       enddo
@@ -402,8 +416,11 @@ c
       open( iuout5, FILE=nameout5,
      &     FORM='unformatted', STATUS="UNKNOWN")
             
-      sumfixed= sand_lay(:,:,1)+silt_lay(:,:,1)+clay_lay(:,:,1)
-     &     +peat_lay(:,:,1)+rock_lay(:,:,1)
+      sumfixed= peat_lay(:,:,1)
+
+c sand_lay(:,:,5)
+c     &     +silt_lay(:,:,5)+clay_lay(:,:,5)
+c     &     +peat_lay(:,:,5)+rock_lay(:,:,5)
 
       title5="sum textures for top layer"
       write(unit=iuout5) title5,sumfixed
@@ -415,13 +432,148 @@ c
       ftext_targ(:,:,4,:)=peat_lay(:,:,:)
       ftext_targ(:,:,5,:)=rock_lay(:,:,:)
 
+c     extend 
+      do k=1,6
+         do j=1,360
+            do i=1,720
+               ftext_targ_n(i,j,:,k)=ftext_targ(i,j,:,k)
+               if (sum(ftext_targ(i,j,:,k)) .lt. 1. .or. 
+     &         focean(i,j) .gt. 0. ) then
+                  call getnn(720,360,ftext_targ(:,:,1,k),skip,i,j,
+     &                 in,jn,nd)
+c      write(*,*) i,j,' filled from',in,jn,ftext_targ(in,jn,1,k),nd
+                  do l=1,5
+                     ftext_targ_n(i,j,l,k)=ftext_targ(in,jn,l,k)
+     &                   /sum(ftext_targ(in,jn,:,k)) 
+                  enddo
+               endif
+               do l=1,5
+               ftext_targ_n(i,j,l,k)=
+     &                      min(max(ftext_targ_n(i,j,l,k),0.0),1.0)
+               enddo
+               sumtext(i,j,k)=sum(ftext_targ_n(i,j,:,k))
+               do l=1,5
+               ftext_targ_n(i,j,l,k)=ftext_targ_n(i,j,l,k)
+     &                   /sumtext(i,j,k)
+               enddo
+            enddo
+         enddo
+      enddo
+
       iuout6=205
       nameout5="SOIL720X360_Reynolds"
       open( iuout6, FILE=nameout5,
      &     FORM='unformatted', STATUS="UNKNOWN")
             
-      write(unit=iuout6)  dz_targ,ftext_targ,ftext_targ,sl_targ 
+      write(unit=iuout6)  dz_targ,ftext_targ_n,ftext_targ_n,sl_targ 
       close(iuout6)
 
 
+      iuout7=206
+      nameout="extend"
+      open(iuout7, FILE=nameout,
+     &     FORM='unformatted', STATUS="UNKNOWN")
+            
+      title7="ext 1"
+      write(unit=iuout7) title7,ftext_targ_n(:,:,1,1)+
+     &     ftext_targ_n(:,:,2,1)+ftext_targ_n(:,:,3,1)+
+     &     ftext_targ_n(:,:,4,1)+ftext_targ_n(:,:,5,1)
+      title7="sand "
+      write(unit=iuout7) title7,ftext_targ_n(:,:,1,1)
+      title7="silt "
+      write(unit=iuout7) title7,ftext_targ_n(:,:,2,1)
+      title7="clay "
+      write(unit=iuout7) title7,ftext_targ_n(:,:,3,1)
+      title7="peat "
+      write(unit=iuout7) title7,ftext_targ_n(:,:,4,1)
+      title7="rock "
+      write(unit=iuout7) title7,ftext_targ_n(:,:,5,1)
+      close(iuout7)
+
+c     144x90 output 
+      do i=1,144
+         do j=1,90
+            sl2X2H(i,j)=0.0
+            do k=1,6
+               dz2X2H(i,j,k)=0.0
+               do l=1,5
+                  f2X2H(i,j,l,k)=0.0
+                  do n=1,5
+                     ires=(i-1)*5+n
+                     do m=1,4
+                        jres=(j-1)*4+m
+                        f2X2H(i,j,l,k)=f2X2H(i,j,l,k)
+     &                       +ftext_targ_n(ires,jres,l,k)
+                        sl2X2H(i,j)=sl2X2H(i,j)
+     &                       +sl_targ(ires,jres)
+                        dz2X2H(i,j,k)=dz2X2H(i,j,k)
+     &                       +dz_targ(ires,jres,k)
+                     enddo
+                  enddo
+                  f2X2H(i,j,l,k)=f2X2H(i,j,l,k)/20.
+                  sl2X2H(i,j)=sl2X2H(i,j)/20.
+                  dz2X2H(i,j,k)=dz2X2H(i,j,k)/20.
+               enddo
+            enddo
+         enddo
+      enddo
+
+      iuout=iuout+1
+      nameout="SOIL2X2H_Reynolds"
+      open(iuout, FILE=nameout,
+     &     FORM='unformatted', STATUS="UNKNOWN")
+c      title="test"
+c       write(unit=iuout) title,sl2X2H
+      write(unit=iuout) dz2X2H,f2X2H,f2X2H,sl2X2H
+
+      close(iuout)
+   
       end program convSOIL
+
+      subroutine getnn(im,jm,dat, skip, i0,j0, in,jn,nd)
+C**** dat(i0,j0)=skip; find (i,j) with dat(i,j).ne.skip 'closest'
+C**** to point (i0,j0) - 'distance' = (j-j0)^2 + min(|i-i0|,im-|i-i0|)
+C**** taking into account that lat is more important than lon: (in,jn)
+      real dat(im,jm)
+
+C****    the distance function
+      ndist(ix,jx,iy,jy)=(jx-jy)**2+min(abs(ix-iy),im-abs(ix-iy))
+
+      nd=jm*jm+im       ! larger than ndist for any pair of points
+      do 20 id=1,im/2                   ! first try j=j0
+      if(id.ge.nd) go to 40
+      do 10 m=-1,1,2
+      i=i0+m*id
+      if(i.gt.im) i=i-im
+      if(i.lt.1) i=i+im
+      if (dat(i,j0).eq.skip) go to 10
+        nd0=id  !  =ndist(i0,j0,i,j0)
+        if (nd0.lt.nd) then
+          in=i
+          jn=j0
+          nd=nd0
+          go to 40
+        end if
+   10 continue
+   20 continue
+
+   40 do 100 jd=1,jm-1
+      if (jd*jd.ge.nd) return
+      do 60 m=-1,1,2
+      j=j0+m*jd
+      if(j.gt.jm) go to 60
+      if(j.le.0) go to 60
+      do 50 i=1,im
+      if (dat(i,j).eq.skip) go to 50
+        nd0=ndist(i0,j0,i,j)
+        if (nd0.lt.nd) then
+          in=i
+          jn=j
+          nd=nd0
+        end if
+   50 continue
+   60 continue
+  100 continue
+
+      return
+      end
