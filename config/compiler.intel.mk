263fab8b711e369ebe504df023fd2b8ba6e31d0e
ialeinov <ialeinov>
2010-05-11 00:12:06 +0000

    -fp-speculationoff actually should be applied only to 10.* and 11.* versions of compiler.

--- config/compiler.intel.mk
+++ config/compiler.intel.mk
@@ -4,8 +4,8 @@ IFORT_RELEASE := $(shell ifort --version | perl -e \
   'while(<>){ if(/ifort.* (\d+\.\d+)/) { print "$$1"; } }')
 FMAKEDEP = $(SCRIPTS_DIR)/sfmakedepend
 CMP_MOD = $(SCRIPTS_DIR)/compare_module_file.pl -compiler INTEL-ifort-9-0-on-LINUX
-FFLAGS = -fpp -O2 -ftz         -convert big_endian  -fp-speculationoff
-F90FLAGS = -fpp -O2 -ftz        -convert big_endian -free  -fp-speculationoff
+FFLAGS = -fpp -O2 -ftz         -convert big_endian 
+F90FLAGS = -fpp -O2 -ftz        -convert big_endian -free 
 LFLAGS = -O2 -ftz
 CPPFLAGS += -DCOMPILER_Intel8 -DCONVERT_BIGENDIAN
 F90_VERSION = $(shell $(F90) -v 2>&1)
@@ -17,18 +17,18 @@ endif
 
 # flags needed for particular releases
 ifeq ($(IFORT_RELEASE),11.1)
-FFLAGS += -assume protect_parens -fp-model strict
-F90FLAGS += -assume protect_parens -fp-model strict
+FFLAGS += -assume protect_parens -fp-model strict -fp-speculationoff
+F90FLAGS += -assume protect_parens -fp-model strict -fp-speculationoff
 endif
 
 ifeq ($(IFORT_RELEASE),11.0)
-FFLAGS += -assume protect_parens -fp-model strict
-F90FLAGS += -assume protect_parens -fp-model strict
+FFLAGS += -assume protect_parens -fp-model strict -fp-speculationoff
+F90FLAGS += -assume protect_parens -fp-model strict -fp-speculationoff
 endif
 
 ifeq ($(IFORT_RELEASE),10.1)
-FFLAGS += -assume protect_parens -fp-model strict -diag-disable vec
-F90FLAGS += -assume protect_parens -fp-model strict -diag-disable vec
+FFLAGS += -assume protect_parens -fp-model strict -diag-disable vec  -fp-speculationoff
+F90FLAGS += -assume protect_parens -fp-model strict -diag-disable vec  -fp-speculationoff
 endif
 
 ifeq ($(COMPILE_WITH_TRAPS),YES)
