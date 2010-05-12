e0ecc820cd4ef8de6273c0bb7e2690bc210717ea
tclune <tclune>
2010-05-12 00:54:14 +0000

    Initial implementation.

---
+++ model/tracers/Tracers_mod.F90
@@ -0,0 +1,513 @@
+module Tracers_mod
+  use Dictionary_mod, only: Dictionary_type
+  implicit none
+  private
+
+  public :: Tracer_type
+  public :: TracerBundle_type
+  public :: readFromText
+  public :: newReadFromText
+  public :: getTracer
+  public :: getProperty
+  public :: getProperties
+  public :: setProperty
+  public :: getIndex
+  public :: getCount
+  public :: makeSubset
+  public :: hasProperty
+  public :: checkMandatory
+  public :: clean
+
+  public :: NOT_FOUND
+
+  type Tracer_type
+    private
+    type (Dictionary_type) :: properties
+  end type Tracer_type
+
+  type TracerBundle_type
+    private
+    type (Tracer_type), pointer :: tracers(:) => null()
+  end type TracerBundle_type
+
+  interface clean
+    module procedure cleanTracer
+    module procedure cleanBundle
+  end interface
+
+  integer, parameter :: NOT_FOUND = -1
+
+  interface getProperty
+    module procedure getProperty_single
+    module procedure getProperty_multi
+  end interface
+
+  interface getProperties
+    module procedure getProperties_single
+  end interface
+
+  interface hasProperty
+    module procedure hasProperty_single
+    module procedure hasProperty_multi
+  end interface
+
+  interface setProperty
+    module procedure setProperty_integer, setProperty_integerArr
+    module procedure setProperty_real64,  setProperty_real64Arr
+    module procedure setProperty_logical, setProperty_logicalArr
+    module procedure setProperty_string,  setProperty_stringArr
+
+    module procedure setProperty_multi_integer, setProperty_multi_integerArr
+    module procedure setProperty_multi_real64,  setProperty_multi_real64Arr
+    module procedure setProperty_multi_logical, setProperty_multi_logicalArr
+    module procedure setProperty_multi_string,  setProperty_multi_stringArr
+  end interface
+
+  interface getCount
+    module procedure getCount_
+    module procedure getCount_property
+  end interface
+contains
+
+  function readFromText(unit, defaultValues) result(list)
+    use Dictionary_mod
+    use Parser_mod
+    integer, intent(in) :: unit
+    type (Dictionary_type), intent(in) :: defaultValues
+    type (Tracer_type), pointer :: list(:)
+    type (Tracer_type) :: tracer
+    type (Parser_type) :: parser
+
+    integer :: count
+    integer :: status
+    type (Tracer_type), allocatable :: oldList(:)
+
+    allocate(list(0))
+
+    count = 0
+    do
+      tracer = readOneTracer(unit, status)
+      if (status /= 0) exit
+
+      allocate(oldList(count))
+      oldList = list ! shallow copy ok
+      deallocate(list)
+      allocate(list(count+1))
+      list(1:count) = oldList
+      deallocate(oldList)
+      call merge(tracer%properties, defaultValues)
+      list(count+1)%properties = tracer%properties
+      count = count + 1
+
+    end do
+
+  end function readFromText
+
+  function newReadFromText(unit, defaultValues) result(bundle)
+    use Dictionary_mod
+    use Parser_mod
+    integer, intent(in) :: unit
+    type (Dictionary_type), intent(in) :: defaultValues
+    type (TracerBundle_type) :: bundle
+    type (Tracer_type) :: tracer
+    type (Parser_type) :: parser
+
+    integer :: count
+    integer :: status
+    type (Tracer_type), allocatable :: oldList(:)
+
+    allocate(bundle%tracers(0))
+
+    count = 0
+    do
+      tracer = readOneTracer(unit, status)
+      if (status /= 0) exit
+
+      allocate(oldList(count))
+      oldList = bundle%tracers ! shallow copy ok
+      deallocate(bundle%tracers)
+      allocate(bundle%tracers(count+1))
+      bundle%tracers(1:count) = oldList
+      deallocate(oldList)
+      call merge(tracer%properties, defaultValues)
+      bundle%tracers(count+1)%properties = tracer%properties
+      count = count + 1
+
+    end do
+
+  end function newReadFromText
+
+  function readOneTracer(unit, status) result(tracer)
+    use Parser_mod, only: Parser_type
+    use Parser_mod, only: setBeginData, setEndData
+    use Parser_mod, only: setCommentCharacters, setTokenSeparators
+    use Parser_mod, only: parse
+
+    integer, intent(in) :: unit
+    integer, intent(out) :: status
+    type (Tracer_type) :: tracer
+
+    type (Parser_type) :: parser
+
+    call setBeginData(parser, '{')
+    call setEndData(parser, '}')
+    call setTokenSeparators(parser, '=,')
+    call setCommentCharacters(parser, '!#')
+
+    tracer%properties = parse(parser, unit, status)
+    if (status /= 0) return
+
+  end function readOneTracer
+
+  integer function getIndex(this, name) result(index)
+    use Dictionary_mod, only: lookup
+    use KeyValuePair_mod, only: MAX_LEN_KEY
+    use GenericType_mod
+    type (TracerBundle_type), intent(in) :: this
+    character(len=*), intent(in) :: name
+
+    integer :: i
+    character(len=MAX_LEN_KEY) :: aName
+
+    index = NOT_FOUND
+    do i = 1, getCount(this)
+      if (all(name == lookup(this%tracers(i)%properties, 'name'))) then
+        index = i
+        exit
+      end if
+    end do
+
+  end function getIndex
+
+  logical function hasProperty_single(this, property) result(has)
+    use Dictionary_mod, only: hasKey
+    type (Tracer_type), intent(in) :: this
+    character(len=*), intent(in) :: property
+    has = hasKey(this%properties, property)
+  end function hasProperty_single
+
+  function hasProperty_multi(this, property) result(has)
+    use Dictionary_mod, only: hasKey
+    type (TracerBundle_type), intent(in) :: this
+    character(len=*), intent(in) :: property
+    logical :: has(size(this%tracers))
+
+    integer :: i
+
+    has = (/ (hasProperty(this%tracers(i), property), i = 1, getCount(this)) /)
+  end function hasProperty_multi
+
+  subroutine checkMandatory(this, properties)
+    type (TracerBundle_type), intent(in) :: this
+    character(len=*), intent(in) :: properties(:)
+
+    integer :: i, j
+    do i = 1, getCount(this)
+      do j = 1, size(properties)
+        if (hasProperty(this%tracers(i), properties(j))) cycle
+        call throwException("Tracers_mod::checkMandatory() - " // &
+             &  trim(getName(this%tracers(i))) // " missing property '" // &
+             &  trim(properties(j)) // "'.", 14)
+        return
+      end do
+    end do
+  end subroutine checkMandatory
+
+  function getName(this) result(name)
+    use GenericType_mod
+    use KeyValuePair_mod, only: MAX_LEN_KEY
+    type (Tracer_type), intent(in) :: this
+    character(len=MAX_LEN_KEY) :: name
+    name = getProperty(this, 'name')
+  end function getName
+
+  function getTracer(this, species) result(tracer)
+    type (TracerBundle_type), target, intent(in) :: this
+    character(len=*), intent(in) :: species
+    type (Tracer_type), pointer :: tracer
+
+
+    integer :: index
+
+    index = getIndex(this, species)
+    if (index /= NOT_FOUND) then
+      tracer => this%tracers(index)
+    else
+      tracer => null()
+    end if
+    
+  end function getTracer
+
+  function getProperties_single(this) result(properties)
+    type (Tracer_type), target, intent(in) :: this
+    type (Dictionary_type), pointer :: properties
+    properties => this%properties
+  end function getProperties_single
+
+  function getProperties_multi(this, species) result(properties)
+    type (TracerBundle_type), target, intent(in) :: this
+    character(len=*), intent(in) :: species
+    type (Dictionary_type), pointer :: properties
+
+    integer :: index
+
+    index = getIndex(this, species)
+    if (index /= NOT_FOUND) then
+      properties => this%tracers(index)%properties
+    else
+      nullify(properties)
+    end if
+
+  end function getProperties_multi
+
+  function getProperty_single(this, property) result (propertyValues)
+    use GenericType_mod
+    use Dictionary_mod, only: lookup
+    type (Tracer_type), intent(in) :: this
+    character(len=*), intent(in) :: property
+    type (GenericType_type), pointer :: propertyValues(:)
+
+    propertyValues => lookup(this%properties, property)
+    
+  end function getProperty_single
+
+  function getProperty_multi(this, species, property) result (propertyValues)
+    use GenericType_mod
+    use Dictionary_mod, only: lookup
+    type (TracerBundle_type), intent(in) :: this
+    character(len=*), intent(in) :: species
+    character(len=*), intent(in) :: property
+    type (GenericType_type), pointer :: propertyValues(:)
+
+    integer :: index
+
+    index = getIndex(this, species)
+    if (index /= NOT_FOUND) then
+      propertyValues => lookup(this%tracers(index)%properties, property)
+    else
+      allocate(propertyValues(0))
+    end if
+    
+  end function getProperty_multi
+
+  subroutine setProperty_integer(this, property, value)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    integer, intent(in) :: value
+    call insert(this%properties, property, value)
+  end subroutine setProperty_integer
+
+  subroutine setProperty_integerArr(this, property, values)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    integer, intent(in) :: values(:)
+    call insert(this%properties, property, values)
+  end subroutine setProperty_integerArr
+
+  subroutine setProperty_real64(this, property, value)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    real*8, intent(in) :: value
+    call insert(this%properties, property, value)
+  end subroutine setProperty_real64
+
+  subroutine setProperty_real64Arr(this, property, values)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    real*8, intent(in) :: values(:)
+    call insert(this%properties, property, values)
+  end subroutine setProperty_real64Arr
+
+  subroutine setProperty_logical(this, property, value)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    logical, intent(in) :: value
+    call insert(this%properties, property, value)
+  end subroutine setProperty_logical
+
+  subroutine setProperty_logicalArr(this, property, values)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    logical, intent(in) :: values(:)
+    call insert(this%properties, property, values)
+  end subroutine setProperty_logicalArr
+
+  subroutine setProperty_string(this, property, value)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    character(len=*), intent(in) :: value
+    call insert(this%properties, property, value)
+  end subroutine setProperty_string
+
+  subroutine setProperty_stringArr(this, property, values)
+    use Dictionary_mod, only: insert
+    type (Tracer_type), intent(inout) :: this
+    character(len=*), intent(in) :: property
+    character(len=*), intent(in) :: values(:)
+    call insert(this%properties, property, values)
+  end subroutine setProperty_stringArr
+
+
+  ! Select a tracer from list
+  subroutine setProperty_multi_integer(this, name, property, value)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    integer, intent(in) :: value
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, value)
+  end subroutine setProperty_multi_integer
+
+  subroutine setProperty_multi_integerArr(this, name, property, values)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    integer, intent(in) :: values(:)
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, values)
+  end subroutine setProperty_multi_integerArr
+
+  subroutine setProperty_multi_real64(this, name, property, value)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    real*8, intent(in) :: value
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, value)
+  end subroutine setProperty_multi_real64
+
+  subroutine setProperty_multi_real64Arr(this, name, property, values)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    real*8, intent(in) :: values(:)
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, values)
+  end subroutine setProperty_multi_real64Arr
+
+  subroutine setProperty_multi_logical(this, name, property, value)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    logical, intent(in) :: value
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, value)
+  end subroutine setProperty_multi_logical
+
+  subroutine setProperty_multi_logicalArr(this, name, property, values)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    logical, intent(in) :: values(:)
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, values)
+  end subroutine setProperty_multi_logicalArr
+
+  subroutine setProperty_multi_string(this, name, property, value)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    character(len=*), intent(in) :: value
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, value)
+  end subroutine setProperty_multi_string
+
+  subroutine setProperty_multi_stringArr(this, name, property, values)
+    use Dictionary_mod, only: insert
+    type (TracerBundle_type), intent(inout) :: this
+    character(len=*), intent(in) :: name
+    character(len=*), intent(in) :: property
+    character(len=*), intent(in) :: values(:)
+    
+    integer :: index
+
+    index = getIndex(this, name)
+    call insert(this%tracers(index)%properties, property, values)
+  end subroutine setProperty_multi_stringArr
+
+  integer function getCount_(this) result(number)
+    type (TracerBundle_type), intent(in) :: this
+
+    number = size(this%tracers)
+    
+  end function getCount_
+
+  integer function getCount_property(this, withProperty) result(number)
+    type (TracerBundle_type), intent(in) :: this
+    character(len=*), intent(in) :: withProperty
+
+    number = count(hasProperty(this, withProperty))
+    
+  end function getCount_property
+
+  subroutine makeSubset(this, withProperty, subset)
+    type (TracerBundle_type), target, intent(in) :: this
+    character(len=*), intent(in) :: withProperty
+    type (TracerBundle_type) :: subset
+
+    integer :: i, j
+
+    allocate(subset%tracers(getCount(this, withProperty)))
+
+    j = 1
+    do i = 1, getCount(this)
+      if (hasProperty(this%tracers(i), withProperty)) then
+        subset%tracers(j)%properties = this%tracers(i)%properties
+        j = j + 1
+      end if
+    end do
+
+  end subroutine makeSubset
+
+  subroutine cleanTracer(this)
+    use Dictionary_mod, only: clean
+    type (Tracer_type), intent(inout) :: this
+    call clean(this%properties)
+  end subroutine cleanTracer
+
+  subroutine cleanBundle(this)
+    use Dictionary_mod, only: clean
+    type (TracerBundle_type), intent(inout) :: this
+    integer :: i
+    do i = 1, getCount(this)
+      call clean(this%tracers(i)%properties)
+    end do
+    deallocate(this%tracers)
+  end subroutine cleanBundle
+
+end module Tracers_mod
