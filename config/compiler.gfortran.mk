d74b05c6d400cf86421769a2a5cce40b74de38b4
ialeinov <ialeinov>
2010-05-21 02:51:15 +0000

    Enabled optimization and switched to 64 bits for IRIX .

--- config/compiler.gfortran.mk
+++ config/compiler.gfortran.mk
@@ -2,9 +2,17 @@
 F90 = gfortran
 FMAKEDEP = $(SCRIPTS_DIR)/sfmakedepend
 CPPFLAGS += -DCOMPILER_G95
-FFLAGS = -cpp -fconvert=big-endian -O0
-F90FLAGS = -cpp -fconvert=big-endian -O0
+FFLAGS = -cpp -fconvert=big-endian -O2
+F90FLAGS = -cpp -fconvert=big-endian -O2
 LFLAGS =
+
+# machine-specific options
+ifeq ($(MACHINE),IRIX64)
+FFLAGS += -mabi=64 
+F90FLAGS += -mabi=64
+LFLAGS += -mabi=64
+endif
+
 # uncomment next two lines for extensive debugging
 # the following switch adds extra debugging
 ifeq ($(COMPILE_WITH_TRAPS),YES)
