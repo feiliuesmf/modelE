c0ec137675302dd14b5a7600f29caf4abdda6aee
ialeinov <ialeinov>
2010-06-08 19:46:47 +0000

    Updated flags to work with latest g95 (version 0.92).

--- config/compiler.g95.mk
+++ config/compiler.g95.mk
@@ -2,12 +2,12 @@
 F90 = g95
 FMAKEDEP = $(SCRIPTS_DIR)/sfmakedepend
 CPPFLAGS += -DCOMPILER_G95
-FFLAGS = -fno-second-underscore -O0 -fendian=big
-F90FLAGS = -fno-second-underscore -O0 -ffree-form -fendian=big
+FFLAGS = -cpp -fno-second-underscore -O2 -fendian=big
+F90FLAGS = -cpp -fno-second-underscore -O2 -ffree-form -fendian=big
 LFLAGS =
 # uncomment next two lines for extensive debugging
 # the following switch adds extra debugging
 ifeq ($(COMPILE_WITH_TRAPS),YES)
-FFLAGS += -trap=INVALID,DIVBYZERO,OVERFLOW -B111 -C
-LFLAGS += -lefence
+FFLAGS += -fbounds-check -ftrace=full -freal=nan -fpointer=invalid
+LFLAGS += #-lefence
 endif
