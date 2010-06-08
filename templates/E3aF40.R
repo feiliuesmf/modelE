e4dbd8a0d704da19558de92f088ee0973b5184e4
cdrar <cdrar>
2010-06-08 20:46:19 +0000

    Corrected input file names.

--- templates/E3aF40.R
+++ templates/E3aF40.R
@@ -53,8 +53,8 @@ ESMF_Interface shared
 
 Data input files:
 AIC=1JAN1956.rsfE6sF40 ! from model with climatol.ocean (in balance for YEARI=1979)
-OSST=OST_144x90.B.1950.M01.Hadl1  ! prescr. SST data starting 1/1950 (set: IYEAR1=1950)
-SICE=SICE_144x90.B.1950.M01.Hadl1 ! prescr. sea ice data starting 1/1950
+OSST=OST_144x90.1950.M01.Hadl1  ! prescr. SST data starting 1/1950 (set: IYEAR1=1950)
+SICE=SICE_144x90.1950.M01.Hadl1 ! prescr. sea ice data starting 1/1950
 CDN=CD144X90 VEG=V144X90_no_crops CROPS=CROPS2007_144X90N_nocasp
 SOIL=S144X900098M TOPO=Z144X90N_nocasp ! bdy.cond
 REG=REG2X2.5          ! special regions-diag
