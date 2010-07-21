f331c3fa4a30d3a675e53dc3a5b3db0b6b3a4102
tclune <tclune>
2010-07-21 14:42:14 +0000

    Minor portability fix for Mac OS using OpenMPI.

--- config/mpi.openmpi.mk
+++ config/mpi.openmpi.mk
@@ -14,8 +14,10 @@ CPPFLAGS += -DMPITYPE_LOOKUP_HACK
 ##LIBS += -lcprts -limf -lm -lcxa -lunwind -lrt -ldl \
 ##-lmpi_f90 -lmpi_f77 -lmpi -lstdc++ -threads
 
-LIBS += -lmpi_f77 -lmpi -lmpi_cxx -lstdc++ -lrt
-
+LIBS += -lmpi_f77 -lmpi -lmpi_cxx -lstdc++
+ifneq ($(shell uname),Darwin)
+  LIBS += -lrt
+endif
 
 #LIBS +=  -lmpi -lmpigc3 -lmpigc4 -lmpigf -lmpigi -lmpiic4 -lmpiic -lmpiif -threads
 
