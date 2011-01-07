2d109f602826102123b0c9a9464b2350473541b9
Tom <Thomas.L.Clune@nasa.gov>
2011-01-07 11:09:42 -0500

    Eliminated various machine dependencies for Random number generation. Previously when I had asked, no one had a compelling reason to use the intrinsic RAND() routines provided by various compilers.  And since the performance of BurnRandom() will only become more important as we scale the model, it will be best to use the hand coded one that provides an efficient implementation for skipping forward in the sequence.
    If this is a problem for anyone, let me know, and I'll put the
    alternate implementations back in.

--- model/shared/Random_mod.F90
+++ model/shared/Random_mod.F90
@@ -7,40 +7,15 @@ module RANDOM
   integer, save :: IX            !@var IX     random number seed
 
   ! Parameters used for "burning" sequences of random numbers
-#if defined( MACHINE_DEC ) || ( MACHINE_Linux)
   integer, parameter :: A_linear = 69069
-#elif defined(MACHINE_SGI) \
-  || ( defined(MACHINE_Linux) && ! defined(COMPILER_G95) ) \
-       || ( defined(MACHINE_MAC) && defined(COMPILER_ABSOFT) )
-  ! don't know how RAN() is implemented, so burn
-#else
-  integer, parameter :: A_linear = 65539
-#endif
+  ! integer, parameter :: A_linear = 65539 ! an alternate seed
+
   integer, parameter :: MAX_BITS = 31
   integer, parameter :: B_Half = 2**(MAX_BITS-1)
   integer, parameter :: B_linear = B_Half + (B_Half-1) ! 2147483647 = 2^31-1
 
 contains
 
-#if defined(MACHINE_SGI) \
-  || ( defined(MACHINE_Linux) && ! defined(COMPILER_G95) && ! defined(COMPILER_NAG) ) \
-       || defined(MACHINE_DEC) \
-  || ( defined(MACHINE_MAC) && defined(COMPILER_Intel8) ) \
-  || ( defined(MACHINE_MAC) && defined(COMPILER_ABSOFT) )
-  function RANDU (X)
-!@sum   RANDU calculates a random number based on the seed IX
-!@calls RAN
-    real*8 X                       !@var X      dummy variable
-    real*4 RAN                     !@fun RAN    SGI intrinsic func.
-    real*8 :: RANDU                !@var RANDU  random number
-    RANDU=RAN(IX)
-    return
-  end function RANDU
-#elif defined( MACHINE_IBM ) \
-  || ( defined(COMPILER_NAG) ) \
-  || ( defined(MACHINE_Linux) && defined(COMPILER_G95) ) \
-  || ( defined(MACHINE_MAC) && defined(COMPILER_G95) ) \
-  || ( defined(MACHINE_MAC) && defined(COMPILER_XLF) )
   function RANDU (X)
 !@sum   RANDU calculates a random number based on the seed IX
     real*8 X                       !@var X      dummy variable
@@ -58,10 +33,6 @@ contains
     RANDU=dble(IY)*.465661287308D-9
     return
   end function RANDU
-#else
-  none of supported architectures was specified.
-  This will crash the compiling process.
-#endif
 
   subroutine RINIT (INIT)
 !@sum   RINIT sets the initial seed IX
@@ -77,8 +48,6 @@ contains
     return
   end subroutine RFINAL
 
-#if defined( MACHINE_DEC ) \
-  || ( defined(MACHINE_Linux) && defined(COMPILER_Intel8) )
   subroutine BURN_RANDOM(n)
 !@sum  BURN_RANDOM burns a set number of random numbers. It is used to
     !                  maintain bit-wise correspondence on parallel runs.
@@ -101,30 +70,5 @@ contains
     end do
     return
   end subroutine burn_random
-#else
-  subroutine burn_random(n)
-    integer :: n
-    integer :: i
-    real*8  :: x
-    integer, save :: first_call=1
-
-#ifdef USE_ESMF
-    if ( first_call .ne. 0 ) then
-      write(6,*) ' ***********************************************'
-      write(6,*) ' Warning: slow implementation of burn_random()  '
-      write(6,*) ' on this platform.  Better performance can be   '
-      write(6,*) ' achieved by using a recursion relation for most'
-      write(6,*) ' random number generators. (contact Tom Clune)  '
-      write(6,*) ' ***********************************************'
-      first_call = 0
-    endif
-#endif
-
-    do i = 1, n
-      x = RANDU(x)
-    end do
-
-  end subroutine burn_random
-#endif
 
 end module RANDOM
