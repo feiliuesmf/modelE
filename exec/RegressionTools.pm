e40d941cf72d8d92bf4666f0dab0234d80f1b7b6
Thomas L Clune <tclune@dali02.prv.cube>
2011-04-12 17:07:54 -0400

    incorporated strong restart testing in nightly regression tests

--- exec/RegressionTools.pm
+++ exec/RegressionTools.pm
@@ -21,11 +21,10 @@ $extraFlags{intel} = "";
 $extraFlags{gfortran} ="";
 
 sub createTemporaryCopy {
-  my $env = shift;
+  my $referenceDir = shift;
   my $tempDir = shift;
 
-  my $referenceDir = $env->{REFERENCE_DIRECTORY};
-  my $commandString = "mkdir -p $tempDir;  cp -r -u $referenceDir/* $tempDir;";
+  my $commandString = "/usr/local/other/git/1.7.3.4_GNU/bin/git clone $referenceDir $tempDir;";
   return (CommandEntry -> new({COMMAND => $commandString}));
 
   }
@@ -36,9 +35,9 @@ sub createTemporaryCopy {
 sub compileRundeck {
   my $env = shift;
   my $installDir = shift;
-  my $compiler = shift;
 
-  my $rundeck    = $env -> {RUNDECK};
+  my $compiler = $env->{COMPILER};
+  my $rundeck  = $env->{RUNDECK};
   my $configuration = $env -> {CONFIGURATION};
   my $resultsDir = $env -> {RESULTS_DIRECTORY};
   $resultsDir .="/$compiler";
@@ -54,6 +53,7 @@ sub compileRundeck {
 
   my $logFile = "$resultsDir/$expName.buildlog";
   unlink($logFile); # delete it
+
   my $commandString = <<EOF;
   export MODELERC=$MODELERC;
   echo "MODELERC is $MODELERC";
@@ -74,8 +74,8 @@ sub runConfiguration {
     my $env = shift;
     my $installDir = shift;
     my $npes = shift;
-    my $compiler = shift;
 
+    my $compiler = $env->{COMPILER};
     my $rundeck    = $env->{RUNDECK};
     my $configuration = $env->{CONFIGURATION};
     my $resultsDir = $env->{RESULTS_DIRECTORY};
@@ -99,61 +99,41 @@ sub runConfiguration {
     
     my $MODELERC = $env->{MODELERC};
 
-    my $continue1Day = "./$expName -r ";
+    my $run1hr = "make rundeck RUN=$expName RUNSRC=$rundeck OVERWRITE=YES; make setup_nocomp RUN=$expName $flags";
+    my $run1dy = "../editRundeck $rundeck 46 2 0; make setup_nocomp RUN=$expName $flags";
+    my $restart = "cd $expName; cp fort.2.nc fort.1.nc ; ./$expName -r ";
+
     if ($configuration eq "MPI" or $configuration eq "OPENMP") {$continue1Day = "./$expName -np $npes -r ";}
 
     my $commandString = <<EOF;
-    echo "Using flags: $flags "
     export MODELERC=$MODELERC;
-    echo "MODELERC is $MODELERC";
     cd $installDir/decks;
-    rm -f $expName/fort.2.nc
-    make setup_nocomp RUN=$expName $flags COMPILER=$compiler;
-    cd $expName;
-    # Remove old result so that we notice if things fail really badly
-    rm -f $resultsDir/$expName.1hr$suffix;
-    cp fort.2.nc $resultsDir/$expName.1hr$suffix;
-    $continue1Day;
-    # Remove old result so that we notice if things fail really badly
-    rm -f $resultsDir/$expName.1dy$suffix;
-    cp fort.2.nc $resultsDir/$expName.1dy$suffix;
+    rm -f $expName/fort.2.nc;
+
+    $run1hr;
+    if [ -e $expName/fort.2.nc ]; then
+	cp $expName/fort.2.nc $resultsDir/$expName.1hr$suffix;
+    else
+	exit 1;
+    fi
+
+    $run1dy;
+    if [ -e $expName/fort.1.nc ]; then
+	cp $expName/fort.1.nc $resultsDir/$expName.1dy$suffix;
+    else
+	exit 1;
+    fi
+
+    $restart;
+    if [ -e $expName/fort.2.nc ]; then
+	cp $expName/fort.2.nc $resultsDir/$expName.restart$suffix;
+    else
+	exit 1;
+    fi
 EOF
   return (CommandEntry -> new({COMMAND => $commandString, QUEUE => "", STDOUT_LOG_FILE => "$logFile", NUM_PROCS => $npes, COMPILER => $compiler} ));
 }
 
-sub checkResults {
-  my $env = shift;
-  my $tempDir = shift;
-  my $npes = shift;
-  my $compiler = shift;
-
-  my $installDir    = $env->{INSTALL_DIR};
-  my $rundeck       = $env->{RUNDECK};
-  my $configuration = $env->{CONFIGURATION};
-  my $resultsDir    = $env->{RESULTS_DIR};
-  $resultsDir .="/$compiler";
-  my $cmp           = "$resultsDir/CMPE002P";
-  my $expName;
-
-  if (@_) {$expName = shift}
-  else {$expName = "$rundeck.$configuration.$compiler"};
-
-  my $suffix;
-  if ($configuration eq "SERIAL" or $configuration eq "SERIALMP") {
-      $suffix = "";
-  }
-  else {
-      $suffix = ".np=$npes";
-  }
-
-  my $commandString = <<EOF;
-cd $resultsDir;
-$cmp $rundeck.SERIAL.1hr $expName.1hr$suffix;
-$cmp $rundeck.SERIAL.1dy $expName.1dy$suffix;
-EOF
-
-  return (CommandEntry -> new({COMMAND => $commandString, STDOUT_LOG_FILE => "$expName.log"}))
-}
 
 sub writeModelErcFile {
     my $env = shift;
@@ -180,6 +160,158 @@ sub gitCheckout {
 popd)
 EOF
   return (CommandEntry -> new({COMMAND => $commandString}))
+}
+
+sub getEnvironment {
+    my $compiler = shift;
+    my $scratchDir = shift;
+
+    if ($compiler eq "intel") {
+	return getIntelEnvironment($scratchDir);
+    }
+    else {
+	return getGfortranEnvironment($scratchDir);
+    }
+}
+
+sub getIntelEnvironment{
+    my $scratchDir = shift;
+
+    my $env = {};
+
+    $env->{SCRATCH_DIRECTORY}=$scratchDir;
+    $env->{BASELINE_DIRECTORY}="$ENV{NOBACKUP}/modelE_baseline";
+    $env->{RESULTS_DIRECTORY} = $ENV{NOBACKUP}."/regression_results";
+    $env->{GITROOT}="simplex.giss.nasa.gov:/giss/gitrepo/modelE.git";
+    $env->{DECKS_REPOSITORY}="$scratchDir/decks_repository";
+    $env->{CMRUNDIR}="$scratchDir/cmrun";
+    $env->{EXECDIR}="$scratchDir/exec";
+    $env->{SAVEDISK}="$scratchDir/savedisk";
+    $env->{GCMSEARCHPATH}="/discover/nobackup/projects/giss/prod_input_files";
+    $env->{MP}="no";
+    $env->{OVERWRITE}="YES";
+    $env->{OUTPUT_TO_FILES}="YES";
+    $env->{VERBOSE_OUTPUT}="YES";
+    $env->{BASELIBDIR5}="/usr/local/other/esmf510/Linux";
+    $env->{MPIDISTR}="intel";
+    $env->{COMPILER}="intel";
+    $env->{ESMF_BOPT}="O";
+    $env->{NETCDFHOME}="/usr/local/other/netcdf/3.6.2_intel-11.0.083";
+    $env->{MODELERC}="$scratchDir/intel/modelErc.intel";
+    return $env;
+}
+
+sub getGfortranEnvironment {
+    my $scratchDir = shift;
+
+    my $env = {};
+    
+    $env->{SCRATCH_DIRECTORY}=$scratchDir;
+    $env->{BASELINE_DIRECTORY}="$ENV{NOBACKUP}/modelE_baseline";
+    $env->{RESULTS_DIRECTORY} = $ENV{NOBACKUP}."/regression_results";
+    $env->{GITROOT}="simplex.giss.nasa.gov:/giss/gitrepo/modelE.git";
+    $env->{DECKS_REPOSITORY}="$scratchDir/decks_repository";
+    $env->{CMRUNDIR}="$scratchDir/cmrun";
+    $env->{EXECDIR}="$scratchDir/exec";
+    $env->{SAVEDISK}="$scratchDir/savedisk";
+    $env->{GCMSEARCHPATH}="/discover/nobackup/projects/giss/prod_input_files";
+    $env->{MP}="no";
+    $env->{OVERWRITE}="YES";
+    $env->{OUTPUT_TO_FILES}="YES";
+    $env->{VERBOSE_OUTPUT}="YES";
+    $env->{BASELIBDIR5}="/usr/local/other/esmf5/gcc4.5_openmpi-1.4.2/Linux";
+    $env->{MPIDIR}="/usr/local/other/openMpi/gcc-4.5";
+    $env->{MPIDISTR}="openmpi";
+    $env->{COMPILER}="gfortran";
+    $env->{ESMF_BOPT}="O";
+    $env->{NETCDFHOME}="/usr/local/other/netcdf/3.6.2_gcc4.5";
+    $env->{MODELERC}="$scratchDir/gfortran/modelErc.gfortran";
+    return $env;
+}
+
+sub checkConsistency {
+    my $env = shift;
+    my $rundeck = shift;
+    my $useCases = shift;
+
+    my $results = {};
+    $results->{ARE_CONSISTENT} = 1; # True unless proved otherwise
+    $results->{ALL_COMPLETED} = 1; # True unless proved otherwise
+    $results->{NEW_SERIAL} = 0; 
+    $results->{MESSAGES} = "";
+    $results->{RESTART_CHECK} = 1; # True unless proved otherwise
+
+    $compiler = $env->{COMPILER};
+
+    my $compare = "/discover/nobackup/projects/giss/exec/diffreport";
+
+
+    foreach my $configuration (@{$useCases->{CONFIGURATIONS}}) {
+
+	my $tempDir="$scratchDir/$compiler/$rundeck.$configuration.$compiler";
+	    
+	@peList = (1);
+	if ($configuration eq "MPI" or $configuration eq "OPENMP") {
+	    @peList = @{$useCases->{NUM_MPI_PROCESSES}};
+	}
+
+	my $resultsDir = $env->{RESULTS_DIRECTORY} . "/$compiler";
+	    
+	foreach my $npes (@peList) {
+	    my $suffix;
+	    if    ($configuration eq "MPI")    {$suffix = ".np=$npes";}
+	    else {$suffix = "";}
+	    
+	    foreach my $duration (@{$useCases->{DURATIONS}}) {
+		
+		my $reference;
+		if    ($configuration eq "MPI" or $duration eq "restart")    {$reference = "$rundeck.SERIAL";}
+		else {$reference = "$env->{BASELINE_DIRECTORY}/$compiler/$rundeck.SERIAL";}
+	    
+		my $outfile = "$resultsDir/$rundeck.$configuration.$compiler.$duration$suffix";
+		print LOG "Looking for $outfile \n";
+		if (-e $outfile) {
+		    if ($duration == "restart") {
+			my $referenceOutput = "$reference.$compiler.$1dy";
+		    }
+		    else {
+			my $referenceOutput = "$reference.$compiler.$duration";
+		    }
+		    my $testOutput = "$rundeck.$configuration.$compiler.$duration$suffix";
+		    my $numLinesFound = `cd $resultsDir; $compare $referenceOutput $testOutput is_npes_reproducible | wc -l`;
+		    chomp($numLinesFound);
+		    print LOG "CHOMP:: $referencOutput \n";
+		    print LOG "CHOMP:: $testOutput \n";
+		    print LOG "CHOMP:: $numLinesFound \n";
+		    if ($numLinesFound > 0) {
+			if ($configuration eq "MPI") {
+			    $results->{MESSAGES} .= "   FAILURE - Inconsistent results for rundeck $rundeck, compiler $compiler, and duration $duration on $npes npes.\n";
+			    $results{ARE_CONSISTENT} = 0; #failure
+			}
+			else {
+			    if ($duration eq "restart") {
+				$results->{MESSAGES} .= "   FAILURE - Serial restart is not consistent.\n";
+				$results->{RESTART_CHECK} = 0;
+			    }
+			    else {
+				$results->{MESSAGES} .= "   WARNING - Serial run of rundeck $rundeck using compiler $compiler is different than stored results for duration $duration. \n";
+				$results{NEW_SERIAL} = 1; # change detected
+			    }
+			}
+		    }
+		}
+		else {
+		    print LOG " but it was not found \n";
+		    $results->{MESSAGES} .= "   FAILURE - expected output file does not exist: $rundeck.$configuration.$compiler.$duration$suffix\n";
+		    $results{ALL_COMPLETED} = 0; # failure
+		    $results{ARE_CONSISTENT} = 0; # failure
+		    $results{RESTART_CHECK} = 0; # failure
+		    last;
+		}
+	    }
+	}
+    }
+    return $results;
 }
 
 1;
--- exec/regressionTests.pl
+++ exec/regressionTests.pl
@@ -12,111 +12,69 @@ open(LOG,">nightlyTests.log");
 }
 
 my $scratchDir = $ENV{NOBACKUP}."/regression_scratch";
+my $resultsDir = $ENV{NOBACKUP}."/regression_results";
+my $reference = "$scratchDir/modelE";
 
-@modelErcVariables = (DECKS_REPOSITORY, MP, CMRUNDIR, EXECDIR, OVERWRITE, 
-		      OUTPUT_TO_FILES, VERBOSE_OUTPUT, SAVEDISK, GCMSEARCHPATH,
-		      COMPILER, COMPILER_VERSION, BASELIBDIR5, ESMF_BOPT, MPIDISTR, NETCDFHOME);
+my $env = {};
+$env->{"intel"} = getEnvironment("intel",$scratchDir);
+$env->{"gfortran"} = getEnvironment("gfortran",$scratchDir);
 
-my $env;
-$env->{"intel"}->{SCRATCH_DIRECTORY}=$scratchDir;
-$env->{"intel"}->{REFERENCE_DIRECTORY}="$scratchDir/modelE";
-$env->{"intel"}->{BASELINE_DIRECTORY}="$ENV{NOBACKUP}/modelE_baseline";
-$env->{"intel"}->{RESULTS_DIRECTORY} = $ENV{NOBACKUP}."/regression_results";
-$env->{"intel"}->{GITROOT}="simplex.giss.nasa.gov:/giss/gitrepo/modelE.git";
-$env->{"intel"}->{DECKS_REPOSITORY}="$scratchDir/decks_repository";
-$env->{"intel"}->{CMRUNDIR}="$scratchDir/cmrun";
-$env->{"intel"}->{EXECDIR}="$scratchDir/exec";
-$env->{"intel"}->{SAVEDISK}="$scratchDir/savedisk";
-$env->{"intel"}->{GCMSEARCHPATH}="/discover/nobackup/projects/giss/prod_input_files";
-$env->{"intel"}->{MP}="no";
-$env->{"intel"}->{OVERWRITE}="YES";
-$env->{"intel"}->{OUTPUT_TO_FILES}="YES";
-$env->{"intel"}->{VERBOSE_OUTPUT}="YES";
-$env->{"intel"}->{BASELIBDIR5}="/usr/local/other/esmf510/Linux";
-$env->{"intel"}->{MPIDISTR}="intel";
-$env->{"intel"}->{COMPILER}="intel";
-$env->{"intel"}->{ESMF_BOPT}="O";
-$env->{"intel"}->{NETCDFHOME}="/usr/local/other/netcdf/3.6.2_intel-11.0.083";
-$env->{"intel"}->{MODELERC}="$scratchDir/intel/modelErc.intel";
 
-$env->{"gfortran"}->{SCRATCH_DIRECTORY}=$scratchDir;
-$env->{"gfortran"}->{REFERENCE_DIRECTORY}="$scratchDir/modelE";
-$env->{"gfortran"}->{BASELINE_DIRECTORY}="$ENV{NOBACKUP}/modelE_baseline";
-$env->{"gfortran"}->{RESULTS_DIRECTORY} = $ENV{NOBACKUP}."/regression_results";
-$env->{"gfortran"}->{GITROOT}="simplex.giss.nasa.gov:/giss/gitrepo/modelE.git";
-$env->{"gfortran"}->{DECKS_REPOSITORY}="$scratchDir/decks_repository";
-$env->{"gfortran"}->{CMRUNDIR}="$scratchDir/cmrun";
-$env->{"gfortran"}->{EXECDIR}="$scratchDir/exec";
-$env->{"gfortran"}->{SAVEDISK}="$scratchDir/savedisk";
-$env->{"gfortran"}->{GCMSEARCHPATH}="/discover/nobackup/projects/giss/prod_input_files";
-$env->{"gfortran"}->{MP}="no";
-$env->{"gfortran"}->{OVERWRITE}="YES";
-$env->{"gfortran"}->{OUTPUT_TO_FILES}="YES";
-$env->{"gfortran"}->{VERBOSE_OUTPUT}="YES";
-$env->{"gfortran"}->{BASELIBDIR5}="/usr/local/other/esmf5/gcc4.5_openmpi-1.4.2/Linux";
-$env->{"gfortran"}->{MPIDIR}="/usr/local/other/openMpi/gcc-4.5";
-$env->{"gfortran"}->{MPIDISTR}="openmpi";
-$env->{"gfortran"}->{COMPILER}="gfortran";
-$env->{"gfortran"}->{ESMF_BOPT}="O";
-$env->{"gfortran"}->{NETCDFHOME}="/usr/local/other/netcdf/3.6.2_gcc4.5";
-$env->{"gfortran"}->{MODELERC}="$scratchDir/gfortran/modelErc.gfortran";
+my $resolutions = {};
+$resolutions->{EC12} = "8x10";
+$resolutions->{EM20} = "4x5";
+$resolutions->{EF40} = "2x2.5";
+$resolutions->{E4TcadF40} = "2x2.5";
+$resolutions->{E4arobio_h4c} = "2x2.5";
+$resolutions->{E4arobio_g6c} = "2x2.5";
+$resolutions->{SCMSGPCONT} = "0"; #hack - serial only
 
-#my $rundecks = ["EM20"];
-my $rundecks = ["EM20", "E4F40", "E4TcadF40",
-		"E4arobio_h4c", "E4arobio_g6c", "SCMSGPCONT"];
+
+#my $rundecks = ["EM20", "E4F40", "E4TcadF40",
+#		"E4arobio_h4c", "E4arobio_g6c", "SCMSGPCONT"];
 my $compilers = ["intel", "gfortran"];
+
+my $rundecks = ["EM20"];
 #my $compilers = ["intel"];
 
-my $configurations;
-$configurations->{"intel"}->{"EM20"}  = ["SERIAL", "MPI"];
-$configurations->{"intel"}->{"E4F40"}  = ["SERIAL", "MPI"];
-$configurations->{"intel"}->{"E4TcadF40"} = ["SERIAL", "MPI"];
-$configurations->{"intel"}->{"E4arobio_h4c"} = ["SERIAL", "MPI"];
-$configurations->{"intel"}->{"E4arobio_g6c"} = ["SERIAL", "MPI"];
-$configurations->{"intel"}->{"SCMSGPCONT"} = ["SERIAL"];
+#set defaults
 
-$configurations->{"gfortran"}->{"EM20"}  = ["SERIAL", "MPI"];
-$configurations->{"gfortran"}->{"E4F40"}  = ["SERIAL", "MPI"];
-$configurations->{"gfortran"}->{"E4TcadF40"} = ["SERIAL", "MPI"];
-$configurations->{"gfortran"}->{"E4arobio_h4c"} = ["SERIAL", "MPI"];
-$configurations->{"gfortran"}->{"E4arobio_g6c"} = ["SERIAL", "MPI"];
-$configurations->{"gfortran"}->{"SCMSGPCONT"}  = ["SERIAL"];
+#my $level = "AGGRESSIVE";
+my $level = "GENTLE";
+my $numProcesses = {};
 
-my $numProcessors;
-    $numProcessors->{EM20} ->{OPENMP} = [1,4];
-    $numProcessors->{E4F40} ->{OPENMP} = [1,4];
-    $numProcessors->{SCMSGPCONT} ->{OPENMP} = [1,4];
+$numProcesses->{"0"}->{GENTLE}     = [];
+$numProcesses->{"0"}->{AGGRESSIVE} = [];
+$numProcesses->{"0"}->{INSANE}     = [];
 
-$numProcessors->{SCMSGPCONT} ->{MPI}    = [1]; # no effective MPI for SCM case
+$numProcesses->{"8x10"}->{GENTLE}     = [1,4];
+$numProcesses->{"8x10"}->{AGGRESSIVE} = [1,4,12];
+$numProcesses->{"8x10"}->{INSANE}     = [1,4,22];
 
-my $level = "aggressive";
+$numProcesses->{"4x5"}->{GENTLE}     = [1,4];
+$numProcesses->{"4x5"}->{AGGRESSIVE} = [1,4,23];
+$numProcesses->{"4x5"}->{INSANE}     = [1,4,23,44];
 
-if ($level eq "gentle") { # 3 lats per proc
-    $numProcessors->{EM20} ->{MPI}    = [1,4,15];
-    $numProcessors->{E4F40} ->{MPI}    = [1,4,30];
-    $numProcessors->{E4TcadF40} ->{MPI}    = [1,4,30];
-    $numProcessors->{E4arobio_H4c} ->{MPI}    = [1,4,30];
-    $numProcessors->{E4arobio_g6c} ->{MPI}    = [1,4,30];
+$numProcesses->{"2x2.5"}->{GENTLE}     = [1,8];
+$numProcesses->{"2x2.5"}->{AGGRESSIVE} = [1,8,45];
+$numProcesses->{"2x2.5"}->{INSANE}     = [1,8,45,88];
+
+my $useCases = {};
+foreach my $rundeck (@$rundecks) {
+    $useCases->{$rundeck}->{COMPILERS} = $compilers;
+    $useCases->{$rundeck}->{CONFIGURATIONS} = ["SERIAL","MPI"];
+    $useCases->{$rundeck}->{NUM_MPI_PROCESSES} = $numProcesses->{$resolutions->{$rundeck}}->{$level};
+    $useCases->{$rundeck}->{DURATIONS} = ["1hr","1dy","restart"];
 }
-elsif ($level eq "aggressive") { # aggressive - 2 lats
-    $numProcessors->{EM20} ->{MPI}    = [1,4,23];
-    $numProcessors->{E4F40} ->{MPI}    = [1,4,45];
-    $numProcessors->{E4TcadF40} ->{MPI}    = [1,4,30];
-    $numProcessors->{E4arobio_H4c} ->{MPI}    = [1,4,30];
-    $numProcessors->{E4arobio_g6c} ->{MPI}    = [1,4,30];
-}
-else { # insane - 1 lat per proc
-    $numProcessors->{EM20} ->{MPI}    = [1,44];
-    $numProcessors->{E4F40} ->{MPI}    = [1,88];
-    $numProcessors->{E4TcadF40} ->{MPI}    = [1,88];
-    $numProcessors->{E4arobio_H4c} ->{MPI}    = [1,44];
-    $numProcessors->{E4arobio_g6c} ->{MPI}    = [1,88];
-}
+
+# Override anything else here
+$useCases->{"SCMSGPCONT"}->{CONFIGURATIONS} = ["SERIAL"];
+
 
 my $pool = CommandPool->new();
 
+my $clean = CommandEntry->new({COMMAND => "rm -rf $scratchDir/* regression.o* $resultsDir/*/*;"});
 my $git = CommandEntry->new(gitCheckout($env->{"intel"})); # Compiler is not important here
-my $clean = CommandEntry->new({COMMAND => "rm -rf $scratchDir/* regression.o*;"});
 
 $pool->add($clean);
 $pool->add($git);
@@ -125,37 +83,36 @@ $pool->add($git);
 $ENV{PATH}="/usr/local/other/gcc/4.5/bin:/usr/local/other/openMpi/gcc-4.5/bin:".$ENV{PATH};
 $ENV{LD_LIBRARY_PATH}="/usr/local/other/gcc/4.5/lib64:/usr/local/other/openMpi/gcc-4.5/lib:".$ENV{LD_LIBRARY_PATH};
 
-foreach my $compiler (@$compilers) {
-    print LOG "Using compiler $compiler\n";
-
-    $pool->add(writeModelErcFile($env->{$compiler}, $clean));
-
+foreach $compiler (@$compilers) {
+    $pool->add(writeModelErcFile($env->{$compiler}, $git));
     unless (-d $env->{$compiler}->{RESULTS_DIRECTORY}."/$compiler") {
 	mkdir "$env->{$compiler}->{RESULTS_DIRECTORY}/$compiler" or die $!;
     }
+}
 
-    foreach my $rundeck (@$rundecks) {
-	print LOG " ... processing rundeck: $rundeck \n";
-	$env->{$compiler}->{RUNDECK}=$rundeck;
-	foreach my $configuration (@{$configurations->{$compiler}->{$rundeck}}) {
-	    $env->{$compiler}->{CONFIGURATION}=$configuration;
-	    print LOG "       ... processing configuration $configuration\n";
+foreach my $rundeck (@$rundecks) { 
+    foreach $compiler (@{$useCases->{$rundeck}->{COMPILERS}}) {
+	$env->{$compiler}->{RUNDECK} = $rundeck;
+	foreach $configuration (@{$useCases->{$rundeck}->{CONFIGURATIONS}}) {
+
+	    $env->{$compiler}->{CONFIGURATION} = $configuration;
+
 	    my $tempDir="$scratchDir/$compiler/$rundeck.$configuration";
-
-	    my $copy  = createTemporaryCopy($env->{$compiler}, $tempDir);
+	    my $copy  = createTemporaryCopy($reference, $tempDir);
 	    $copy->{STDOUT_LOG_FILE} = "$env->{$compiler}->{RESULTS_DIRECTORY}/$compiler/$rundeck.$configuration.$compiler.buildlog";
 	    $pool->add($copy, $git);
-	    my $build = compileRundeck($env->{$compiler}, $tempDir, $compiler);
+
+	    my $build = compileRundeck($env->{$compiler}, $tempDir);
 	    $pool->add($build, $copy);
 	    
-	    my $previous = $build;
+	    my $previous = $build; # for dependencies
 	    
 	    my @peList = (1);
-	    if ($configuration eq "MPI" or $configuration eq "OPENMP") {
-		@peList = @{$numProcessors->{$rundeck}->{$configuration}};
+	    if ($configuration eq "MPI") {
+		@peList = @{$useCases->{$rundeck}->{NUM_MPI_PROCESSES}};
 	    }
 	    foreach my $npes (@peList) {
-		my $run = runConfiguration($env->{$compiler}, $tempDir, $npes, $compiler);
+		my $run = runConfiguration($env->{$compiler}, $tempDir, $npes);
 		$pool->add($run, $previous, $compiler);
 		$previous = $run;
 	    }
@@ -177,80 +134,20 @@ if ($pool->{notCompleted}) {
     print REPORT "***************************************\n";
 }
 
+foreach my $rundeck (@$rundecks) { 
+    foreach $compiler (@{$useCases->{$rundeck}->{COMPILERS}}) {
 
-foreach my $compiler (@$compilers) {
-    print LOG "Checking results for compiler $compiler: \n";
-    my $resultsDir = $env->{$compiler}->{RESULTS_DIRECTORY};
-    $resultsDir .= "/$compiler";
-    foreach my $rundeck (@$rundecks) {
-	next if $rundeck eq "SERIAL" or $rundeck eq "SERIALMP";
-	
-	my $cmp = "/discover/nobackup/projects/giss/exec/diffreport";
-	print LOG "    ... rundeck $rundeck: \n";
-	my $consistent = 1; # unless proved otherwise
-	my $newSerial  = 0; 
-	foreach my $configuration (@{$configurations->{$compiler}->{$rundeck}}) {
-	    my $tempDir="$scratchDir/$compiler/$rundeck.$configuration.$compiler";
-	    my $reference;
-	    if    ($configuration eq "MPI")    {$reference = "$rundeck.SERIAL";}
-	    elsif ($configuration eq "OPENMP") {$reference = "$rundeck.SERIALMP";}
-	    elsif ($configuration eq "SERIAL") {$reference = "$env->{$compiler}->{BASELINE_DIRECTORY}/$compiler/$rundeck.SERIAL";}
-	    else {next;}
-	    
-	    my $suffix;
-	    
-	    print LOG "         ... configuration: $configuration \n";
-	    
-	    @peList = (1);
-	    if ($configuration eq "MPI" or $configuration eq "OPENMP") {
-		@peList = @{$numProcessors->{$rundeck}->{$configuration}};
-	    }
-	    
-	    foreach my $npes (@peList){
-		if    ($configuration eq "MPI" or $configuration eq "OPENMP")    {$suffix = ".np=$npes";}
-		else {$suffix = "";}
-		
-		print LOG "     ... NPES=$npes ";
-		
-		foreach my $duration ("1hr", "1dy") {
-		    
-		    print LOG "$duration: ";
-		    
-		    $numLinesFound = `cd $resultsDir; $cmp $reference.$compiler.$duration $rundeck.$configuration.$compiler.$duration$suffix is_npes_reproducible | wc -l`;
-		    chomp($numLinesFound);
-		    
-		    if ($numLinesFound > 0){
-			print REPORT "Rundeck $rundeck ($configuration, $duration, $compiler) differs on $npes processors.\n";
-			print REPORT "   diffreport shows $numLinesFound lines of output.\n";
-			print REPORT `cd $resultsDir; $cmp $reference.$compiler.$duration $rundeck.$configuration.$compiler.$duration$suffix is_npes_reproducible`;
-			print REPORT "\n";
-			if ($configuration eq "SERIAL") {$newSerial = 1;}
-			else {$consistent = 0;}
-		    }
-		    else {
-			$checklist->{$rundeck}->{"$configuration.$compiler.$duration$suffix"} = "succeeded";
-			print LOG "succeeded ";
-		    }
-		}
-		print LOG "\n";
-	    }
-	    print LOG "\n";
-	}
-	if ($consistent) {
+	my $results = checkConsistency($env->{$compiler}, $rundeck, $useCases->{$rundeck});
+	print REPORT $results->{MESSAGES};
+
+	if ($results{ALL_COMPLETED} && $results{ARE_CONSISTENT} && $results{RESTART_CHECK}) {
 	    print REPORT "Rundeck $rundeck with compiler $compiler is strongly reproducible.\n";
-	    if ($newSerial) {
+	    if ($results{NEW_SERIAL}) {
 		print REPORT "  ... However serial results for rundeck $rundeck have changed.  Assuming change is intentional.\n";
-		`cd $resultsDir; cp $rundeck.SERIAL.$compiler.1* $env->{$compiler}->{BASELINE_DIRECTORY}/$compiler/.; `;
+                `cd $resultsDir/$compiler; cp $rundeck.SERIAL.$compiler.1* $env->{$compiler}->{BASELINE_DIRECTORY}/$compiler/.; `;
 	    }
 	}
-	else {
-	    foreach my $configuration (@{$configurations->{$compiler}->{$rundeck}}) {
-		#my $buildLogTail = `tail -20 $resultsDir/$rundeck.$configuration.$compiler.buildlog`;
-		#print REPORT "\nTail of Build Log for $rundeck.$configuration:\n";
-		#print REPORT "$buildLogTail\n";
-	    }
-	}
-	
+
     }
 }
 
