2b85cfbc3b7e3ba18def914cf384a2642b9d8e88
Carlos Cruz <carlos.a.cruz@nasa.gov>
2011-05-06 11:10:10 -0400

    Changes needed to deal with the new "setup".

--- exec/CommandPool.pm
+++ exec/CommandPool.pm
@@ -89,7 +89,7 @@ sub resetAll {
 }
 
 my $MAX_CYCLES=5000;
-my $SLEEP=10;
+my $SLEEP=20;
 
 sub run {
   my $self       = shift;
@@ -103,59 +103,59 @@ sub run {
   print "DEBUG is $DEBUG\n";
 
   for (;;) {
-    ++$counter;
+      ++$counter;
     
-    print $LOG "new cycle ... $counter\n" if $DEBUG;
-    if ($counter > $MAX_CYCLES) {
-	print $LOG "MAX_CYCLES exceeded - something is very wrong.\n"; 
-	$self->{notCompleted} = true;
-	return 0;
-    };
-
-    if ($self -> allComplete()) {
-	print $LOG "All tasks completed.  Shutting down.\n";
-	$self->{notCompleted} = 0;
-	last;
-    };
-  
-    for (my $id = 0; $id < ($self -> numCommands()); $id ++) {
-      my $cmd = $self -> get($id);
-      my $semaphore = $self -> semaphore($id);
-
-      if ($cmd -> isReady()) {
-        print $LOG " ... Launching $id \n" if $DEBUG;
-	print $LOG "     ...   " . $cmd -> {COMMAND} . "\n"  if $DEBUG;
-        $cmd -> setStatus(RUNNING);
-	
-        $cmd -> launch($semaphore);
-	$somethingChanged = 1;
+      print $LOG "new cycle ... $counter\n" if $DEBUG;
+      if ($counter > $MAX_CYCLES) {
+	  print $LOG "MAX_CYCLES exceeded - something is very wrong.\n"; 
+	  $self->{notCompleted} = true;
+	  return 0;
       }
-      elsif ($cmd -> isRunning()) {
-        if (-e $semaphore) {
-          print $LOG " ... Command $id has completed.\n" if $DEBUG;
-          unlink $semaphore;
-          $cmd -> setStatus(COMPLETE);
-	$somethingChanged = 1;
-        }
+      
+      if ($self -> allComplete()) {
+	  print $LOG "All tasks completed.  Shutting down.\n";
+	  $self->{notCompleted} = 0;
+	  last;
       }
-      elsif ($cmd -> isComplete()) {
+      
+      for (my $id = 0; $id < ($self -> numCommands()); $id ++) {
+	  my $cmd = $self -> get($id);
+	  my $semaphore = $self -> semaphore($id);
+	  
+	  if ($cmd -> isReady()) {
+	      print $LOG " ... Launching $id \n" if $DEBUG;
+	      print $LOG "     ...   " . $cmd -> {COMMAND} . "\n"  if $DEBUG;
+	      $cmd -> setStatus(RUNNING);
+	      
+	      $cmd -> launch($semaphore);
+	      $somethingChanged = 1;
+	  }
+	  elsif ($cmd -> isRunning()) {
+	      if (-e $semaphore) {
+		  print $LOG " ... Command $id has completed.\n" if $DEBUG;
+		  unlink $semaphore;
+		  $cmd -> setStatus(COMPLETE);
+		  $somethingChanged = 1;
+	      }
+	  }
+	  elsif ($cmd -> isComplete()) {
+	  }
+	  else {
+	      print $LOG " ... $id is waiting on dependencies.\n " if $DEBUG;
+	  }
       }
-      else {
-        print $LOG " ... $id is waiting on dependencies.\n " if $DEBUG;
-      }
-    }
-
-    if ($somethingChanged) {$somethingChanged =0}
-    else { print $LOG " ... sleeping for a while ... \n" if $DEBUG;
-	   sleep $SLEEP; }
-
+      
+      if ($somethingChanged) {$somethingChanged =0}
+      else { print $LOG " ... sleeping for a while ... \n" if $DEBUG;
+	     sleep $SLEEP; }
+      
   }
 # Do any final task
   if ($self -> {FINAL_COMMAND}) {
-        print $LOG "Final task ...\n";
-	$self -> {FINAL_COMMAND} -> launch(semaphore("/dev/nul"))
-	};
-
+      print $LOG "Final task ...\n";
+      $self -> {FINAL_COMMAND} -> launch(semaphore("/dev/nul"));
+  }
+  
   return 1;
 }
 
