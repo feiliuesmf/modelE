af726e6602c0ee26f753a30197f98147ffc4667b
Tom <Thomas.L.Clune@nasa.gov>
2012-05-24 15:18:11 -0400

    Update for NAG 5.3 compiler.

--- config/compiler.nag.mk
+++ config/compiler.nag.mk
@@ -1,12 +1,13 @@
 
-F90 = f95
+F90 = nagfor
 FMAKEDEP = $(SCRIPTS_DIR)/sfmakedepend
 CPPFLAGS += -DCOMPILER_NAG
-FFLAGS = -fpp -O0 -maxcontin=100 -kind=byte -dusty
-F90FLAGS = -fpp -O2 -free -kind=byte
+FFLAGS = -fpp -O0 -maxcontin=100 -kind=byte -dusty -f2003 -convert=BIG_ENDIAN
+F90FLAGS = -fpp -O2 -free -kind=byte  -f2003 -convert=BIG_ENDIAN
 LFLAGS =
 # the following switch adds extra debugging
 ifeq ($(COMPILE_WITH_TRAPS),YES)
-FFLAGS +=
-LFLAGS += -lefence
+FFLAGS += -C=all -gline
+F90FLAGS += -C=all -gline
+#LFLAGS += -lefence
 endif
