b454bd041beea100bb5d1c3d344c1dde19c92881
Maxwell Kelley <kelley@giss.nasa.gov>
2012-07-17 11:54:24 -0400

    Repairs after recent clock-related changes

--- model/SOATM_DRV.f
+++ model/SOATM_DRV.f
@@ -470,14 +470,18 @@ c      real*8, parameter :: c712=6d0/12d0,c112=0d0/12d0
       real*8 :: oevg,oprg,ebyp,t,rsiloc,sssresfac,sssresfac_ice
       integer :: i,j,l,n,j6hr,itmod,itperyr,jmm
       integer :: i_0,i_1, j_0,j_1
+      integer :: jyear,jday
+
+      jyear = modelEclock%year()
+      jday  = modelEclock%dayOfYear()
 
       if(interannual_forcing==1) then
-        if(modelEclock%year() < iaf_year_start) then
+        if(jyear < iaf_year_start) then
           write(6,*) 'please set model year >= iaf_year_start'
           call stop_model('model year < iaf_year_start',255)
         endif
         iaf_year = iaf_year_start +
-     &       mod(modelEclock%year()-iaf_year_start,1+iaf_year_end-iaf_year_start)
+     &       mod(jyear-iaf_year_start,1+iaf_year_end-iaf_year_start)
         if(iaf_year.ne.iaf_year_sv) then
           call read_core_interannual_data        
           iaf_year_sv = iaf_year
@@ -2147,6 +2151,7 @@ C****
       IMPLICIT NONE
       integer :: iu
       REAL*8 pyear
+      integer :: jyear
 
       character(len=300) :: out_line
       character*6 :: skip
@@ -2157,10 +2162,12 @@ C**** sync radiation parameters from input
       call sync_param( "S0_day", S0_day )
 
 C**** Set orbital parameters appropriately
-C**** Set orbital parameters appropriately
+
+      jyear = modelEclock%year()
+
       if (calc_orb_par_year.ne.0) then ! calculate from paleo-year
         ! 0 BP is defined as 1950CE
-        pyear = 1950.+modelEclock%YEAR()-IYEAR1-calc_orb_par_year
+        pyear = 1950.+JYEAR-IYEAR1-calc_orb_par_year
         write(out_line,*)
      *  "   Calculating Orbital Params for year : ",
      *  pyear,"     (CE);"
@@ -2295,9 +2302,7 @@ c
 !@sum  DAILY performs daily tasks at end-of-day and maybe at (re)starts
 !@auth Original Development Team
 !@ver  1.0
-      USE MODEL_COM, only :
-     *      itime,itimei,iyear1,nday,jdpery,jdendofm
-     *     ,aMON,aMONTH
+      USE MODEL_COM, only : iyear1,modelEclock
       USE RAD_COM, only : RSDIST,COSD,SIND,COSZ_day,DUSK,
      *     omegt,obliq,eccn,omegt_def,obliq_def,eccn_def,
      *     calc_orb_par_year
@@ -2308,12 +2313,12 @@ c
       INTEGER i,j,l,iy,it
       character(len=300) :: out_line
       LOGICAL, INTENT(IN) :: end_of_day
+      integer :: jday,jyear
+
+      jyear = modelEclock%year()
+      jday  = modelEclock%dayOfYear()
 
 C**** Tasks to be done at end of day and at each start or restart
-C****
-C**** CALCULATE THE DAILY CALENDAR
-C****
-      call getdte(Itime,Nday,iyear1,Jyear,Jmon,Jday,Jdate,Jhour,amon)
 
 C**** CALCULATE SOLAR ANGLES AND ORBIT POSITION
 C**** This is for noon (GMT) for new day.
