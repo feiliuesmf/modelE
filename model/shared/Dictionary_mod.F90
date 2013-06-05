2eb93e9685ed74dcae0979cfb8b3382fdbc805eb
Carlos Cruz <carlos.a.cruz@nasa.gov>
2013-06-05 12:07:51 -0400

    Increase CPARAMS limit to avoid aborts in Tcad runs

--- model/shared/Dictionary_mod.F90
+++ model/shared/Dictionary_mod.F90
@@ -155,7 +155,7 @@ module Dictionary_mod
   integer, parameter :: MAX_PARAMS =500 
   integer, parameter :: MAX_RPARAMS =430
   integer, parameter :: MAX_IPARAMS = 1000 
-  integer, parameter :: MAX_CPARAMS = 350
+  integer, parameter :: MAX_CPARAMS = 400
 
   integer, parameter :: MAX_NAME_LEN = 32
   integer, parameter :: MAX_CHAR_LEN = 128
